579ba33167612f98212fdb92eb8b538b722ec74e
matumoto1234 <matumotosizuoka@gmail.com>
2019-02-03 21:39:13 +0900

    "add ../atcoder/abc117/abc117_a/4160453.cpp"

---
+++ atcoder/abc117/abc117_a/4160453.cpp
@@ -0,0 +1,12 @@
+// detail: https://atcoder.jp/contests/abc117/submissions/4160453
+#include<stdio.h>
+int main(void)
+{
+	float a,b;
+	float time;
+	scanf("%f",&a);
+	scanf("%f",&b);
+	time=a/b;
+	printf("%f",time);
+	return 0;
+}
