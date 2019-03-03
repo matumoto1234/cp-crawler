17839f6dd92bf22d490202b48347fa690d598efb
matumoto1234 <matumotosizuoka@gmail.com>
2019-03-03 21:46:09 +0900

    "add ../atcoder/abc120/abc120_b/4453946.cpp"

---
+++ atcoder/abc120/abc120_b/4453946.cpp
@@ -0,0 +1,21 @@
+// detail: https://atcoder.jp/contests/abc120/submissions/4453946
+#include<stdio.h>
+int main(void)
+{
+	int a, b, k,yaku[1000],su;
+	scanf("%d", &a);
+	scanf("%d", &b);
+	scanf("%d", &k);
+	int i,co=0;
+	for (i = 1; i <= 100; i++) {
+		if (a%i == 0) {
+			if (b%i == 0) {
+				yaku[co]=i;
+				co++;
+			}
+		}
+	}
+	su = yaku[k-1];
+	printf("%d\n", su);
+	return 0;
+}
