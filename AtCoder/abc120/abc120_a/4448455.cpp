638a9f0762fdcef6c5834d33677c2ba1df41e76e
crawler <>
2019-03-03 21:14:44 +0900

    add ../AtCoder/abc120/abc120_a/4448455.cpp

---
+++ AtCoder/abc120/abc120_a/4448455.cpp
@@ -0,0 +1,16 @@
+// detail: https://atcoder.jp/contests/abc120/submissions/4448455
+#include<stdio.h>
+int main(void)
+{
+	int a, b, c,co=0;
+	scanf("%d", &a);
+	scanf("%d", &b);
+	scanf("%d", &c);
+	while (a <= b&&c>0) {
+		b = b - a;
+		co++;
+		c--;
+	}
+	printf("%d\n",co);
+	return 0;
+}
