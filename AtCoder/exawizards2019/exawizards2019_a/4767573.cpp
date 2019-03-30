8457512c0d0fa95ffca865ad76ed26e3b6bae2f5
crawler <>
2019-03-30 21:09:24 +0900

    add ../AtCoder/exawizards2019/exawizards2019_a/4767573.cpp

---
+++ AtCoder/exawizards2019/exawizards2019_a/4767573.cpp
@@ -0,0 +1,16 @@
+// detail: https://atcoder.jp/contests/exawizards2019/submissions/4767573
+#include<stdio.h>
+int main(void)
+{
+	int n1,n2,n3;
+	scanf("%d", &n1);
+	scanf("%d", &n2);
+	scanf("%d", &n3);
+	if (n1 == n2 && n2 == n3) {
+		printf("Yes\n");
+	}
+	else {
+		printf("No\n");
+	}
+	return 0;
+}
