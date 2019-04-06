edcdf28bb928235fa893494060888bfc01afda8f
matumoto1234 <matumotosizuoka@gmail.com>
2019-04-06 21:33:40 +0900

    "add ../atcoder/abc123/abc123_a/4852055.cpp"

---
+++ atcoder/abc123/abc123_a/4852055.cpp
@@ -0,0 +1,31 @@
+// detail: https://atcoder.jp/contests/abc123/submissions/4852055
+#include<stdio.h>
+int main(void)
+{
+	int antena[5];
+	int k;
+	int i, j;
+	int flag = 0;
+
+	for (i = 0; i < 5; i++) {
+		scanf("%d", &antena[i]);
+	}
+	scanf("%d", &k);
+	for (i = 0; i < 5; i++) {
+		for (j = i + 1; j < 5; j++) {
+			printf("%d %d\n", antena[j], antena[i]);
+			if (antena[j] > antena[i]) {
+				if (k < antena[j] - antena[i]) {
+					flag = 1;
+				}
+			}
+		}
+	}
+	if (flag == 1) {
+		printf(":(\n");
+	}
+	else {
+		printf("Yay!\n");
+	}
+	return 0;
+}
