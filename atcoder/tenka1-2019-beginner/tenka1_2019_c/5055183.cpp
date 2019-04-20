35b4664032e6614ab9d153d6c03c2d398482031e
matumoto1234 <matumotosizuoka@gmail.com>
2019-04-20 22:01:17 +0900

    "add ../atcoder/tenka1-2019-beginner/tenka1_2019_c/5055183.cpp"

---
+++ atcoder/tenka1-2019-beginner/tenka1_2019_c/5055183.cpp
@@ -0,0 +1,59 @@
+// detail: https://atcoder.jp/contests/tenka1-2019-beginner/submissions/5055183
+#include<stdio.h>
+int main(void)
+{
+	int n;
+	int i;
+	char a[200001];
+	int co, co1, co2;
+
+	co = co1 = co2 = 0;
+	scanf("%d", &n);
+	scanf("%s", a);
+	for (i = 0; a[i] != '\0'; i++) {
+		if (a[i] == '#') {
+			co1++;
+		}
+		else if (a[i] == '.') {
+			co2++;
+		}
+	}
+	if (co1 > co2) {
+		for (i = 0; a[i] != '\0'; i++) {
+			if (a[i] == '#') {
+				if (a[i + 1] == '.') {
+					a[i + 1] = '#';
+					co++;
+				}
+			}
+		}
+	}
+	else if(co1 < co2){
+		for (i = 0; a[i] != '\0'; i++) {
+			if (a[i] == '.') {
+				if (a[i - 1] == '#') {
+					a[i - 1] = '.';
+					co++;
+				}
+			}
+		}
+	}
+	else if (co1 == co2) {
+		for (i = 0; a[i] != '\0'; i++) {
+			if (a[i] == '#') {
+				if (a[i + 1] == '.') {
+					if (a[i + 2] == '.') {
+						a[i] = '.';
+						co++;
+					}
+					else {
+						a[i + 1] = '#';
+						co++;
+					}
+				}
+			}
+		}
+	}
+	printf("%d\n", co);
+	return 0;
+}
