5f9c42349c2822793a527a9b7214bfb5427273a9
crawler <>
2019-06-01 21:21:33 +0900

    add ../AtCoder/m-solutions2019/m_solutions2019_a/5730701.cpp

---
+++ AtCoder/m-solutions2019/m_solutions2019_a/5730701.cpp
@@ -0,0 +1,10 @@
+// detail: https://atcoder.jp/contests/m-solutions2019/submissions/5730701
+#include<stdio.h>
+int main(void)
+{
+  int n;
+  scanf("%d",&n);
+  n=(n-2)*180;
+  printf("%d¥n",n);
+  return 0;
+}
