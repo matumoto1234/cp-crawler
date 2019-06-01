f6290023187165378dc1e2d21c57e0dac9236715
matumoto1234 <matumotosizuoka@gmail.com>
2019-06-01 21:21:57 +0900

    "add ../atcoder/m-solutions2019/m_solutions2019_a/5730735.cpp"

---
+++ atcoder/m-solutions2019/m_solutions2019_a/5730735.cpp
@@ -0,0 +1,10 @@
+// detail: https://atcoder.jp/contests/m-solutions2019/submissions/5730735
+#include<stdio.h>
+int main(void)
+{
+  int n;
+  scanf("%d",&n);
+  n=(n-2)*180;
+  printf("%d",n);
+  return 0;
+}
