fa53e8a296762c5b0bf1fa48b977a4bd8d35c946
matumoto1234 <matumotosizuoka@gmail.com>
2019-06-16 21:01:31 +0900

    "add ../atcoder/abc130/abc130_a/5951926.cpp"

---
+++ atcoder/abc130/abc130_a/5951926.cpp
@@ -0,0 +1,18 @@
+// detail: https://atcoder.jp/contests/abc130/submissions/5951926
+#include<stdio.h>
+int main(void)
+{
+	int n;
+	int s;
+	int i;
+
+	scanf("%d", &n);
+	scanf("%d", &s);
+	if (n < s) {
+		printf("0\n");
+	}
+	else {
+		printf("10\n");
+	}
+	return 0;
+}
