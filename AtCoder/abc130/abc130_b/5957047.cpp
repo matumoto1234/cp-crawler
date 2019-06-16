133ba89d8b009fd1477b4c9e248d77873141bcb0
crawler <>
2019-06-16 21:07:20 +0900

    add ../AtCoder/abc130/abc130_b/5957047.cpp

---
+++ AtCoder/abc130/abc130_b/5957047.cpp
@@ -0,0 +1,24 @@
+// detail: https://atcoder.jp/contests/abc130/submissions/5957047
+#include<stdio.h>
+int main(void)
+{
+	int n;
+	int x;
+	int s[101];
+	int i;
+	int x1 = 0, co = 0;
+
+	scanf("%d %d", &n, &x);
+	for (i = 0; i < n; i++) {
+		scanf("%d", &s[i]);
+	}
+	for (i = 0; i < n; i++) {
+		if (x1 <= x) {
+			co++;
+		}
+		x1 = x1 + s[i];
+		
+	}
+	printf("%d\n", co);
+	return 0;
+}
