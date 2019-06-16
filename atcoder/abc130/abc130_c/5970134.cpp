28b5229f81c9ef26f2e8419d5d0d2d587c8c3ccc
matumoto1234 <matumotosizuoka@gmail.com>
2019-06-16 21:39:39 +0900

    "add ../atcoder/abc130/abc130_c/5970134.cpp"

---
+++ atcoder/abc130/abc130_c/5970134.cpp
@@ -0,0 +1,34 @@
+// detail: https://atcoder.jp/contests/abc130/submissions/5970134
+#include<stdio.h>
+int main(void)
+{
+	int w, h, x, y;
+	double menseki;
+	int i, j, k;
+
+	scanf("%d %d %d %d", &w, &h, &x, &y);
+	menseki = (double)w*(double)h;
+	if (x == w / 2) {
+		if (y == h / 2) {
+			printf("%.9lf\n", menseki / 2);
+			printf("1\n");
+			return 0;
+		}
+	}
+	if (x == w / 2 || y == h / 2) {
+		printf("%.9lf\n", menseki / 2);
+		printf("0\n");
+	}
+	else {
+		if (w / 2 > x) {
+			w = w - x;
+		}
+		else {
+			w = w - (w - x);
+		}
+		menseki = w * h;
+		printf("%.9lf\n", menseki);
+		printf("1\n");
+	}
+	return 0;
+}
