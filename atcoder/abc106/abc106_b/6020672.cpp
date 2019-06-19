2b59671d9dc93c859c8347371705d57646a7a96b
matumoto1234 <matumotosizuoka@gmail.com>
2019-06-19 18:30:42 +0900

    "add ../atcoder/abc106/abc106_b/6020672.cpp"

---
+++ atcoder/abc106/abc106_b/6020672.cpp
@@ -0,0 +1,25 @@
+// detail: https://atcoder.jp/contests/abc106/submissions/6020672
+#include<stdio.h>
+int main(void)
+{
+  int n,co=0,cnt=0;
+  int i ,j;
+  
+  scanf("%d",&n);
+  for(i=1;i<n;i+=2){
+    for(j=1;j<i;j++){
+      if(i%j==0){
+      co++;
+      }
+    } 
+    if(co>=9){
+      continue;
+    }
+    else if(co==8){
+      cnt++;
+      break;
+    }
+  }
+  printf("%d\n",cnt);
+  return 0;
+}
