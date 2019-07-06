eb52adfe9faff653120827b34a3807b2fa71cbe5
matumoto1234 <matumotosizuoka@gmail.com>
2019-07-07 00:41:12 +0900

    "add ../atcoder/abc100/abc100_a/6257264.c"

---
+++ atcoder/abc100/abc100_a/6257264.c
@@ -0,0 +1,14 @@
+// detail: https://atcoder.jp/contests/abc100/submissions/6257264
+#include<stdio.h>
+int main()
+{
+  int a,b;
+  scanf("%d %d",&a,&b);
+  if(a<=8||b<=8){
+    printf("Yay!\n");
+  }
+  else{
+	printf(":(\n");
+  }
+  return 0;
+}
