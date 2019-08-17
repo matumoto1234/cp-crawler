8af4f29cc54fe3a68fc196e93fc6099518324e4b
matumoto1234 <matumotosizuoka@gmail.com>
2019-08-17 23:12:38 +0900

    "add ../atcoder/agc037/agc037_a/6962735.c"

---
+++ atcoder/agc037/agc037_a/6962735.c
@@ -0,0 +1,17 @@
+// detail: https://atcoder.jp/contests/agc037/submissions/6962735
+#include<stdio.h>
+#include<string.h>
+int main(void){
+  int len,i,co=0;
+  char s[100001];
+  scanf("%s",s);
+  len=strlen(s);
+  for(i=0;i<len;i++){
+    if(s[i]==s[i+1]){
+      co++;
+      i++;
+    }
+  }
+  printf("%d",len-co);
+  return 0;
+}
