c1a94b57cfc212cc349eb9f47a7bdd2bd3dd1ac6
crawler <matumotosizuoka+crawler@gmail.com>
2019-08-21 14:55:45 +0000

    add ../AtCoder/abc113/abc113_c/7065424.c

---
+++ AtCoder/abc113/abc113_c/7065424.c
@@ -0,0 +1,44 @@
+// detail: https://atcoder.jp/contests/abc113/submissions/7065424
+#include<stdio.h>
+#include<stdlib.h>
+typedef long long int ll;
+
+int int_sort(const void* a, const void* b) {
+	if (*(int*)a < *(int*)b) {
+		return -1;
+	}
+	else
+		if (*(int*)a == *(int*)b) {
+			return 0;
+		}
+	return 1;
+}
+//qsort((void*)a, 3*n, sizeof(a[0]), int_sort);
+#define NUM 100000
+#define NUM_RANK 100000
+int main(void)
+{
+	int n, m;
+	int year[NUM], p[NUM];
+	int rank[NUM][NUM_RANK+2] = {};
+
+	scanf("%d %d", &n, &m);
+	for (int i = 0; i < m; i++) {
+		scanf("%d %d", &p[i], &year[i]);
+	}
+	for (long int i = 0; i < NUM; i++) {
+		rank[i][NUM_RANK] = 1;
+	}
+	for (int i = 0; i < m; i++) {
+		rank[p[i] - 1][year[i]]++;
+	}
+	for (long int j = 0; j < NUM; j++) {
+		for (long int i = NUM_RANK; i >= 0; i--) {
+			rank[j][i] += rank[j][i + 1];
+		}
+	}
+	for (int i = 0; i < m; i++) {
+		printf("%06d%06d\n", p[i], rank[p[i]-1][year[i]]);
+	}
+	return 0;
+}
