f3970476503ba6d42508020643f7d6de7769c373
crawler <matumotosizuoka+crawler@gmail.com>
2019-08-30 14:47:05 +0000

    add ../AtCoder/joi2007yo/joi2007yo_f/7212364.c

---
+++ AtCoder/joi2007yo/joi2007yo_f/7212364.c
@@ -0,0 +1,59 @@
+// detail: https://atcoder.jp/contests/joi2007yo/submissions/7212364
+#include<stdio.h>
+
+int a, b, n;
+int x[40], y[40];
+int done[50][50] = { 0 }, memo[50][50];
+int itix, itiy;
+
+int road(int rx, int ry) {
+	
+	if (done[rx][ry] == 1)return memo[rx][ry];
+
+	for (int i = 0; i < n; i++) {
+		if (x[i] == rx && y[i] == ry) {
+			return 0;
+		}
+	}
+	if (rx == 0 || ry == 0) {
+		return 0;
+	}
+	if (rx < itix && rx == 1) {
+		return 1;
+	}
+	if (rx < itiy && ry == 1) {
+		return 1;
+	}
+	
+	int res;
+	res = road(rx - 1, ry) + road(rx, ry - 1);
+	done[rx][ry] = 1;
+	return memo[rx][ry] = res;
+}
+
+int main(void)
+{
+	
+
+	scanf("%d %d %d", &a, &b, &n);
+	itix = a+1;
+	itiy = b+1;
+	for (int i = 0; i < n; i++) {
+		scanf("%d %d", &x[i], &y[i]);
+		if (x[i] == 1) {
+			if (x[i] < itix) {
+				itix = x[i];
+			}
+		}
+		if (y[i] == 1) {
+			if (y[i] < itiy) {
+				itiy = y[i];
+			}
+		}
+	}
+	//printf("%d %d\n", itix, itiy);
+	int ans;
+	ans = road(a, b);
+	printf("%d\n", ans);
+	return 0;
+}
