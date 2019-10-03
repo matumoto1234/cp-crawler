c87147f4b0fc4528e22c28fb8a87a1cef8ee5e89
crawler <>
2019-10-03 23:25:05 +0900

    "add ../atcoder/abc118/abc118_c/7837890.cpp"

---
+++ atcoder/abc118/abc118_c/7837890.cpp
@@ -0,0 +1,31 @@
+// detail: https://atcoder.jp/contests/abc118/submissions/7837890
+#include<stdio.h>
+#include<stdlib.h>
+typedef long long int lli;
+long long int gcd(lli,lli);
+int lli_compare(const void*a,const void*b){return *(lli*)a-*(lli*)b;}
+int main(void)
+{
+    lli n;
+    lli a[100000];
+
+    scanf("%lld",&n);
+    for(lli i=0;i<n;i++){
+        scanf("%lld",&a[i]);
+    }
+    for(lli i=0;i<n-1;i++){
+        a[i+1]=gcd(a[i],a[i+1]);
+    }
+    printf("%lld\n",a[n-1]);
+    return 0;
+}
+long long int gcd(lli x,lli y)
+{
+    if(x<y){
+        lli tmp=x;
+        x=y;
+        y=tmp;
+    }
+    if(x%y==0)return y;
+    else return gcd(y,x%y);
+}
