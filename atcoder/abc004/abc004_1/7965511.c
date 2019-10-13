46a1ef051a1be9a4aef4e5b39a4ce753ab17061c
matumoto1234 <matumotosizuoka@gmail.com>
2019-10-14 00:59:02 +0900

    "add ../atcoder/abc004/abc004_1/7965511.c"

---
+++ atcoder/abc004/abc004_1/7965511.c
@@ -0,0 +1,9 @@
+// detail: https://atcoder.jp/contests/abc004/submissions/7965511
+#include<stdio.h>
+int main(void)
+{
+    long int n;
+    scanf("%ld",&n);
+    printf("%ld\n",2*n);
+    return 0;
+}
