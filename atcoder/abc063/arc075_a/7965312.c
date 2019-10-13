1a4965a2a33f306e51c2f33c7e35bb5b58cf0869
matumoto1234 <matumotosizuoka@gmail.com>
2019-10-14 00:37:05 +0900

    "add ../atcoder/abc063/arc075_a/7965312.c"

---
+++ atcoder/abc063/arc075_a/7965312.c
@@ -0,0 +1,43 @@
+// detail: https://atcoder.jp/contests/abc063/submissions/7965312
+#include<stdio.h>
+#include<stdlib.h>
+int int_compare(const void*a,const void*b){return *(int*)a-*(int*)b;}
+int main(void)
+{
+    int n;
+    int s[100];
+    int ans=0,tmp;
+
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        scanf("%d",&s[i]);
+        ans+=s[i];
+    }
+    qsort(s,n,sizeof(int),int_compare);
+    if(ans%10==0){
+        for(int j=0;j<n;j++){
+            for(int i=0;i<n;i++){
+                tmp=0;
+                for(int k=i;k<i+j;k++){
+                    ans-=s[k];
+                    tmp+=s[k];
+                }
+                if(ans%10==0){
+                    ans+=tmp;
+                }
+                else{
+                    printf("%d\n",ans);
+                    return 0;
+                }
+                
+            }
+        }
+        
+    }
+    else{
+        printf("%d\n",ans);
+        return 0;
+    }
+    printf("0\n");
+    return 0;
+}
