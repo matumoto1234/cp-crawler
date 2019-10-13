5597222a540888415efb8ae2a1c1215fafb22ce2
matumoto1234 <matumotosizuoka@gmail.com>
2019-10-14 01:03:47 +0900

    "add ../atcoder/abc005/abc005_1/7965557.cpp"

---
+++ atcoder/abc005/abc005_1/7965557.cpp
@@ -0,0 +1,9 @@
+// detail: https://atcoder.jp/contests/abc005/submissions/7965557
+#include<stdio.h>
+int main(void)
+{
+    int x,y;
+    scanf("%d %d",&x,&y);
+    printf("%d\n",y/x);
+    return 0;
+}
