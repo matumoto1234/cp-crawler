ad5b6d1736000e9f214f930295fa102a8f7654b8
crawler <>
2019-10-18 22:58:45 +0900

    "add ../atcoder/abc011/abc011_1/8010204.cpp"

---
+++ atcoder/abc011/abc011_1/8010204.cpp
@@ -0,0 +1,10 @@
+// detail: https://atcoder.jp/contests/abc011/submissions/8010204
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if(n==12)n=0;
+    cout<<n+1<<endl;
+}
