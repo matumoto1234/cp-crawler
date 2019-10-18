09becdafe26ddada197e508abfc46dac8d7d5ab2
crawler <matumotosizuoka+crawler@gmail.com>
2019-10-18 13:29:53 +0000

    add ../AtCoder/abc008/abc008_1/8009916.cpp

---
+++ AtCoder/abc008/abc008_1/8009916.cpp
@@ -0,0 +1,10 @@
+// detail: https://atcoder.jp/contests/abc008/submissions/8009916
+#include<stdio.h>
+int main(void)
+{
+    int s,t;
+
+    scanf("%d %d",&s,&t);
+    printf("%d\n",t-s+1);
+    return 0;
+}
