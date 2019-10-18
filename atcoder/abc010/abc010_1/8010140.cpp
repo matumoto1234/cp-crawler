702eeba946ec69e0ea42b5959eb67c2a344c3aa8
crawler <>
2019-10-18 22:53:58 +0900

    "add ../atcoder/abc010/abc010_1/8010140.cpp"

---
+++ atcoder/abc010/abc010_1/8010140.cpp
@@ -0,0 +1,12 @@
+// detail: https://atcoder.jp/contests/abc010/submissions/8010140
+#include<iostream>
+#include<string.h>
+using namespace std;
+int main(void)
+{
+    char s[13];
+
+    cin>>s;
+    strcat(s,"pp");
+    cout<<s<<endl;
+}
