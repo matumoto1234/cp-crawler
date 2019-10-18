eb40782f46ab4c16227163482fc8a248c7db55a5
crawler <>
2019-10-19 00:26:39 +0900

    "add ../atcoder/abc017/abc017_1/8011081.cpp"

---
+++ atcoder/abc017/abc017_1/8011081.cpp
@@ -0,0 +1,9 @@
+// detail: https://atcoder.jp/contests/abc017/submissions/8011081
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a1,a2,b1,b2,c1,c2;
+    cin>>a1>>a2>>b1>>b2>>c1>>c2;
+    cout<<(a1*a2+b1*b2+c1*c2)/10<<endl;
+}
