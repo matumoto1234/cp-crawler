7c808e60147e51b66005c290d5aec82cef476f43
crawler <matumotosizuoka+crawler@gmail.com>
2019-10-18 14:55:46 +0000

    add ../AtCoder/abc012/abc012_1/8010823.cpp

---
+++ AtCoder/abc012/abc012_1/8010823.cpp
@@ -0,0 +1,11 @@
+// detail: https://atcoder.jp/contests/abc012/submissions/8010823
+#include<iostream>
+#include<vector>
+
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout<<b<<" "<<a<<endl;
+}
