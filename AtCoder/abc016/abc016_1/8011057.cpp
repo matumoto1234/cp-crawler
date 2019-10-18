37f9abadfc6adbe79c88bbe148ecf16c95befcfc
crawler <matumotosizuoka+crawler@gmail.com>
2019-10-18 15:23:37 +0000

    add ../AtCoder/abc016/abc016_1/8011057.cpp

---
+++ AtCoder/abc016/abc016_1/8011057.cpp
@@ -0,0 +1,14 @@
+// detail: https://atcoder.jp/contests/abc016/submissions/8011057
+#include<iostream>
+using namespace std;
+int main()
+{
+    int m,d;
+    cin>>m>>d;
+    if(m%d==0){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
