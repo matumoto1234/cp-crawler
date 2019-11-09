de1722e0fdff7991ebcccaab5a830a9ec009c963
crawler <matumotosizuoka+crawler@gmail.com>
2019-11-09 12:02:01 +0000

    add ../AtCoder/nikkei2019-2-qual/nikkei2019_2_qual_a/8348623.c

---
+++ AtCoder/nikkei2019-2-qual/nikkei2019_2_qual_a/8348623.c
@@ -0,0 +1,10 @@
+// detail: https://atcoder.jp/contests/nikkei2019-2-qual/submissions/8348623
+#include<stdio.h>
+int main(void)
+{
+    int n;
+
+    scanf("%d",&n);
+    printf("%d\n",n/2);
+    return 0;
+}
