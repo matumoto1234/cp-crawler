87d7e352bbb748d8daa0280c8fe4063c7d75cf7b
matumoto1234 <matumotosizuoka@gmail.com>
2019-11-09 11:36:04 +0900

    "add ../atcoder/joi2007ho/joi2007ho_b/8341197.c"

---
+++ atcoder/joi2007ho/joi2007ho_b/8341197.c
@@ -0,0 +1,11 @@
+// detail: https://atcoder.jp/contests/joi2007ho/submissions/8341197
+#include<stdio.h>
+int main(void){
+  int n,k;
+  int a[1000000];
+  scanf("%d %d",&n,&k);
+  for(int i=0;i<k;i++){
+    scanf("%d",&a[i]);
+  }
+  return 0;
+}
