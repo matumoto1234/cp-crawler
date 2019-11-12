18c40b0a94c685ded1c988c5c7b371d2d8cdbbe1
crawler <>
2019-11-12 11:40:41 +0900

    "add ../atcoder/abc127/abc127_a/8403414.cpp"

---
+++ atcoder/abc127/abc127_a/8403414.cpp
@@ -0,0 +1,16 @@
+// detail: https://atcoder.jp/contests/abc127/submissions/8403414
+#include<stdio.h>
+int main(void)
+{
+  int a,b;
+  
+  scanf("%d %d",&a,&b);
+  if(a>=13){
+    printf("%d\n",b);
+  }else if(6<=a){
+    printf("%d\n",b/2);
+  }else{
+    printf("0\n");
+  }
+  return 0;
+}
