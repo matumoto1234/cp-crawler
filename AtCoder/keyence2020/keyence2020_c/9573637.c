6e9f7cd06faedac1f54b6e20b2ac7c910b578e16
crawler <matumotosizuoka+crawler@gmail.com>
2020-01-18 12:53:28 +0000

    add ../AtCoder/keyence2020/keyence2020_c/9573637.c

---
+++ AtCoder/keyence2020/keyence2020_c/9573637.c
@@ -0,0 +1,34 @@
+// detail: https://atcoder.jp/contests/keyence2020/submissions/9573637
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+    int n,k,s;
+    int wa[100001];
+    int a[100000];
+
+    scanf("%d %d %d",&n,&k,&s);
+    wa[0]=0;
+    if(s!=1000000000){
+        for(int i=0;i<n;i++){
+            a[i]=1000000000;
+        }
+    }else{
+        for(int i=0;i<n;i++){
+            a[i]=1000000000-1;
+        }
+    }
+
+    if(n>=k){
+        for(int i=0;i<k;i++){
+            a[i]=s;
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        i<n-1?printf("%d ",a[i]):printf("%d",a[i]);
+    }
+    printf("\n");
+    return 0;
+}
