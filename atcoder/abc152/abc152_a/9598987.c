2698f6904ae138583e1d15cec652a454a81ddea6
matumoto1234 <matumotosizuoka@gmail.com>
2020-01-19 20:51:34 +0900

    "add ../atcoder/abc152/abc152_a/9598987.c"

---
+++ atcoder/abc152/abc152_a/9598987.c
@@ -0,0 +1,16 @@
+// detail: https://atcoder.jp/contests/abc152/submissions/9598987
+#include<stdio.h>
+int main(void)
+{
+    int n,m;
+
+    scanf("%d %d",&n,&m);
+
+    if(n==m){
+        printf("Yes\n");
+    }
+    else{
+        printf("No\n");
+    }
+    return 0;
+}
