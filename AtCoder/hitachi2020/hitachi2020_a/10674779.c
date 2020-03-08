19afcfa0f66c4e765fb7ae9baf254ed3926ffa38
crawler <matumotosizuoka+crawler@gmail.com>
2020-03-08 12:09:37 +0000

    add ../AtCoder/hitachi2020/hitachi2020_a/10674779.c

---
+++ AtCoder/hitachi2020/hitachi2020_a/10674779.c
@@ -0,0 +1,36 @@
+// detail: https://atcoder.jp/contests/hitachi2020/submissions/10674779
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+#include<math.h>
+#include<string.h>
+#include<time.h>
+typedef long long int ll;
+int int_comp(const void*a,const void*b){return *(int*)a-*(int*)b;}
+void yes(){printf("Yes\n");}
+void no(){printf("No\n");}
+
+int main(void)
+{
+    char s[11];
+    int flg=0;
+    int cnt=0;
+
+    scanf("%s",s);
+    for(int i=0;s[i]!='\0';i++){
+        if(s[i]=='h'&&s[i+1]=='i'){
+            i++;
+        }
+        else{
+            flg=1;
+        }
+    }
+    if(flg==1)no();
+    else yes();
+    
+
+
+
+
+    return 0;
+}
