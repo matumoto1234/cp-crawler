3a95c6f65450bab010150bb7643b849ca5949974
matumoto1234 <matumotosizuoka@gmail.com>
2020-03-14 23:43:11 +0900

    "add ../atcoder/panasonic2020/panasonic2020_c/10895610.c"

---
+++ atcoder/panasonic2020/panasonic2020_c/10895610.c
@@ -0,0 +1,16 @@
+// detail: https://atcoder.jp/contests/panasonic2020/submissions/10895610
+#include<math.h>
+#include<stdio.h>
+typedef long double ld;
+int main(void)
+{
+  ld a,b,c;
+  scanf("%lf %lf %lf",&a,&b,&c);
+  if(sqrt(a)+sqrt(b)>sqrt(c)){
+    printf("Yes");
+  }
+  else {
+    printf("No");
+  }
+  return 0;
+}
