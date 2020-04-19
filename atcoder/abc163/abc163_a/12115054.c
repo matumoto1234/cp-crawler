008825fb0fd8fc4a21bb80b1844525807ec4b264
matumoto1234 <matumotosizuoka@gmail.com>
2020-04-19 21:17:00 +0900

    "add ../atcoder/abc163/abc163_a/12115054.c"

---
+++ atcoder/abc163/abc163_a/12115054.c
@@ -0,0 +1,22 @@
+// detail: https://atcoder.jp/contests/abc163/submissions/12115054
+#include<stdio.h>
+#include<iostream>
+#include<string.h>
+#include<math.h>
+#include<limits.h>
+#include<stdlib.h>
+
+typedef long long int lli;
+using namespace std;
+
+void yespr(){cout<<"Yes\n"<<endl;}
+void nopr(){cout<<"No\n"<<endl;}
+
+int main(void){
+    int n;
+
+    cin>>n;
+    printf("%.6f\n",2.0*(double)n*3.14159265);
+
+    return 0;
+}
