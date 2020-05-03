e8652dd359096691b0f058ebc8175388c1446d8a
matumoto1234 <matumotosizuoka@gmail.com>
2020-05-03 22:39:15 +0900

    "add ../atcoder/abc166/abc166_d/12775712.cpp"

---
+++ atcoder/abc166/abc166_d/12775712.cpp
@@ -0,0 +1,24 @@
+// detail: https://atcoder.jp/contests/abc166/submissions/12775712
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int lli;
+
+int main(void){
+    lli x;
+
+    cin>>x;
+
+    lli xx=100000;
+
+    for(lli i=-xx;i<=xx;i++){
+        for(lli j=-xx;j<=xx;j++){
+            lli temp=(i*i-j*j)*(i*i+j*j)*(i+j)-i*j*(i+j)*(i*i-i*j+j*j);
+            if(temp==x){
+                cout<<i<<" "<<j<<endl;
+                return 0;
+            }
+        }
+    }
+
+    return 0;
+}
