447bd24dab8b69b2479f36f676c55733dc5a1a6b
matumoto1234 <matumotosizuoka@gmail.com>
2020-05-08 23:57:57 +0900

    "add ../atcoder/joi2020yo1c/joi2020_yo1c_c/12961035.cpp"

---
+++ atcoder/joi2020yo1c/joi2020_yo1c_c/12961035.cpp
@@ -0,0 +1,26 @@
+// detail: https://atcoder.jp/contests/joi2020yo1c/submissions/12961035
+#include<bits/stdc++.h>
+using namespace std;
+int main(int argc,char* argv[]){
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    int max,r,l;
+    max=r=l=0;
+
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for(int i=1;i<n;i++){
+        if(a[i]>=a[i-1]){
+            r++;
+        }else{
+            if(max<r-l+1){
+                max=r-l+1;
+            }
+            r=l=i;
+        }
+    }
+    cout<<max<<endl;
+    return 0;
+}
