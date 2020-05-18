9a02b586b33d74a0ad8493ccc2da32d6a025b8c0
crawler <matumotosizuoka+crawler@gmail.com>
2020-05-18 00:02:05 +0000

    add ../AtCoder/abc019/abc019_1/13361445.cpp

---
+++ AtCoder/abc019/abc019_1/13361445.cpp
@@ -0,0 +1,10 @@
+// detail: https://atcoder.jp/contests/abc019/submissions/13361445
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  vector<int> a(3);
+  cin >>a[0] >>a[1] >>a[2];
+  sort(a.begin(),a.end());
+  cout <<a[1]<<endl;
+  return 0;
+}
