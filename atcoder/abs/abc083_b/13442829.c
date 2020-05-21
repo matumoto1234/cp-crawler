57cdcf7d8c5b5a4fb10e37a01819c9bf346f8561
matumoto1234 <matumotosizuoka@gmail.com>
2020-05-21 10:38:23 +0900

    "add ../atcoder/abs/abc083_b/13442829.c"

---
+++ atcoder/abs/abc083_b/13442829.c
@@ -0,0 +1,21 @@
+// detail: https://atcoder.jp/contests/abs/submissions/13442829
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int n, a, b, temp, ans = 0, sum = 0;
+  
+  cin >> n >> a >> b;
+  for(int i = 1; i <= n; i++){
+    temp = i;
+    sum = 0;
+    while(temp > 1){
+      sum += temp % 10;
+      temp /= 10;
+    }
+    if(sum >= a && sum <= b){
+      ans += i;
+    }
+  }
+  cout << ans << endl;
+  return 0;
+}
