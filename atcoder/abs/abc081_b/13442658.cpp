aca7130b37ebe124d916a6d5390cbb83dd005561
matumoto1234 <matumotosizuoka@gmail.com>
2020-05-21 10:25:13 +0900

    "add ../atcoder/abs/abc081_b/13442658.cpp"

---
+++ atcoder/abs/abc081_b/13442658.cpp
@@ -0,0 +1,22 @@
+// detail: https://atcoder.jp/contests/abs/submissions/13442658
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int n, min = INT_MAX, cnt;
+  cin >> n;
+  vector<int> a(n);
+  
+  for(int i = 0; i < n; i++){
+    cin >> a[i];
+    cnt = 0;
+    while(a[i] % 2 == 0){
+      a[i] /= 2;
+      cnt++;
+    }
+    if(cnt < min){
+      min = cnt;
+    }
+  }
+  cout << min << endl;
+  return 0;
+}
