13e80d2a9e66f8b2ad6b5cbdb30efbca6782f79f
matumoto1234 <matumotosizuoka@gmail.com>
2020-05-21 22:24:34 +0900

    "add ../atcoder/abs/abc085_c/13458082.cpp"

---
+++ atcoder/abs/abc085_c/13458082.cpp
@@ -0,0 +1,33 @@
+// detail: https://atcoder.jp/contests/abs/submissions/13458082
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main(){
+  ll a,b,c,y,n,max;
+  bool check=false;
+  cin>>n>>y;
+  max=10000*n;
+  a=n;
+  b=c=0;
+  while(max>=y&&a>-1){
+    if(max==y){
+      check=true;
+      break;
+    }
+    a--;
+    b++;
+    max-=5000;
+    if(max==y){
+      check=true;
+      break;
+    }
+    b--;
+    c++;
+    max-=4000;
+  }
+  if(!check){
+    a=b=c=-1;
+  }
+  cout<<a<<" "<<b<<" "<<c<<endl;
+  return 0;
+}
