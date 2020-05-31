a92ab10bddbf2a1d1c325f1bf2252620b5ea11c9
matumoto1234 <matumotosizuoka@gmail.com>
2020-05-31 21:09:53 +0900

    "add ../atcoder/abc169/abc169_b/13797987.c"

---
+++ atcoder/abc169/abc169_b/13797987.c
@@ -0,0 +1,24 @@
+// detail: https://atcoder.jp/contests/abc169/submissions/13797987
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int n;
+  long long int ans = 1;
+  cin >> n;
+  vector<long long int> a(n);
+
+  for (int i = 0; i < n;i++){
+    cin >> a[i];
+    if(a[i]>=1000000000&&ans>=1000000000){
+      cout << -1 << endl;
+      return 0;
+    }
+    if(a[i]*ans>1000000000000000000){
+      cout << -1 << endl;
+      return 0;
+    }
+    ans *= a[i];
+  }
+  cout << ans << endl;
+  return 0;
+}
