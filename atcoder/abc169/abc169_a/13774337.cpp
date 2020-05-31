98aa45d62b2edd08a3ab1c6133a732b94cfcf47b
matumoto1234 <matumotosizuoka@gmail.com>
2020-05-31 21:00:22 +0900

    "add ../atcoder/abc169/abc169_a/13774337.cpp"

---
+++ atcoder/abc169/abc169_a/13774337.cpp
@@ -0,0 +1,9 @@
+// detail: https://atcoder.jp/contests/abc169/submissions/13774337
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int a, b;
+  cout << a * b << endl;
+
+  return 0;
+}
