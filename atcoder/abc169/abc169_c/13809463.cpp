384ccc97fd959ef96037d764ead2a74691cbd007
matumoto1234 <matumotosizuoka@gmail.com>
2020-05-31 21:16:11 +0900

    "add ../atcoder/abc169/abc169_c/13809463.cpp"

---
+++ atcoder/abc169/abc169_c/13809463.cpp
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/abc169/submissions/13809463
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+int main() {
+  ll a;
+  long double b;
+  cin >> a >> b;
+  long double ans = a * b;
+  cout << (ll)ans  << endl;
+
+  return 0;
+}
