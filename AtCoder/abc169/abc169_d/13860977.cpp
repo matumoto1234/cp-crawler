a3a41450ec7300c5ffa4c920d227a67fa01d87cb
crawler <matumotosizuoka+crawler@gmail.com>
2020-05-31 13:07:39 +0000

    add ../AtCoder/abc169/abc169_d/13860977.cpp

---
+++ AtCoder/abc169/abc169_d/13860977.cpp
@@ -0,0 +1,29 @@
+// detail: https://atcoder.jp/contests/abc169/submissions/13860977
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+int main(){
+  ll n, cnt = 0, temp;
+  cin >> n;
+  bool sosu = true;
+  temp = n;
+  if(n==1){
+    cout << 0 << endl;
+  }
+  for (ll i = 2; i <= (ll)sqrtl(temp)+1LL; i++){
+    if(n%i==0){
+      n = n/ i;
+      cnt++;
+      sosu = false;
+      //cout << n << endl;
+    }
+
+    if(i==(ll)sqrtl(temp)+1LL&&sosu==true){
+      cout << 1 << endl;
+      return 0;
+    }
+    //cout << "i=" << i << endl;
+  }
+  cout << cnt << endl;
+  return 0;
+}
