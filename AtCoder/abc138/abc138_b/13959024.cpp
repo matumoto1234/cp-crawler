00ca1d5932e166fbb759fed540be209cd5af1b31
crawler <matumotosizuoka+crawler@gmail.com>
2020-06-02 14:22:41 +0000

    add ../AtCoder/abc138/abc138_b/13959024.cpp

---
+++ AtCoder/abc138/abc138_b/13959024.cpp
@@ -0,0 +1,16 @@
+// detail: https://atcoder.jp/contests/abc138/submissions/13959024
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  long double ans=0.0;
+  int n;
+  cin>>n;
+  vector<long double> a(n);
+  for(int i=0;i<n;i++){
+    cin>>a[i];
+    ans+=1.0/a[i];
+  }
+  cout<<1.0/ans<<endl;
+  return 0;
+}
+  
