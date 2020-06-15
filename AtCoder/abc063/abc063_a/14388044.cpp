2674cd3d4b14dbd45971819d0d203ba99b2410eb
crawler <matumotosizuoka+crawler@gmail.com>
2020-06-15 10:28:25 +0000

    add ../AtCoder/abc063/abc063_a/14388044.cpp

---
+++ AtCoder/abc063/abc063_a/14388044.cpp
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/abc063/submissions/14388044
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int a,b;
+  cin>>a>>b;
+  if(a+b>=10){
+    cout<<"error"<<endl;
+  }else{
+    cout<<a+b<<endl;
+  }
+  return 0;
+}
