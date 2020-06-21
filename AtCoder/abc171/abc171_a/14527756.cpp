5eea4069f11453ebbf50c6d066fdf5205b83566d
crawler <matumotosizuoka+crawler@gmail.com>
2020-06-21 12:01:20 +0000

    add ../AtCoder/abc171/abc171_a/14527756.cpp

---
+++ AtCoder/abc171/abc171_a/14527756.cpp
@@ -0,0 +1,14 @@
+// detail: https://atcoder.jp/contests/abc171/submissions/14527756
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  char a;
+  cin >> a;
+  if(a>='A'&&a<='Z'){
+    cout << "A" << endl;
+  }else{
+    cout << "a" << endl;
+  }
+
+  return 0;
+}
