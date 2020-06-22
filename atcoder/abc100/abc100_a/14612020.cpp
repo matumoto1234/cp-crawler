ac606c4de72c63f52bd5bd3eadb54dcd123baa0d
matumoto1234 <matumotosizuoka@gmail.com>
2020-06-22 19:02:34 +0900

    "add ../atcoder/abc100/abc100_a/14612020.cpp"

---
+++ atcoder/abc100/abc100_a/14612020.cpp
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/abc100/submissions/14612020
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int a, b;
+  cin >> a >> b;
+  if(a>=9||b>=9){
+    cout << ":(" << endl;
+  }else{
+    cout << "Yay!" << endl;
+  }
+  return 0;
+}
