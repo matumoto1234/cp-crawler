c6ede81fc83ec848370b4f44d252966889ca7d88
matumoto1234 <matumotosizuoka@gmail.com>
2020-06-22 19:13:06 +0900

    "add ../atcoder/abc100/abc100_b/14612278.cpp"

---
+++ atcoder/abc100/abc100_b/14612278.cpp
@@ -0,0 +1,9 @@
+// detail: https://atcoder.jp/contests/abc100/submissions/14612278
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int d, n;
+  cin >> d >> n;
+  cout << pow(100, d) * n << endl;
+  return 0;
+}
