7bfae9e7135974eba15fcb6a5c1354fdb291108b
crawler <matumotosizuoka+crawler@gmail.com>
2020-06-27 12:01:51 +0000

    add ../AtCoder/abc172/abc172_b/14726569.cpp

---
+++ AtCoder/abc172/abc172_b/14726569.cpp
@@ -0,0 +1,15 @@
+// detail: https://atcoder.jp/contests/abc172/submissions/14726569
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int ans = 0;
+  string s, t;
+  cin >> s >> t;
+  for (int i = 0; i < s.size();i++){
+    if(s[i]!=t[i]){
+      ans++;
+    }
+  }
+  cout << ans << endl;
+  return 0;
+}
