bbc9e8f682966906669a3d062f20c2d8677f24e7
matumoto1234 <matumotosizuoka@gmail.com>
2020-06-27 14:08:39 +0900

    "add ../atcoder/abc059/abc059_a/14709346.c"

---
+++ atcoder/abc059/abc059_a/14709346.c
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/abc059/submissions/14709346
+#include <bits/stdc++.h>
+#include <cstdlib>
+using namespace std;
+int main() {
+  string a, b, c;
+  cin >> a >> b >> c;
+  transform(a.begin(), a.end(), a.begin(), ::toupper);
+  transform(b.begin(), b.end(), b.begin(), ::toupper);
+  transform(c.begin(), c.end(), c.begin(), ::toupper);
+  cout << a[0] << b[0] << c[0] << endl;
+  return 0;
+}
