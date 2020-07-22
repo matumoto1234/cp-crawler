7fdbdc6097089da269abebed52019777442a5f31
matumoto1234 <matumotosizuoka@gmail.com>
2020-07-22 19:35:06 +0900

    "add ../atcoder/abc063/abc063_b/15360592.c"

---
+++ atcoder/abc063/abc063_b/15360592.c
@@ -0,0 +1,21 @@
+// detail: https://atcoder.jp/contests/abc063/submissions/15360592
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  string s;
+  cin >> s;
+  bool flg = false;
+  for (int i = 0; i < s.size()-1;i++){
+    for (int j = i + 1; j < s.size();j++) {
+      if(s[i]==s[j]){
+        flg = true;
+      }
+    }
+  }
+  if(!flg){
+    cout << "yes" << endl;
+  }else{
+    cout << "no" << endl;
+  }
+  return 0;
+}
