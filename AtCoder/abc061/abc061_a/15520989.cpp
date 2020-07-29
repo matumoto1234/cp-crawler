2ef78a4f6cdd785d9972a3a46e18d69089d7dc14
crawler <matumotosizuoka+crawler@gmail.com>
2020-07-29 10:31:38 +0000

    add ../AtCoder/abc061/abc061_a/15520989.cpp

---
+++ AtCoder/abc061/abc061_a/15520989.cpp
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/abc061/submissions/15520989
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int a,b,c;
+  cin >> a >> b >> c;
+  if(c>=a&&c<=b){
+    cout << "Yes" << endl;
+  }else{
+    cout << "No" << endl;
+  }
+  return 0;
+}
