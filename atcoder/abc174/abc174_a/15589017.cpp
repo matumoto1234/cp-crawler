0890ebd63d1392da6f79808faaf327124c150d54
matumoto1234 <matumotosizuoka@gmail.com>
2020-08-02 21:00:50 +0900

    "add ../atcoder/abc174/abc174_a/15589017.cpp"

---
+++ atcoder/abc174/abc174_a/15589017.cpp
@@ -0,0 +1,14 @@
+// detail: https://atcoder.jp/contests/abc174/submissions/15589017
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main() {
+  int n;
+  cin >> n;
+  if(n>=30){
+    cout << "Yes" << endl;
+  }else{
+    cout << "No" << endl;
+  }
+  return 0;
+}
