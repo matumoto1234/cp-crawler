db24862ab8d38273dabaf6a5572151931aa12a59
matumoto1234 <matumotosizuoka@gmail.com>
2020-08-03 19:32:48 +0900

    "add ../atcoder/abc116/abc116_a/15665176.cpp"

---
+++ atcoder/abc116/abc116_a/15665176.cpp
@@ -0,0 +1,9 @@
+// detail: https://atcoder.jp/contests/abc116/submissions/15665176
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int a, b, c;
+  cin >> a >> b >> c;
+  cout << a * b / 2 << endl;
+  return 0;
+}
