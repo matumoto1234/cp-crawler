4ce1d13a0b2f16b5a4a81ba415ca0aace01337a6
matumoto1234 <matumotosizuoka@gmail.com>
2020-08-08 10:10:53 +0900

    "add ../atcoder/abc160/abc160_b/15749556.cpp"

---
+++ atcoder/abc160/abc160_b/15749556.cpp
@@ -0,0 +1,15 @@
+// detail: https://atcoder.jp/contests/abc160/submissions/15749556
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main(){
+  ll n;
+  cin>>n;
+  ll ans;
+  ans=n/500;
+  n-=ans*500;
+  ans*=1000;
+  ans+=n/5*5;
+  cout<<ans<<endl;
+  return 0;
+}
