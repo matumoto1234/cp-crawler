7746064ffbbb5a74c5fa498d770238617f94f493
matumoto1234 <matumotosizuoka@gmail.com>
2020-08-10 19:32:30 +0900

    "add ../atcoder/abc113/abc113_a/15807701.cpp"

---
+++ atcoder/abc113/abc113_a/15807701.cpp
@@ -0,0 +1,9 @@
+// detail: https://atcoder.jp/contests/abc113/submissions/15807701
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int x,y;
+  cin>>x>>y;
+  cout<<x+y/2<<endl;
+  return 0;
+}
