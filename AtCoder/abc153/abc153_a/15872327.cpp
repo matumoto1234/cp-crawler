c36ac605a591554b38f0879fc2e3118604d065ab
crawler <matumotosizuoka+crawler@gmail.com>
2020-08-13 15:57:32 +0000

    add ../AtCoder/abc153/abc153_a/15872327.cpp

---
+++ AtCoder/abc153/abc153_a/15872327.cpp
@@ -0,0 +1,9 @@
+// detail: https://atcoder.jp/contests/abc153/submissions/15872327
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int a,b;
+  cin>>a>>b;
+  cout<<a/b+a%b<<endl;
+  return 0;
+}
