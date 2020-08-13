3bf3e1dd4857796b4a1cbda5ee97b587b735d870
matumoto1234 <matumotosizuoka@gmail.com>
2020-08-14 01:03:32 +0900

    "add ../atcoder/abc153/abc153_c/15872419.cpp"

---
+++ atcoder/abc153/abc153_c/15872419.cpp
@@ -0,0 +1,21 @@
+// detail: https://atcoder.jp/contests/abc153/submissions/15872419
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main(){
+  ll n,k;
+  cin>>n>>k;
+  vector<ll> a(n);
+  for(int i=0;i<n;i++){
+    cin>>a[i];
+  }
+  sort(a.rbegin(),a.rend());
+  ll ans=0;
+  for(int i=0;i<n;i++){
+    if(i>=k){
+      ans+=a[i];
+    }
+  }
+  cout<<ans<<endl;
+  return 0;
+}
