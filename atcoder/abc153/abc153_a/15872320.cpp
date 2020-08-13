a7beb88d7745465c02492cb8eb42ac9ec4408b7a
matumoto1234 <matumotosizuoka@gmail.com>
2020-08-14 00:57:10 +0900

    "add ../atcoder/abc153/abc153_a/15872320.cpp"

---
+++ atcoder/abc153/abc153_a/15872320.cpp
@@ -0,0 +1,9 @@
+// detail: https://atcoder.jp/contests/abc153/submissions/15872320
+#include<bits/stdc++.h>
+using namespac std;
+int main(){
+  int a,b;
+  cin>>a>>b;
+  cout<<a/b+a%b<<endl;
+  return 0;
+}
