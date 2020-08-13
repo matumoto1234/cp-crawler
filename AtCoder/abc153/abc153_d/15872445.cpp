5021025946d7107bfff86882f45f2a7e23ec91dd
crawler <matumotosizuoka+crawler@gmail.com>
2020-08-13 16:05:58 +0000

    add ../AtCoder/abc153/abc153_d/15872445.cpp

---
+++ AtCoder/abc153/abc153_d/15872445.cpp
@@ -0,0 +1,16 @@
+// detail: https://atcoder.jp/contests/abc153/submissions/15872445
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main(){
+  ll h;
+  cin>>h;
+  ll ans=0,cnt=0;
+  while(h>1){
+    ans+=pow(2,cnt);
+    cnt++;
+    h/=2;
+  }
+  cout<<ans<<endl;
+  return 0;
+}
