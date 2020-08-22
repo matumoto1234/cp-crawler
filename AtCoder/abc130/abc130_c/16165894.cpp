f26271bee05e5615b83d2f2244ed81dc7365cd92
crawler <matumotosizuoka+crawler@gmail.com>
2020-08-22 21:51:09 +0000

    add ../AtCoder/abc130/abc130_c/16165894.cpp

---
+++ AtCoder/abc130/abc130_c/16165894.cpp
@@ -0,0 +1,11 @@
+// detail: https://atcoder.jp/contests/abc130/submissions/16165894
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int W,H,x,y;
+  cin>>W>>H>>x>>y;
+  double s=(double)(W*H);
+  cout<<fixed<<setprecision(10);
+  cout<<s/2.0<<endl;
+  return 0;
+}
