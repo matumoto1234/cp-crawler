d9ee0d7e6d19bf4db02c21370b505de15033df47
crawler <>
2020-08-24 09:34:17 +0900

    "add ../atcoder/abc130/abc130_c/16192275.cpp"

---
+++ atcoder/abc130/abc130_c/16192275.cpp
@@ -0,0 +1,16 @@
+// detail: https://atcoder.jp/contests/abc130/submissions/16192275
+#include<bits/stdc++.h>
+using namespace std;
+using ld = long double;
+
+int main(){
+  int a,b,x,y;
+  cin>>a>>b>>x>>y;
+  cout<<(double)a*(double)b/2.0<<" ";
+  if(x+x==a&&y+y==b){
+    cout<<1<<endl;
+  }else{
+    cout<<0<<endl;
+  }
+  return 0;
+}
