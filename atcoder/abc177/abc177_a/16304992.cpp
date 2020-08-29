975524524c8e28ba3303b01d0d459d35ff3dafae
matumoto1234 <matumotosizuoka@gmail.com>
2020-08-29 21:01:15 +0900

    "add ../atcoder/abc177/abc177_a/16304992.cpp"

---
+++ atcoder/abc177/abc177_a/16304992.cpp
@@ -0,0 +1,15 @@
+// detail: https://atcoder.jp/contests/abc177/submissions/16304992
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  int a,b,c;
+  cin>>a>>b>>c;
+  if(a>b*c){
+    cout<<"No"<<endl;
+  }else{
+    cout<<"Yes"<<endl;
+  }
+  return 0;
+}
