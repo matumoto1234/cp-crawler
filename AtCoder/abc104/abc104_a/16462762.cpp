c9310ce36f51f5db30ba6c5ce482ec9a62e6fc91
crawler <matumotosizuoka+crawler@gmail.com>
2020-09-02 10:02:23 +0000

    add ../AtCoder/abc104/abc104_a/16462762.cpp

---
+++ AtCoder/abc104/abc104_a/16462762.cpp
@@ -0,0 +1,15 @@
+// detail: https://atcoder.jp/contests/abc104/submissions/16462762
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int R;
+  cin>>R;
+  if(R>=2800){
+    cout<<"AGC"<<endl;
+  }else if(R>=1200){
+    cout<<"ARC"<<endl;
+  }else{
+    cout<<"ABC"<<endl;
+  }
+  return 0;
+}
