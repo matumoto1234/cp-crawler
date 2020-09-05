1c6aa133b174ef8e3ae565d20005e7733dc63056
crawler <matumotosizuoka+crawler@gmail.com>
2020-09-05 11:57:03 +0000

    add ../AtCoder/abc040/abc040_c/16521368.cpp

---
+++ AtCoder/abc040/abc040_c/16521368.cpp
@@ -0,0 +1,23 @@
+// detail: https://atcoder.jp/contests/abc040/submissions/16521368
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int n;
+  cin>>n;
+  vector<int> a(n);
+  for(int i=0;i<n;i++){
+    cin>>a[i];
+  }
+  vector<int> dp(n+1,0);
+  dp[0]=0;
+  dp[1]=abs(a[1]-a[0]);
+  for(int i=2;i<n;i++){
+    int v1,v2;
+    v1=dp[i-1]+abs(a[i]-a[i-1]);
+    v2=dp[i-2]+abs(a[i]-a[i-2]);
+    dp[i]=min(v1,v2);
+  }
+  cout<<dp[n-1]<<endl;
+  return 0;
+}
