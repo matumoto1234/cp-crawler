7b019c26349357fdba1b3201b1d69d7453f21e07
crawler <>
2020-09-14 18:57:23 +0900

    "add ../atcoder/abc178/abc178_a/16744008.cpp"

---
+++ atcoder/abc178/abc178_a/16744008.cpp
@@ -0,0 +1,9 @@
+// detail: https://atcoder.jp/contests/abc178/submissions/16744008
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  bool flg;
+  cin>>flg;
+  cout<<!(flg)<<endl;
+  return 0;
+}
