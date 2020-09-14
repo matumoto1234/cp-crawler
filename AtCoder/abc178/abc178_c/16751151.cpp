20f856157f9cf95cae5afe2b52524cd7a78b9919
crawler <matumotosizuoka+crawler@gmail.com>
2020-09-14 15:03:43 +0000

    add ../AtCoder/abc178/abc178_c/16751151.cpp

---
+++ AtCoder/abc178/abc178_c/16751151.cpp
@@ -0,0 +1,29 @@
+// detail: https://atcoder.jp/contests/abc178/submissions/16751151
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const ll p = 1e9+7;
+
+ll llmodpow(ll a,ll e){
+  if(e==0){
+    return 1LL;
+  }
+  if(e%2==0){
+    ll ret=llmodpow(a,e/2);
+    ret%=p;
+    return (ret*ret)%p;
+  }else{
+    ll ret=a*llmodpow(a,e-1);
+    ret%=p;
+    return ret;
+  }
+}
+
+int main(){
+  ll n;
+  cin>>n;
+  ll ans;
+  ans=llmodpow(10LL,n)-2*llmodpow(9LL,n)+llmodpow(8LL,n);
+  cout<<ans<<endl;
+  return 0;
+}
