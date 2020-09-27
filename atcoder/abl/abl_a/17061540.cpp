6ffa5f5f536749a8215de36c7daee098566e9224
matumoto1234 <matumotosizuoka@gmail.com>
2020-09-27 17:09:49 +0900

    "add ../atcoder/abl/abl_a/17061540.cpp"

---
+++ atcoder/abl/abl_a/17061540.cpp
@@ -0,0 +1,12 @@
+// detail: https://atcoder.jp/contests/abl/submissions/17061540
+#include<iostream>
+using namespace std;
+int main(){
+  int k;
+  cin>>k;
+  for(int i=0;i<k;i++){
+    cout<<"ACL";
+  }
+  cout<<endl;
+  return 0;
+}
