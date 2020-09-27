f04b6424ccedfc31fddb3868ddb386f159d619a3
matumoto1234 <matumotosizuoka@gmail.com>
2020-09-27 17:20:15 +0900

    "add ../atcoder/abl/abl_b/17061712.cpp"

---
+++ atcoder/abl/abl_b/17061712.cpp
@@ -0,0 +1,14 @@
+// detail: https://atcoder.jp/contests/abl/submissions/17061712
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main(){
+  ll a,b,c,d;
+  cin>>a>>b>>c>>d;
+  if((a<=c&&c<=b)||(c<=b&&b<=d)){
+    cout<<"Yes"<<endl;
+  }else{
+    cout<<"No"<<endl;
+  }
+  return 0;
+}
