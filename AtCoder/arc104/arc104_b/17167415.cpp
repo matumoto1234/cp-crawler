e4f6dc20ed998f3028c6c061ae3e1325f3267d18
crawler <matumotosizuoka+crawler@gmail.com>
2020-10-03 12:46:09 +0000

    add ../AtCoder/arc104/arc104_b/17167415.cpp

---
+++ AtCoder/arc104/arc104_b/17167415.cpp
@@ -0,0 +1,43 @@
+// detail: https://atcoder.jp/contests/arc104/submissions/17167415
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+bool isTrue(char l,char r){
+  if(l>r){
+    char c=l;
+    l=r;
+    r=c;
+  }
+  if(l=='A'&&r=='T'){
+    return true;
+  }else if(l=='C'&&r=='G'){
+    return true;
+  }
+  return false;
+}
+
+ll solve(int n,string s,int l,int r){
+  ll sum=0;
+  if(l<0||r>=n){
+    return 0LL;
+  }
+  if(isTrue(s[l],s[r])==false){
+    return 0LL;
+  }
+  sum++;
+  return sum+solve(n,s,l-1,r+1);
+}
+
+int main() {
+  int n;
+  string s;
+  cin>>n>>s;
+
+  ll ans=0;
+  for(int i=0;i<n;i++){
+    ans+=solve(n,s,i,i+1);
+  }
+  cout<<ans<<endl;
+  return 0;
+}
