68b1ef398c4c01f1643d059c7a7e894f58491389
crawler <>
2020-10-11 22:44:57 +0900

    "add ../atcoder/arc105/arc105_b/17341796.cpp"

---
+++ atcoder/arc105/arc105_b/17341796.cpp
@@ -0,0 +1,25 @@
+// detail: https://atcoder.jp/contests/arc105/submissions/17341796
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+
+int main()
+{
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+
+  ll n;
+  cin >> n;
+  vector<ll> a(n);
+  ll minv;
+  minv=1e9+1;
+  for(int i=0;i<n;i++){
+    cin>>a[i];
+    if(i){
+      minv = min(minv, __gcd(a[i-1],a[i]));
+    }
+  }
+  cout<<minv<<endl;
+  return 0;
+}
