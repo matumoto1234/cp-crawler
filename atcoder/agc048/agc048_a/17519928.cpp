27e53a18479a311acefd5660d6722345b0a3d2d9
crawler <>
2020-10-19 10:12:53 +0900

    "add ../atcoder/agc048/agc048_a/17519928.cpp"

---
+++ atcoder/agc048/agc048_a/17519928.cpp
@@ -0,0 +1,29 @@
+// detail: https://atcoder.jp/contests/agc048/submissions/17519928
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  cin.tie(0);
+  ios_base::sync_with_stdio(false);
+  int t;
+  cin>>t;
+  string atcoder="atcoder";
+  for(int i=0;i<t;i++){
+    string s;
+    cin>>s;
+    if(s.size()<atcoder.size()){
+      cout<<-1<<"\n";
+      continue;
+    }
+    bool flg=false;
+    int counter=0;
+    for(int j=0;j<min(atcoder.size(),s.size());j++){
+      if(s[j]!='a'){
+        flg=true;
+        break;
+      }
+      counter++;
+    }
+    cout<<(flg?counter?1:0:-1)<<'\n';
+  }
+  return 0;
+}
