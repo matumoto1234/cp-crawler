ec9f91284b0275f7bfb032c8463fb73a60074632
crawler <>
2020-10-24 11:19:11 +0900

    "add ../atcoder/abc174/abc174_a/17597186.cpp"

---
+++ atcoder/abc174/abc174_a/17597186.cpp
@@ -0,0 +1,12 @@
+// detail: https://atcoder.jp/contests/abc174/submissions/17597186
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+  int n;
+  cin >> n;
+  if(n>=30)cout << "Yes"
+  else cout << "No"
+  return 0;
+}
