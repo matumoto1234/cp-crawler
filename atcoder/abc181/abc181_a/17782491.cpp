b5fe60f010a95b4ccfeafbe6def86c8716bda2b5
matumoto1234 <matumotosizuoka@gmail.com>
2020-11-01 21:01:38 +0900

    "add ../atcoder/abc181/abc181_a/17782491.cpp"

---
+++ atcoder/abc181/abc181_a/17782491.cpp
@@ -0,0 +1,10 @@
+// detail: https://atcoder.jp/contests/abc181/submissions/17782491
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main(){
+  ll n;
+  cin>>n;
+  cout << (n % 2 ==0? "White" : "Black") << endl;
+  return 0;
+}
