2fb72f25ade8a90c84c1034796373291fd18a275
matumoto1234 <matumotosizuoka@gmail.com>
2020-11-08 21:04:45 +0900

    "add ../atcoder/abc182/abc182_a/17954505.cpp"

---
+++ atcoder/abc182/abc182_a/17954505.cpp
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/abc182/submissions/17954505
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+using namespace std;
+using ll = long long;
+int main(){
+  ll a,b; 
+  cin>>a>>b;
+  cout<<2*a+100-b<<endl;
+  return 0;
+}
