40e59d8b2c397b252833e6a819092dfd1a75a5d6
matumoto1234 <matumotosizuoka@gmail.com>
2020-11-08 20:58:05 +0900

    "add ../atcoder/agc014/agc014_a/17948123.cpp"

---
+++ atcoder/agc014/agc014_a/17948123.cpp
@@ -0,0 +1,31 @@
+// detail: https://atcoder.jp/contests/agc014/submissions/17948123
+#include<iostream>
+using namespace std;
+int main(){
+  int a,b,c;
+  cin>>a>>b>>c;
+  int ans=0;
+  if(a==b&&a==c){
+    if(a%2||b%2||c%2){
+      cout<<0<<endl;
+      return 0;
+    }
+
+    cout<<-1<<endl;
+    return 0;
+  }
+  while(1){
+    if(a%2||b%2||c%2){
+      break;
+    }
+    int temp=a;
+    int temp2=b;
+    int temp3=c;
+    a=temp2/2+temp3/2;
+    b=temp/2+temp3/2;
+    c=temp/2+temp2/2;
+    ans++;
+  }
+  cout<<ans<<endl;
+  return 0;
+}
