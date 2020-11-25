135640bf2aca13766f0691a6303a9300ac53edf4
crawler <matumotosizuoka+crawler@gmail.com>
2020-11-25 11:45:13 +0000

    add ../AtCoder/dp/dp_d/18403973.cpp

---
+++ AtCoder/dp/dp_d/18403973.cpp
@@ -0,0 +1,44 @@
+// detail: https://atcoder.jp/contests/dp/submissions/18403973
+#include<iostream>
+#include<vector>
+using namespace std;
+using ll = long long;
+const int MAXN=100;
+const int MAXW=100000;
+
+int N,W;
+int w[MAXN];
+ll v[MAXN];
+
+vector<ll> done(MAXW+1,0LL);
+ll calc(int weight,vector<bool>& flg){
+  if(weight==0){
+    return 0LL;
+  }
+
+  ll &maxv=done[weight];
+  if(maxv!=0LL){
+    return maxv;
+  }
+
+  for(int i=0;i<N;i++){
+    if(flg[i]) continue;
+    if(weight-w[i]>=0){
+      flg[i]=true;
+      maxv=max(maxv,calc(weight-w[i],flg)+v[i]);
+      flg[i]=false;
+    }
+  }
+  return maxv;
+}
+
+int main(){
+  cin>>N>>W;
+  for(int i=0;i<N;i++){
+    cin>>w[i]>>v[i];
+  }
+  vector<bool> flg(N,false);
+  cout<<calc(W,flg)<<endl;
+  
+  return 0;
+}
