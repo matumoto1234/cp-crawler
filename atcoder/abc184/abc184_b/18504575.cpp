5fed28d463d8c2acbbe7f3f540328e25cef8be03
matumoto1234 <matumotosizuoka@gmail.com>
2020-11-30 22:31:19 +0900

    "add ../atcoder/abc184/abc184_b/18504575.cpp"

---
+++ atcoder/abc184/abc184_b/18504575.cpp
@@ -0,0 +1,26 @@
+// detail: https://atcoder.jp/contests/abc184/submissions/18504575
+#include <algorithm>
+#include <iostream>
+#include<string>
+#include <vector>
+using namespace std;
+using ll = long long;
+
+int main() {
+  ll n,x;
+  string s;
+  cin>>n>>x>>s;
+  ll ans=0;
+  for(int i=0;i<n;i++){
+    if(s[i]=='o'){
+      ans++;
+    }else{
+      ans--;
+      if(ans<0){
+        ans=0;
+      }
+    }
+  }
+  cout<<ans<<endl;
+  return 0;
+}
