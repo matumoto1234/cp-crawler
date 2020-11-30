1e84b50fb498be94688491571435f178af83d805
matumoto1234 <matumotosizuoka@gmail.com>
2020-11-30 22:37:29 +0900

    "add ../atcoder/abc020/abc020_b/18504693.c"

---
+++ atcoder/abc020/abc020_b/18504693.c
@@ -0,0 +1,11 @@
+// detail: https://atcoder.jp/contests/abc020/submissions/18504693
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+  string a,b;
+  cin>>a>>b;
+  a+=b;
+  cout<<stoi(a)*2<<endl;
+  return 0;
+}
