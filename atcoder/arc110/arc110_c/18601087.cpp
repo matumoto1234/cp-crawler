f765760feb2f35f6f8225e1946a1b73afa462d56
crawler <>
2020-12-06 00:24:26 +0900

    "add ../atcoder/arc110/arc110_c/18601087.cpp"

---
+++ atcoder/arc110/arc110_c/18601087.cpp
@@ -0,0 +1,60 @@
+// detail: https://atcoder.jp/contests/arc110/submissions/18601087
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+void output(vector<int>& v){
+  int n=v.size();
+  for(int i=0;i<n;i++){
+    cout<<v[i]<<' ';
+  }
+  cout<<endl;
+}
+
+int main(){
+  int n;
+  cin>>n;
+  vector<int> a(n),sorted(n);
+  vector<int> id(n);
+  for(int i=0;i<n;i++){
+    cin>>a[i];
+    a[i]--;
+    sorted[i]=a[i];
+    id[a[i]]=i;
+  }
+  sort(sorted.begin(),sorted.end());
+  vector<bool> done(n,false);
+  vector<int> ans;
+  for(int i=0;i<n;i++){
+    if(a[i]==i){
+      continue;
+    }
+    for(int j=id[i];j>i;j--){
+      if(done[j-1]){
+        break;
+      }
+      done[j-1]=true;
+      ans.push_back(j-1);
+      id[a[j-1]]++;
+      id[a[j]]--;
+      swap(a[j-1],a[j]);
+    }
+  }
+  //output(a);
+  bool flg=true;
+  for(int i=0;i<n;i++){
+    if(sorted[i]!=a[i]){
+      flg=false;
+      break;
+    }
+  }
+  if(flg){
+    for(int x:ans){
+      cout<<x+1<<endl;
+    }
+  }else{
+    cout<<-1<<endl;
+  }
+  return 0;
+}
