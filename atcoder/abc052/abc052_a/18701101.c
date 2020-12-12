e1f2a0bf2dec11d5a6e9cc64573fa690e2a0d089
matumoto1234 <matumotosizuoka@gmail.com>
2020-12-12 21:56:57 +0900

    "add ../atcoder/abc052/abc052_a/18701101.c"

---
+++ atcoder/abc052/abc052_a/18701101.c
@@ -0,0 +1,10 @@
+// detail: https://atcoder.jp/contests/abc052/submissions/18701101
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+  int a,b,c,d;
+  cin>>a>>b>>c>>d;
+  cout<<max(a*b,c*d)<<endl;
+  return 0;
+}
