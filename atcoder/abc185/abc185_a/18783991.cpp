0aa6cf1010ca924fedb93102a942dbeefae47bf5
matumoto1234 <matumotosizuoka@gmail.com>
2020-12-14 22:47:45 +0900

    "add ../atcoder/abc185/abc185_a/18783991.cpp"

---
+++ atcoder/abc185/abc185_a/18783991.cpp
@@ -0,0 +1,11 @@
+// detail: https://atcoder.jp/contests/abc185/submissions/18783991
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+  int a,b,c,d;
+  cin>>a>>b>>c>>d;
+  cout<<min({a,b,c,d})<<endl;
+  return 0;
+}
