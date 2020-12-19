5ba2b30e6638c5d040ab5b1bffc7a9eef83b6c02
matumoto1234 <matumotosizuoka@gmail.com>
2020-12-19 21:03:20 +0900

    "add ../atcoder/abc186/abc186_b/18864562.cpp"

---
+++ atcoder/abc186/abc186_b/18864562.cpp
@@ -0,0 +1,22 @@
+// detail: https://atcoder.jp/contests/abc186/submissions/18864562
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+using ll = long long;
+int main() {
+  ll h,w;
+  cin >> h>>w;
+  ll n=h*w;
+  vector<ll> a(n);
+  for(int i=0;i<n;i++){
+    cin>>a[i];
+  }
+  sort(a.begin(),a.end());
+  ll ans=0;
+  for(int i=1;i<n;i++){
+    ans+=a[i]-a[0];
+  }
+  cout<<ans<<endl;
+  return 0;
+}
