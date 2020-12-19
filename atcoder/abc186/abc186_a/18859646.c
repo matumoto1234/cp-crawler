8ec277ae89cad371f0eaa663c0f46b17a54705f6
matumoto1234 <matumotosizuoka@gmail.com>
2020-12-19 21:00:42 +0900

    "add ../atcoder/abc186/abc186_a/18859646.c"

---
+++ atcoder/abc186/abc186_a/18859646.c
@@ -0,0 +1,12 @@
+// detail: https://atcoder.jp/contests/abc186/submissions/18859646
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+using ll = long long;
+int main() {
+  ll n,w;
+  cin >> n>>w;
+  cout<<n/w<<endl;
+  return 0;
+}
