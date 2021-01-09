d696d81f19ba23bfe9a713638460ae764be5b32d
matumoto1234 <matumotosizuoka@gmail.com>
2021-01-09 21:52:29 +0900

    "add ../atcoder/arc111/arc111_b/19285513.c"

---
+++ atcoder/arc111/arc111_b/19285513.c
@@ -0,0 +1,28 @@
+// detail: https://atcoder.jp/contests/arc111/submissions/19285513
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+const ll MAXV = 400000;
+
+int main() {
+  ll n;
+  cin >> n;
+  using P = pair<ll,ll>;
+  vector<P> a(n);
+  ll ans=0;
+  vector<bool> used(MAXV+1,false);
+  for(int i=0;i<n;i++){
+    cin>>a[i].first>>a[i].second;
+    if(used[a[i].first]==false){
+      used[a[i].first]=true;
+      ans++;
+    }
+    if(used[a[i].second]==false){
+      used[a[i].second]=true;
+      ans++;
+    }
+  }
+
+  cout<<min(ans,n)<<endl;
+  return 0;
+}
