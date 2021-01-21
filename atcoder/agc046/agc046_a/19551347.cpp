6c18752811f267f4180ef3469624def37162f757
matumoto1234 <matumotosizuoka@gmail.com>
2021-01-21 21:08:02 +0900

    "add ../atcoder/agc046/agc046_a/19551347.cpp"

---
+++ atcoder/agc046/agc046_a/19551347.cpp
@@ -0,0 +1,8 @@
+// detail: https://atcoder.jp/contests/agc046/submissions/19551347
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int x;
+  cin>>x;
+  cout<<360/__gcd(360,x)<<endl;
+}
