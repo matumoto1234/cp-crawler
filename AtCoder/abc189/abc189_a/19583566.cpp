49b4ae75646864de42a16b681c160a364e4f28a7
crawler <matumotosizuoka+crawler@gmail.com>
2021-01-23 12:01:00 +0000

    add ../AtCoder/abc189/abc189_a/19583566.cpp

---
+++ AtCoder/abc189/abc189_a/19583566.cpp
@@ -0,0 +1,10 @@
+// detail: https://atcoder.jp/contests/abc189/submissions/19583566
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+int main(){
+  string s;
+  cin>>s;
+  cout<<(s[0]==s[1]&&s[0]==s[2]?"Won":"Lost")<<endl;
+}
