b7b53d4d1195dd0b7719ab84e34e1e2e8073c07e
crawler <matumotosizuoka+crawler@gmail.com>
2021-01-27 13:03:06 +0000

    add ../AtCoder/abc099/abc099_b/19724307.cpp

---
+++ AtCoder/abc099/abc099_b/19724307.cpp
@@ -0,0 +1,12 @@
+// detail: https://atcoder.jp/contests/abc099/submissions/19724307
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+int main(){
+  ll a,b;
+  cin>>a>>b;
+  ll idx=b-a;
+  ll ans=0;
+  for(ll i=1;i<=idx;i++) ans+=i;
+  cout<<ans<<endl;
+}
