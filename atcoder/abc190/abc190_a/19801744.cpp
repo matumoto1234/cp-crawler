a0d755f91e84c4c98c34cca8b88ed571618d656e
matumoto1234 <matumotosizuoka@gmail.com>
2021-01-30 21:40:58 +0900

    "add ../atcoder/abc190/abc190_a/19801744.cpp"

---
+++ atcoder/abc190/abc190_a/19801744.cpp
@@ -0,0 +1,30 @@
+// detail: https://atcoder.jp/contests/abc190/submissions/19801744
+#include<bits/stdc++.h>
+using namespace std;
+
+const int AWIN = 2;
+const int TWIN = 1;
+
+int solve(int a,int b){
+  while(1){
+    //cerr<<"a:"<<a<<" b:"<<b<<endl;
+    if(a<=0) return AWIN;
+    a--;
+    if(b<=0) return TWIN;
+    b--;
+  }
+}
+
+int main(){
+  int a,b,c;
+  cin>>a>>b>>c;
+  if(c==1&&b==0){
+    cout<<"Takahashi"<<endl;
+    return 0;
+  }
+  if(c==0){
+    cout<<(solve(a,b)==AWIN?"Aoki":"Takahashi")<<endl;
+  }else{
+    cout<<(solve(a,b-1)==AWIN?"Aoki":"Takahashi")<<endl;
+  }
+}
