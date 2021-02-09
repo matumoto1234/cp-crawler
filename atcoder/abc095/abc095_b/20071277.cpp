cc4bbfeb80e891bd36239d05a3bf809a806ff496
matumoto1234 <matumotosizuoka@gmail.com>
2021-02-09 21:14:27 +0900

    "add ../atcoder/abc095/abc095_b/20071277.cpp"

---
+++ atcoder/abc095/abc095_b/20071277.cpp
@@ -0,0 +1,27 @@
+// detail: https://atcoder.jp/contests/abc095/submissions/20071277
+#include <bits/stdc++.h>
+using namespace std;
+#define range(i, l, r) for (int i = (int)(l); i < (int)(r); (i) += 1)
+#define rrange(i, l, r) for (int i = (int)(r)-1; i >= (int)(l); (i) -= 1)
+template <typename T1, typename T2> inline void chmax(T1 &a, T2 b) { a = (a > b ? a : b); }
+template <typename T1, typename T2> inline void chmin(T1 &a, T2 b) { a = (a < b ? a : b); }
+template <typename T> ostream &operator<<(ostream &os,const vector<T> &v) { range(i,0,v.size()) {os<<(i?" ":"")<<v[i];} return os;}
+using ll = long long;
+using LP = pair<ll, ll>;
+using P = pair<int, int>;
+const ll INF64 = INT64_MAX / 2;
+const int INF32 = INT32_MAX / 2;
+
+int main() {
+  int n,x;
+  cin>>n>>x;
+  vector<int> m(n);
+  int minv=INF32;
+  range(i,0,n){
+    cin>>m[i];
+    x-=m[i];
+    chmin(minv,m[i]);
+  }
+  int ans=x/minv;
+  cout<<ans+n<<endl;
+}
