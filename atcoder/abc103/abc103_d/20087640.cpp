08fb3970f5a2484c6331e5f962fd309df90803e1
matumoto1234 <matumotosizuoka@gmail.com>
2021-02-10 20:48:00 +0900

    "add ../atcoder/abc103/abc103_d/20087640.cpp"

---
+++ atcoder/abc103/abc103_d/20087640.cpp
@@ -0,0 +1,42 @@
+// detail: https://atcoder.jp/contests/abc103/submissions/20087640
+#include <bits/stdc++.h>
+using namespace std;
+#define range(i, l, r) for (int i = (int)(l); i < (int)(r); (i) += 1)
+#define rrange(i, l, r) for (int i = (int)(r)-1; i >= (int)(l); (i) -= 1)
+template <typename T1, typename T2> inline void chmax(T1 &a, T2 b) { a = (a > b ? a : b); }
+template <typename T1, typename T2> inline void chmin(T1 &a, T2 b) { a = (a < b ? a : b); }
+template <typename T> ostream &operator<<(ostream &os,const vector<T> &v) { range(i,0,v.size()) {os<<(i?" ":"")<<v[i];} return os;}
+using ll = long long;
+using LP = pair<ll, ll>;
+using P = pair<int, int>;
+const ll INF64 = INT64_MAX / 2;
+const int INF32 = INT32_MAX / 2;
+
+const int MAXN = 100000;
+
+int main() {
+  int n,m;
+  cin>>n>>m;
+  vector<vector<int>> v(n);
+  range(i,0,m){
+    int a,b;
+    cin>>a>>b;
+    a--;
+    b--;
+    v[b].push_back(a);
+  }
+
+  int ans=0;
+  int reset_pos=-1;
+  range(i,0,n){
+    if(v[i].size()==0) continue;
+    for(int a:v[i]){
+      if(reset_pos<=a){
+        reset_pos=i;
+        ans++;
+      }
+    }
+    //cerr<<"i:"<<i<<" reset_pos:"<<reset_pos<<endl;
+  }
+  cout<<ans<<endl;
+}
