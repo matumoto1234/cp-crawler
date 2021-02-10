1f6b4342c69a05c76b0baf49be280e12404b0756
crawler <matumotosizuoka+crawler@gmail.com>
2021-02-10 10:32:35 +0000

    add ../AtCoder/abc103/abc103_b/20086465.cpp

---
+++ AtCoder/abc103/abc103_b/20086465.cpp
@@ -0,0 +1,28 @@
+// detail: https://atcoder.jp/contests/abc103/submissions/20086465
+#include <bits/stdc++.h>
+using namespace std;
+#define range(i, l, r) for (int i = (int)(l); i < (int)(r); (i) += 1)
+#define rrange(i, l, r) for (int i = (int)(r)-1; i >= (int)(l); (i) -= 1)
+template <typename T1, typename T2> inline void chmax(T1 &a, T2 b) { a = (a > b ? a : b); }
+template <typename T1, typename T2> inline void chmin(T1 &a, T2 b) { a = (a < b ? a : b); }
+template <typename T> ostream &operator<<(ostream &os,const vector<T> &v) { range(i,0,v.size()) {os<<(i?" ":"")<<v[i];} return os;}
+using ll = long long;
+using LP = pair<ll, ll>;
+using P = pair<int, int>;
+const ll INF64 = INT64_MAX / 2;
+const int INF32 = INT32_MAX / 2;
+
+int main() {
+  string s,t;
+  cin>>s>>t;
+  range(i,0,s.size()){
+    rotate(s.begin(),s.begin()+1,s.end());
+    if(s==t){
+      cout<<"Yes"<<endl;
+      return 0;
+    }
+    //cout<<s<<endl;
+  }
+  cout<<"No"<<endl;
+  return 0;
+}
