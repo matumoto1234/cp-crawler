9691215ebb5381396b703d410845286ee75e8259
crawler <matumotosizuoka+crawler@gmail.com>
2021-02-13 12:25:57 +0000

    add ../AtCoder/arc112/arc112_a/20147329.cpp

---
+++ AtCoder/arc112/arc112_a/20147329.cpp
@@ -0,0 +1,36 @@
+// detail: https://atcoder.jp/contests/arc112/submissions/20147329
+#include <bits/stdc++.h>
+using namespace std;
+#define range(i, l, r) for (int i = (int)(l); i < (int)(r); (i) += 1)
+#define rrange(i, l, r) for (int i = (int)(r)-1; i >= (int)(l); (i) -= 1)
+template <typename T1, typename T2> inline void chmax(T1 &a, T2 b) { a = (a > b ? a : b); }
+template <typename T1, typename T2> inline void chmin(T1 &a, T2 b) { a = (a < b ? a : b); }
+template <typename T> ostream &operator<<(ostream &os,const vector<T> &v) { range(i,0,v.size()) {os<<(i?" ":"")<<v[i];} return os;}
+using ll = long long;
+using LP = pair<ll, ll>;
+using P = pair<int, int>;
+const ll INF64 = INT64_MAX / 2;
+const int INF32 = INT32_MAX / 2;
+
+int main() {
+  cin.tie(0);
+  ios_base::sync_with_stdio(false);
+
+  int t;
+  cin>>t;
+  range(i,0,t){
+    ll l,r;
+    cin>>l>>r;
+    if(l==r){
+      if(l==0){
+        cout<<1<<'\n';
+      }else{
+        cout<<0<<'\n';
+      }
+      continue;
+    }
+
+    ll n=r-l-l+1;
+    cout<<(n+1)*n/2<<'\n';
+  }
+}
