c0435956d0d0272908622604378f189407f358e0
crawler <matumotosizuoka+crawler@gmail.com>
2021-02-17 07:38:06 +0000

    add ../AtCoder/abc108/abc108_b/20225992.cpp

---
+++ AtCoder/abc108/abc108_b/20225992.cpp
@@ -0,0 +1,29 @@
+// detail: https://atcoder.jp/contests/abc108/submissions/20225992
+#include <bits/stdc++.h>
+using namespace std;
+#define range(i, l, r) for (int i = (int)(l); i < (int)(r); (i) += 1)
+#define rrange(i, l, r) for (int i = (int)(r)-1; i >= (int)(l); (i) -= 1)
+template <typename T1, typename T2> inline void chmax(T1 &a, T2 b) { a = (a > b ? a : b); }
+template <typename T1, typename T2> inline void chmin(T1 &a, T2 b) { a = (a < b ? a : b); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os,const pair<T1,T2> &p) { os<<p.first<<' '<<p.second<<'\n'; return os;}
+template <typename T> ostream &operator<<(ostream &os,const vector<T> &v) { range(i,0,v.size()) {os<<(i?" ":"")<<v[i];} return os;}
+using ll = long long;
+using PL = pair<ll, ll>;
+using P = pair<int, int>;
+const ll INF64 = INT64_MAX / 2;
+const int INF32 = INT32_MAX / 2;
+const char newl = '\n';
+
+int main() {
+  int x1,y1,x2,y2;
+  cin>>x1>>y1>>x2>>y2;
+  int dx=x2-x1;
+  int dy=y2-y1;
+
+  int x3=x2-dy;
+  int y3=y2+dx;
+
+  int x4=x3-dx;
+  int y4=y3-dy;
+  cout<<x3<<' '<<y3<<' '<<x4<<' '<<y4<<endl;
+}
