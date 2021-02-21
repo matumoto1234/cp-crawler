706482596a72eaccd8132cea591ea3bd282385a2
crawler <matumotosizuoka+crawler@gmail.com>
2021-02-21 12:14:16 +0000

    add ../AtCoder/arc113/arc113_b/20378109.cpp

---
+++ AtCoder/arc113/arc113_b/20378109.cpp
@@ -0,0 +1,34 @@
+// detail: https://atcoder.jp/contests/arc113/submissions/20378109
+#include <bits/stdc++.h>
+using namespace std;
+#define range(i, l, r) for (int i = (int)(l); i < (int)(r); (i) += 1)
+#define rrange(i, l, r) for (int i = (int)(r)-1; i >= (int)(l); (i) -= 1)
+template <typename T1, typename T2> inline void chmax(T1 &a, T2 b) { a = (a > b ? a : b); }
+template <typename T1, typename T2> inline void chmin(T1 &a, T2 b) { a = (a < b ? a : b); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os,const pair<T1,T2> &p) { os<<p.first<<' '<<p.second<<'\n'; return os;}
+template <typename T> ostream &operator<<(ostream &os,const vector<T> &v) { range(i,0,v.size()) {os<<(i?" ":"")<<v[i];} return os;}
+using ll = long long;
+using PL = pair<ll, ll>;
+using P = pair<int, int>;
+const ll INF64 = INT64_MAX / 2;
+const int INF32 = INT32_MAX / 2;
+const char newl = '\n';
+
+ll modPow(ll a,ll e,ll p){
+  if(e==0) return 1;
+  if(e%2==0){
+    ll res=modPow(a,e/2,p);
+    return (res*res)%p;
+  }else{
+    ll res=modPow(a,e-1,p);
+    res=(res*a)%p;
+    return res;
+  }
+}
+
+int main() {
+  ll a,b,c;
+  cin>>a>>b>>c;
+  ll p=1e18;
+  cout<<modPow(a,modPow(b,c,p),p)%10<<endl;
+}
