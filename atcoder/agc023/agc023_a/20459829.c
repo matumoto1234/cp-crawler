fdb493b4be52f21fd4e2f7470d7e931695307c16
matumoto1234 <matumotosizuoka@gmail.com>
2021-02-24 16:23:47 +0900

    "add ../atcoder/agc023/agc023_a/20459829.c"

---
+++ atcoder/agc023/agc023_a/20459829.c
@@ -0,0 +1,47 @@
+// detail: https://atcoder.jp/contests/agc023/submissions/20459829
+#include <bits/stdc++.h>
+using namespace std;
+#define range(i, l, r) for (int i = (int)(l); i < (int)(r); (i) += 1)
+#define rrange(i, l, r) for (int i = (int)(r)-1; i >= (int)(l); (i) -= 1)
+template <typename T1, typename T2> inline void chmax(T1 &a, T2 b) { a = (a > b ? a : b); }
+template <typename T1, typename T2> inline void chmin(T1 &a, T2 b) { a = (a < b ? a : b); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os,const pair<T1,T2> &p) { os<<p.first<<' '<<p.second<<'\n'; return os;}
+template <typename T> ostream &operator<<(ostream &os,const vector<T> &v) { range(i,0,v.size()) {os<<(i?" ":"")<<v[i];} return os;}
+using ll = long long;
+using PL = pair<ll, ll>;
+using P = pair<int, int>;
+const ll INF64 = INT64_MAX / 2;
+const int INF32 = INT32_MAX / 2;
+const char newl = '\n';
+
+
+int main() {
+  ll n;
+  cin>>n;
+  
+  vector<ll> a(n);
+  range(i,0,n){
+    cin>>a[i];
+  }
+
+  vector<ll> accum(n+1,0);
+  range(i,0,n) accum[i+1]+=accum[i]+a[i];
+
+  //cerr<<accum<<endl;
+
+  map<ll,ll> mp;
+  range(i,1,n+1){
+    mp[accum[i]]++;
+  }
+
+  ll ans=0;
+  for(auto [v,cnt]:mp){
+    //cerr<<"v:"<<v<<" cnt:"<<cnt<<endl;
+    if(v==0){
+      ans+=cnt*(cnt+1)/2;
+      continue;
+    }
+    ans+=cnt*(cnt-1)/2;
+  }
+  cout<<ans<<endl;
+}
