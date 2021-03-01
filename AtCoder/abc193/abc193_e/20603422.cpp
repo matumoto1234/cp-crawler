2259b5b192e5695c463ceabc5ab467e5cf7a6bdc
crawler <matumotosizuoka+crawler@gmail.com>
2021-03-01 23:29:54 +0000

    add ../AtCoder/abc193/abc193_e/20603422.cpp

---
+++ AtCoder/abc193/abc193_e/20603422.cpp
@@ -0,0 +1,57 @@
+// detail: https://atcoder.jp/contests/abc193/submissions/20603422
+#include <bits/stdc++.h>
+using namespace std;
+#define range(i, l, r) for (long long i = (int)(l); i < (int)(r); (i) += 1)
+#define rrange(i, l, r) for (long long i = (int)(r)-1; i >= (int)(l); (i) -= 1)
+template <typename T1, typename T2> inline void chmax(T1 &a, T2 b) { a = (a > b ? a : b); }
+template <typename T1, typename T2> inline void chmin(T1 &a, T2 b) { a = (a < b ? a : b); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os,const pair<T1,T2> &p) { os<<p.first<<' '<<p.second<<'\n'; return os;}
+template <typename T> ostream &operator<<(ostream &os,const vector<T> &v) { range(i,0,v.size()) {os<<(i?" ":"")<<v[i];} return os;}
+using ll = long long;
+using PL = pair<ll, ll>;
+using P = pair<int, int>;
+const ll INF64 = INT64_MAX / 2;
+const int INF32 = INT32_MAX / 2;
+const char newl = '\n';
+
+template <typename T>
+T extgcd(T a,T b,T &x,T &y){
+  if(b==0){
+    x=1;
+    y=0;
+    return a;
+  }
+  T d=extgcd(b,a%b,y,x);
+  y=y-(a/b)*x;
+  return d;
+}
+
+// a%b
+int mod(int a,int b){
+  return (a%b+b)%b;
+}
+
+int main() {
+  int t;
+  cin>>t;
+  range(i,0,t){
+    ll x,y,p,q;
+    cin>>x>>y>>p>>q;
+
+    ll c1=2*x+2*y;
+    ll c2=p+q;
+    ll d=gcd(c1,c2);
+
+    ll ans=INF64;
+    range(t1,x,x+y) range(t2,p,p+q){
+      if(mod(t2-t1,d)!=0) continue;
+      ll x,y;
+      extgcd(c1,c2,x,y);
+      x*=(t2-t1)/d;
+      //y*=(t2-t1)/d;
+      chmin(ans,t1+c1*x);
+    }
+    if(ans==INF64) cout<<"infinity"<<newl;
+    else cout<<ans<<newl;
+  }
+}
