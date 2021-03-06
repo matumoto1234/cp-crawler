0f58f59a1ac8e066474bab3aa3bd9b89e65e46c5
crawler <matumotosizuoka+crawler@gmail.com>
2021-03-06 04:48:32 +0000

    add ../AtCoder/pakencamp-2020-day1/pakencamp_2020_day1_c/20678714.cpp

---
+++ AtCoder/pakencamp-2020-day1/pakencamp_2020_day1_c/20678714.cpp
@@ -0,0 +1,38 @@
+// detail: https://atcoder.jp/contests/pakencamp-2020-day1/submissions/20678714
+#include <bits/stdc++.h>
+using namespace std;
+#define range(i, l, r) for (int i = (int)(l); i < (int)(r); (i) += 1)
+#define rrange(i, l, r) for (int i = (int)(r)-1; i >= (int)(l); (i) -= 1)
+template <typename T1, typename T2> inline void chmax(T1 &a, T2 b) { a = (a > b ? a : b); }
+template <typename T1, typename T2> inline void chmin(T1 &a, T2 b) { a = (a < b ? a : b); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os,const pair<T1,T2> &p) { os<<p.first<<' '<<p.second; return os;}
+template <typename T> ostream &operator<<(ostream &os,const vector<T> &v) { range(i,0,v.size()) {os<<(i?" ":"")<<v[i];} return os;}
+using ull = unsigned long long;
+using ll = long long;
+using PL = pair<ll, ll>;
+using P = pair<int, int>;
+const ll INF64 = INT64_MAX / 2;
+const int INF32 = INT32_MAX / 2;
+const char newl = '\n';
+
+int main() {
+  int n;
+  cin>>n;
+
+  map<string,int> mp;
+  range(i,0,n){
+    int k;
+    cin>>k;
+    range(j,0,k){
+      string s;
+      cin>>s;
+      mp[s]++;
+    }
+  }
+
+  int ans=0;
+  for(auto [name,cnt]:mp){
+    if(cnt==n) ans++;
+  }
+  cout<<ans<<endl;
+}
