dea0b353949cf2cb3e54167d02a63adb4e420c1f
matumoto1234 <matumotosizuoka@gmail.com>
2021-03-08 00:01:50 +0900

    "add ../atcoder/abc194/abc194_b/20762702.cpp"

---
+++ atcoder/abc194/abc194_b/20762702.cpp
@@ -0,0 +1,32 @@
+// detail: https://atcoder.jp/contests/abc194/submissions/20762702
+#include <bits/stdc++.h>
+using namespace std;
+#define range(i, l, r) for (int i = (int)(l); i < (int)(r); (i) += 1)
+#define rrange(i, l, r) for (int i = (int)(r)-1; i >= (int)(l); (i) -= 1)
+template <typename T1, typename T2> inline void chmax(T1 &a, T2 b) { a = (a > b ? a : b); }
+template <typename T1, typename T2> inline void chmin(T1 &a, T2 b) { a = (a < b ? a : b); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os,const pair<T1,T2> &p) { os<<p.first<<' '<<p.second; return os;}
+template <typename T> ostream &operator<<(ostream &os,const vector<T> &v) { range(i,0,v.size()) {os<<(i?" ":"")<<v[i];} return os;}
+using ull = unsigned long long;
+using ll = long long;
+using PL = pair<ll, ll>;
+using P = pair<int, int>;
+const ll INF64 = INT64_MAX / 2;
+const int INF32 = INT32_MAX / 2;
+const char newl = '\n';
+
+int main() {
+  int n;
+  cin>>n;
+  vector<int> a(n),b(n);
+  range(i,0,n){
+    cin>>a[i]>>b[i];
+  }
+
+  int ans=INF32;
+  range(i,0,n) range(j,0,n){
+    if(i==j) chmin(ans,a[i]+b[j]);
+    else chmin(ans,max(a[i],b[j]));
+  }
+  cout<<ans<<endl;
+}
