989b723fbce6a9b3c03bcd3b8519be8af6fa5102
crawler <>
2021-03-10 16:29:29 +0900

    "add ../atcoder/abc080/abc080_c/20809246.cpp"

---
+++ atcoder/abc080/abc080_c/20809246.cpp
@@ -0,0 +1,52 @@
+// detail: https://atcoder.jp/contests/abc080/submissions/20809246
+#include<bits/stdc++.h>
+#define rep(i, n) for(int i = 0;i < (n);i++)
+using namespace std;
+using ll = long long;
+
+vector<vector<int>> f(100, vector<int>(10)), p(100, vector<int>(11));
+
+    const ll INF = 1000000000;
+
+    int n;
+
+ll search(vector<int> x,ll na){
+    
+    if(na == 11){
+    return -INF;
+    } 
+
+    ll ans = search(x, na + 1);
+    x[na] = 1;
+    ans = max(search(x, na + 1), ans);
+    int sum = 0;
+
+    bool flag = false;
+    rep(i, 10) if(x[i] == 1) flag = true;
+    if(flag == false) return -INF;  
+
+    rep(i, n){
+        int num = 0;
+     rep(j, 10){
+         num += min(x[j], f[i][j]);
+     }       
+    sum += p[i][num];
+    }
+    ans = max(ans, (ll)sum);
+    return ans;
+}
+
+
+int main() {
+    cin >> n;
+
+
+    rep(i, n) rep(j, 10) cin >> f[i][j];
+
+    rep(i, n) rep(j, 11) cin >> p[i][j];
+
+    vector<int> x(11, 0);
+
+   ll ans = search(x, 0);
+    cout << ans << endl;
+}
