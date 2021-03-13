de8fd1499a5a221316b3038c7b0645c7975aeebc
crawler <matumotosizuoka+crawler@gmail.com>
2021-03-13 04:32:13 +0000

    add ../AtCoder/agc013/agc013_a/20858892.cpp

---
+++ AtCoder/agc013/agc013_a/20858892.cpp
@@ -0,0 +1,37 @@
+// detail: https://atcoder.jp/contests/agc013/submissions/20858892
+#include <bits/stdc++.h>
+using namespace std;
+#define range(i, l, r) for (int i = (int)(l); i < (int)(r); (i) += 1)
+#define rrange(i, l, r) for (int i = (int)(r)-1; i >= (int)(l); (i) -= 1)
+template <typename T1, typename T2> inline void chmax(T1 &a, T2 b) { a = (a > b ? a : b); }
+template <typename T1, typename T2> inline void chmin(T1 &a, T2 b) { a = (a < b ? a : b); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os,const pair<T1,T2> &p) { os<<p.first<<' '<<p.second; return os;}
+template <typename T> ostream &operator<<(ostream &os,const vector<T> &v) { range(i,0,v.size()) {os<<(i?" ":"")<<v[i];} return os;}
+using ull = unsigned long long;
+using ll = long long;
+using PL = pair<ll, ll>;
+using P = pair<int, int>;
+const ll INF64 = INT64_MAX / 2;
+const int INF32 = INT32_MAX / 2;
+const char newl = '\n';
+
+int main() {
+  int n;
+  cin>>n;
+  if(n<=2){
+    cout<<1<<endl;
+    return 0;
+  }
+  vector<int> a(n);
+  range(i,0,n){
+    cin>>a[i];
+  }
+  int ans=1;
+  range(i,1,n-1){
+    if(a[i-1]<a[i]==a[i]<a[i+1]) continue;
+    if(a[i-1]>a[i]==a[i]>a[i+1]) continue;
+    ans++;
+    i++;
+  }
+  cout<<ans<<endl;
+}
