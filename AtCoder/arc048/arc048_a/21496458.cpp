be66b6a8e1c21f46aabb5fb3bb356b9c760a6706
crawler <matumotosizuoka+crawler@gmail.com>
2021-04-04 07:50:48 +0000

    add ../AtCoder/arc048/arc048_a/21496458.cpp

---
+++ AtCoder/arc048/arc048_a/21496458.cpp
@@ -0,0 +1,24 @@
+// detail: https://atcoder.jp/contests/arc048/submissions/21496458
+#include <bits/stdc++.h>
+using namespace std;
+#define range(i, l, r) for (int i = (int)(l); i < (int)(r); (i) += 1)
+#define rrange(i, l, r) for (int i = (int)(r)-1; i >= (int)(l); (i) -= 1)
+template <typename T1, typename T2> inline void chmax(T1 &a, T2 b) { a = (a > b ? a : b); }
+template <typename T1, typename T2> inline void chmin(T1 &a, T2 b) { a = (a < b ? a : b); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os,const pair<T1,T2> &p) { os<<p.first<<' '<<p.second; return os;}
+template <typename T> ostream &operator<<(ostream &os,const vector<T> &v) { range(i,0,v.size()) {os<<(i?" ":"")<<v[i];} return os;}
+using ull = unsigned long long;
+using ll = long long;
+using Pll = pair<ll, ll>;
+using P = pair<int, int>;
+const ll INF64 = INT64_MAX / 2;
+const int INF32 = INT32_MAX / 2;
+const char newl = '\n';
+
+int main() {
+  int a,b;
+  cin>>a>>b;
+  int ans=abs(a-b);
+  if((a<0)!=(b<0)) ans--;
+  cout<<ans<<endl;
+}
