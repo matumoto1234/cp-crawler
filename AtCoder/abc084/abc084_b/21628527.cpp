1ea57348f521c67981f625216bbe96c3fbb603f1
crawler <matumotosizuoka+crawler@gmail.com>
2021-04-11 08:14:49 +0000

    add ../AtCoder/abc084/abc084_b/21628527.cpp

---
+++ AtCoder/abc084/abc084_b/21628527.cpp
@@ -0,0 +1,31 @@
+// detail: https://atcoder.jp/contests/abc084/submissions/21628527
+#include <bits/stdc++.h>
+using namespace std;
+#define range(i, l, r) for (int i = (int)(l); i < (int)(r); (i) += 1)
+#define rrange(i, l, r) for (int i = (int)(r)-1; i >= (int)(l); (i) -= 1)
+template <typename T1, typename T2> inline void chmax(T1 &a, T2 b) { a = (a > b ? a : b); }
+template <typename T1, typename T2> inline void chmin(T1 &a, T2 b) { a = (a < b ? a : b); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os,const pair<T1,T2> &p) { os<<p.first<<' '<<p.second; return os;}
+template <typename T> ostream &operator<<(ostream &os,const vector<T> &v) { range(i,0,v.size()) {os<<(i?" ":"")<<v[i];} return os;}
+using ull = unsigned long long;
+using ll = long long;
+using Pll = pair<ll, ll>;
+using P = pair<int, int>;
+const ll INF64 = INT64_MAX / 2;
+const int INF32 = INT32_MAX / 2;
+const char newl = '\n';
+
+int main() {
+  int a,b;
+  cin>>a>>b;
+  string s;
+  cin>>s;
+
+  bool ans=(s[a]=='-');
+  range(i,0,s.size()){
+    if(i==a) continue;
+    else if(s[i]<'0'|| s[i]>'9') ans=false;
+  }
+
+  cout<<(ans?"Yes":"No")<<endl;
+}
