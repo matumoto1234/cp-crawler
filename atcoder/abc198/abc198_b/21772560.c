5707fbaa593f8d4c42edcd7b8a75313918c5ee8f
matumoto1234 <matumotosizuoka@gmail.com>
2021-04-15 18:51:45 +0900

    "add ../atcoder/abc198/abc198_b/21772560.c"

---
+++ atcoder/abc198/abc198_b/21772560.c
@@ -0,0 +1,28 @@
+// detail: https://atcoder.jp/contests/abc198/submissions/21772560
+#include <stdio.h>
+ 
+int main()
+{
+  int N,Z;
+  int n = 0,temp;
+ 
+  scanf("%d",&N);
+  
+  if(n==0){
+    printf("Yes\n");
+    return 0;
+  }
+
+  while(N % 10 == 0) N = N / 10;
+
+  temp = N;
+  while(temp != 0){
+    Z = Z * 10 + temp % 10;
+    temp = temp / 10;
+  }
+  
+  if(N == Z) printf("Yes\n");
+  else printf("No\n");
+ 
+  return 0;
+}
