38d586623c8446f68174904c600123d4c297fd6b
crawler <matumotosizuoka+crawler@gmail.com>
2021-04-16 03:17:30 +0000

    add ../AtCoder/abc127/abc127_c/21783471.java

---
+++ AtCoder/abc127/abc127_c/21783471.java
@@ -0,0 +1,31 @@
+// detail: https://atcoder.jp/contests/abc127/submissions/21783471
+import java.util.Scanner;
+
+public class  Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+    int m = scanner.nextInt();
+    int[] imos = new int[100001];
+    
+    for(int i=0;i<m;i++){
+      int l = scanner.nextInt();
+      int r = scanner.nextInt();
+      l--;
+      
+      imos[l] = 1;
+      imos[r] = -1;
+    }
+    for(int i=0;i<n;i++){
+      imos[i+1]+=imos[i];
+    }
+    
+    int ans=0;
+    for(int i=0;i<n;i++){
+      if(imos[i]==m){
+        ans++;
+      }
+    }
+    System.out.println(ans);
+  }
+}
