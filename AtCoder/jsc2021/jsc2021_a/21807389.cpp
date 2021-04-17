3403d95f307233ed79f50a4a39c11996460728ca
crawler <matumotosizuoka+crawler@gmail.com>
2021-04-17 07:16:38 +0000

    add ../AtCoder/jsc2021/jsc2021_a/21807389.cpp

---
+++ AtCoder/jsc2021/jsc2021_a/21807389.cpp
@@ -0,0 +1,28 @@
+// detail: https://atcoder.jp/contests/jsc2021/submissions/21807389
+#include <bits/stdc++.h>
+using namespace std;
+#define range(i, l, r) for (int i = (int)(l); i < (int)(r); (i) += 1)
+#define rrange(i, l, r) for (int i = (int)(r)-1; i >= (int)(l); (i) -= 1)
+template <typename T1, typename T2> inline void chmax(T1 &a, T2 b) { a = (a > b ? a : b); }
+template <typename T1, typename T2> inline void chmin(T1 &a, T2 b) { a = (a < b ? a : b); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os,const pair<T1,T2> &p) { os<<p.first<<' '<<p.second; return os;}
+template <typename T> ostream &operator<<(ostream &os,const vector<T> &v) { range(i,0,v.size()) {os<<(i?" ":"")<<v[i];} return os;}
+using ull = unsigned long long;
+using ll = long long;
+using Pll = pair<ll, ll>;
+using P = pair<int, int>;
+const ll INF64 = INT64_MAX / 2;
+const int INF32 = INT32_MAX / 2;
+const char newl = '\n';
+
+int main() {
+  int x,y,z;
+  cin>>x>>y>>z;
+
+  rrange(i,0,10001){
+    if(y*z>i*x){
+      cout<<i<<'\n';
+      break;
+    }
+  }
+}
