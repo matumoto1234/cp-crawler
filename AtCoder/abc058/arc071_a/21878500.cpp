457da4770db9ac80d026566017e6f03be87cfa27
crawler <matumotosizuoka+crawler@gmail.com>
2021-04-18 23:10:20 +0000

    add ../AtCoder/abc058/arc071_a/21878500.cpp

---
+++ AtCoder/abc058/arc071_a/21878500.cpp
@@ -0,0 +1,42 @@
+// detail: https://atcoder.jp/contests/abc058/submissions/21878500
+#include <bits/stdc++.h>
+using namespace std;
+#define range(i, l, r) for (int i = (int)(l); i < (int)(r); (i) += 1)
+#define rrange(i, l, r) for (int i = (int)(r)-1; i >= (int)(l); (i) -= 1)
+template <typename T1, typename T2> inline void chmax(T1 &a, T2 b) { a = (a > b ? a : b); }
+template <typename T1, typename T2> inline void chmin(T1 &a, T2 b) { a = (a < b ? a : b); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os,const pair<T1,T2> &p) { os<<p.first<<' '<<p.second; return os;}
+template <typename T> ostream &operator<<(ostream &os,const vector<T> &v) { range(i,0,v.size()) {os<<(i?" ":"")<<v[i];} return os;}
+using ull = unsigned long long;
+using ll = long long;
+using Pll = pair<ll, ll>;
+using P = pair<int, int>;
+const ll INF64 = INT64_MAX / 2;
+const int INF32 = INT32_MAX / 2;
+const char newl = '\n';
+
+int main() {
+  int n;
+  cin>>n;
+  vector<string> s(n);
+  vector<vector<int>> cnt(n,vector<int>(26,0));
+  range(i,0,n){
+    cin>>s[i];
+    for(char c:s[i]){
+      int idx=c-'a';
+      cnt[i][idx]++;
+    }
+  }
+
+  string ans;
+
+  range(j,0,26){
+    char c=j+'a';
+    int sum=INF32;
+    range(i,0,n){
+      chmin(sum,cnt[i][j]);
+    }
+    range(i,0,sum) ans+=c;
+  }
+  cout<<ans<<endl;
+}
