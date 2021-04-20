97d40d5ae8e404772514ac6a19a91238b104f4bc
matumoto1234 <matumotosizuoka@gmail.com>
2021-04-21 07:44:33 +0900

    "add ../atcoder/dwango2016-prelims/dwango2016qual_b/21919180.c"

---
+++ atcoder/dwango2016-prelims/dwango2016qual_b/21919180.c
@@ -0,0 +1,33 @@
+// detail: https://atcoder.jp/contests/dwango2016-prelims/submissions/21919180
+#include <bits/stdc++.h>
+using namespace std;
+#define range(i, l, r) for (int i = (int)(l); i < (int)(r); (i) += 1)
+#define rrange(i, l, r) for (int i = (int)(r)-1; i >= (int)(l); (i) -= 1)
+template <typename T1, typename T2> inline void chmax(T1 &a, T2 b) { a = (a > b ? a : b); }
+template <typename T1, typename T2> inline void chmin(T1 &a, T2 b) { a = (a < b ? a : b); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os,const pair<T1,T2> &p) { os<<p.first<<' '<<p.second; return os;}
+template <typename T> ostream &operator<<(ostream &os,const vector<T> &v) { range(i,0,v.size()) {os<<(i?" ":"")<<v[i];} return os;}
+using ull = unsigned long long;
+using ll = long long;
+using Pll = pair<ll, ll>;
+using P = pair<int, int>;
+const ll INF64 = INT64_MAX / 2;
+const int INF32 = INT32_MAX / 2;
+const char newl = '\n';
+
+int main() {
+  int n;
+  cin>>n;
+  vector<int> k(n-1);
+  range(i,0,n-1){
+    cin>>k[i];
+  }
+
+  vector<int> ans(n,1);
+  ans.front()=k.front();
+  ans.back()=k.back();
+  range(i,1,n-1){
+    ans[i]=min(k[i-1],k[i]);
+  }
+  cout<<ans<<endl;
+}
