60b373a00f11041128210675a08abba5daedd306
crawler <>
2021-04-21 01:04:40 +0900

    "add ../atcoder/agc021/agc021_a/21916756.cpp"

---
+++ atcoder/agc021/agc021_a/21916756.cpp
@@ -0,0 +1,46 @@
+// detail: https://atcoder.jp/contests/agc021/submissions/21916756
+#include <bits/stdc++.h>
+using namespace std;
+#define range(i, l, r) for (int i = (int)(l); i < (int)(r); (i) += 1)
+#define rrange(i, l, r) for (int i = (int)(r)-1; i >= (int)(l); (i) -= 1)
+template <typename T1, typename T2> inline void chmax(T1 &a, T2 b) { a = (a > b ? a : b); }
+template <typename T1, typename T2> inline void chmin(T1 &a, T2 b) { a = (a < b ? a : b); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os,const pair<T1,T2> &p) { os<<p.first<<' '<<p.second; return os;}
+template <typename T> ostream &operator<<(ostream &os,const vector<T> &v) { range(i,0,v.size()) {os<<(i?" ":"")<<v[i];} return os;}
+using ull = unsigned long long;
+using ll = long long;
+using Pll = pair<ll, ll>;
+using P = pair<int, int>;
+const ll INF64 = INT64_MAX / 2;
+const int INF32 = INT32_MAX / 2;
+const char newl = '\n';
+
+int main() {
+  string n;
+  cin>>n;
+
+  int sum=0;
+  range(i,0,n.size()){
+    sum+=n[i]-'0';
+  }
+  int ans=sum;
+
+  range(j,0,16){
+    rrange(i,1,n.size()){
+      if(n[i-1]=='0' || n[i]=='9') continue;
+      n[i]='9';
+      n[i-1]--;
+    }
+  }
+
+  sum=0;
+  range(i,0,n.size()) sum+=n[i]-'0';
+  chmax(ans,sum);
+
+  sum=0;
+  range(i,0,n.size()-1){
+    sum+=9;
+  }
+  chmax(ans,sum);
+  cout<<ans<<endl;
+}
