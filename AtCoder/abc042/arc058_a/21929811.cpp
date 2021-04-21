b019b931018cea83a703135353b1999f90166b27
crawler <matumotosizuoka+crawler@gmail.com>
2021-04-21 10:37:40 +0000

    add ../AtCoder/abc042/arc058_a/21929811.cpp

---
+++ AtCoder/abc042/arc058_a/21929811.cpp
@@ -0,0 +1,39 @@
+// detail: https://atcoder.jp/contests/abc042/submissions/21929811
+#include <bits/stdc++.h>
+using namespace std;
+#define range(i, l, r) for (int i = (int)(l); i < (int)(r); (i) += 1)
+#define rrange(i, l, r) for (int i = (int)(r)-1; i >= (int)(l); (i) -= 1)
+template <typename T1, typename T2> inline void chmax(T1 &a, T2 b) { a = (a > b ? a : b); }
+template <typename T1, typename T2> inline void chmin(T1 &a, T2 b) { a = (a < b ? a : b); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os,const pair<T1,T2> &p) { os<<p.first<<' '<<p.second; return os;}
+template <typename T> ostream &operator<<(ostream &os,const vector<T> &v) { range(i,0,v.size()) {os<<(i?" ":"")<<v[i];} return os;}
+using ull = unsigned long long;
+using ll = long long;
+using Pll = pair<ll, ll>;
+using P = pair<int, int>;
+const ll INF64 = INT64_MAX / 2;
+const int INF32 = INT32_MAX / 2;
+const char newl = '\n';
+
+int main() {
+  int n,k;
+  cin>>n>>k;
+  string d;
+  range(i,0,k){
+    char c;
+    cin>>c;
+    d+=c;
+  }
+
+
+  while(1){
+    string s=to_string(n);
+    bool found=false;
+    range(i,0,k){
+      if(s.find(d[i])!=string::npos) found=true;
+    }
+    if(!found) break;
+    n++;
+  }
+  cout<<n<<endl;
+}
