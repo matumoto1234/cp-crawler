42dbafcdb30b486d900a777b7547fedf4af31f7f
crawler <matumotosizuoka+crawler@gmail.com>
2021-04-22 12:02:16 +0000

    add ../AtCoder/abc159/abc159_a/21951049.cpp

---
+++ AtCoder/abc159/abc159_a/21951049.cpp
@@ -0,0 +1,34 @@
+// detail: https://atcoder.jp/contests/abc159/submissions/21951049
+#include <bits/stdc++.h>
+using namespace std;
+#define range(i, l, r) for (int i = (int)(l); i < (int)(r); (i) += 1)
+#define rrange(i, l, r) for (int i = (int)(r)-1; i >= (int)(l); (i) -= 1)
+template <typename T1, typename T2> inline void chmax(T1 &a, T2 b) { a = (a > b ? a : b); }
+template <typename T1, typename T2> inline void chmin(T1 &a, T2 b) { a = (a < b ? a : b); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os,const pair<T1,T2> &p) { os<<p.first<<' '<<p.second; return os;}
+template <typename T> ostream &operator<<(ostream &os,const vector<T> &v) { range(i,0,v.size()) {os<<(i?" ":"")<<v[i];} return os;}
+using ull = unsigned long long;
+using ll = long long;
+using Pll = pair<ll, ll>;
+using P = pair<int, int>;
+const ll INF64 = INT64_MAX / 2;
+const int INF32 = INT32_MAX / 2;
+const char newl = '\n';
+
+template <typename T1,typename T2>
+T1 combination(T1 n,T2 r){
+  T1 res=1;
+  for(T1 i=0;i<r;i++){
+    res*=n-i;
+    res/=i+1;
+  }
+  return res;
+}
+
+int main() {
+  int n,m;
+  cin>>n>>m;
+  int ans=combination(n,2);
+  ans+=combination(m,2);
+  cout<<ans<<endl;
+}
