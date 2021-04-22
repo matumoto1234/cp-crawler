229ed7602f9da1122bd6345933983e1a8c1795c4
matumoto1234 <matumotosizuoka@gmail.com>
2021-04-23 07:32:59 +0900

    "add ../atcoder/abc055/abc055_b/21958428.cpp"

---
+++ atcoder/abc055/abc055_b/21958428.cpp
@@ -0,0 +1,29 @@
+// detail: https://atcoder.jp/contests/abc055/submissions/21958428
+#include <bits/stdc++.h>
+#include <atcoder/modint.hpp>
+using namespace atcoder;
+using mint = modint1000000007;
+using namespace std;
+#define range(i, l, r) for (int i = (int)(l); i < (int)(r); (i) += 1)
+#define rrange(i, l, r) for (int i = (int)(r)-1; i >= (int)(l); (i) -= 1)
+template <typename T1, typename T2> inline void chmax(T1 &a, T2 b) { a = (a > b ? a : b); }
+template <typename T1, typename T2> inline void chmin(T1 &a, T2 b) { a = (a < b ? a : b); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os,const pair<T1,T2> &p) { os<<p.first<<' '<<p.second; return os;}
+template <typename T> ostream &operator<<(ostream &os,const vector<T> &v) { range(i,0,v.size()) {os<<(i?" ":"")<<v[i];} return os;}
+using ull = unsigned long long;
+using ll = long long;
+using Pll = pair<ll, ll>;
+using P = pair<int, int>;
+const ll INF64 = INT64_MAX / 2;
+const int INF32 = INT32_MAX / 2;
+const char newl = '\n';
+
+int main() {
+  int n;
+  cin>>n;
+  mint ans=1;
+  range(i,0,n){
+    ans*=i+1;
+  }
+  cout<<ans.val()<<endl;
+}
