fbbe14eb5e02d3b3a471a76ce618279dc7d7df2d
matumoto1234 <matumotosizuoka@gmail.com>
2021-04-22 23:23:08 +0900

    "add ../atcoder/apc001/apc001_b/21954319.cpp"

---
+++ atcoder/apc001/apc001_b/21954319.cpp
@@ -0,0 +1,42 @@
+// detail: https://atcoder.jp/contests/apc001/submissions/21954319
+#include <bits/stdc++.h>
+using namespace std;
+#define range(i, l, r) for (int i = (int)(l); i < (int)(r); (i) += 1)
+#define rrange(i, l, r) for (int i = (int)(r)-1; i >= (int)(l); (i) -= 1)
+template <typename T1, typename T2> inline void chmax(T1 &a, T2 b) { a = (a > b ? a : b); }
+template <typename T1, typename T2> inline void chmin(T1 &a, T2 b) { a = (a < b ? a : b); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os,const pair<T1,T2> &p) { os<<p.first<<' '<<p.second; return os;}
+template <typename T> ostream &operator<<(ostream &os,const vector<T> &v) { range(i,0,v.size()) {os<<(i?" ":"")<<v[i];} return os;}
+using ull = unsigned long long;
+using ll = long long;
+using Pll = pair<ll, ll>;
+using P = pair<int, int>;
+const ll INF64 = INT64_MAX / 2;
+const int INF32 = INT32_MAX / 2;
+const char newl = '\n';
+
+template <typename T1,typename T2>
+T1 ceilDiv(T1 a,T2 b){
+  return (a+(b-1))/b;
+}
+
+int main() {
+  int n;
+  cin>>n;
+  vector<ll> a(n),b(n);
+  range(i,0,n) cin>>a[i];
+  range(i,0,n) cin>>b[i];
+  ll op=0;
+  range(i,0,n) op+=b[i]-a[i];
+  if(op<0){
+    cout<<"No"<<endl;
+    return 0;
+  }
+
+  ll sum=0;
+  range(i,0,n){
+    if(a[i]<b[i])sum+=ceilDiv(b[i]-a[i],2);
+  }
+  bool ans=(sum<=op);
+  cout<<(ans?"Yes":"No")<<endl;
+}
