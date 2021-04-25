6f30f9774286eb6068176b394ada326c61a84f0a
matumoto1234 <matumotosizuoka@gmail.com>
2021-04-25 23:57:50 +0900

    "add ../atcoder/abc064/abc064_a/22077002.cpp"

---
+++ atcoder/abc064/abc064_a/22077002.cpp
@@ -0,0 +1,23 @@
+// detail: https://atcoder.jp/contests/abc064/submissions/22077002
+#include <bits/stdc++.h>
+using namespace std;
+#define range(i, l, r) for (int i = (int)(l); i < (int)(r); (i) += 1)
+#define rrange(i, l, r) for (int i = (int)(r)-1; i >= (int)(l); (i) -= 1)
+template <typename T1, typename T2> inline void chmax(T1 &a, T2 b) { a = (a > b ? a : b); }
+template <typename T1, typename T2> inline void chmin(T1 &a, T2 b) { a = (a < b ? a : b); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os,const pair<T1,T2> &p) { os<<p.first<<' '<<p.second; return os;}
+template <typename T> ostream &operator<<(ostream &os,const vector<T> &v) { range(i,0,v.size()) {os<<(i?" ":"")<<v[i];} return os;}
+using ull = unsigned long long;
+using ll = long long;
+using Pll = pair<ll, ll>;
+using P = pair<int, int>;
+constexpr ll INF64 = INT64_MAX / 2;
+constexpr int INF32 = INT32_MAX / 2;
+constexpr char newl = '\n';
+
+int main() {
+  int a,b,c;
+  cin>>a>>b>>c;
+  int v=a*100+b*10+c;
+  cout<<(v%4==0?"YES":"NO")<<endl;
+}
