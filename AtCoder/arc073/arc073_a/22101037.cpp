9422a8cba148c62a2c6cd590e2a4332e4218fd27
crawler <matumotosizuoka+crawler@gmail.com>
2021-04-26 22:25:35 +0000

    add ../AtCoder/arc073/arc073_a/22101037.cpp

---
+++ AtCoder/arc073/arc073_a/22101037.cpp
@@ -0,0 +1,34 @@
+// detail: https://atcoder.jp/contests/arc073/submissions/22101037
+#include <bits/stdc++.h>
+using namespace std;
+#define range(i, l, r) for (int i = (int)(l); i < (int)(r); (i) += 1)
+#define rrange(i, l, r) for (int i = (int)(r)-1; i >= (int)(l); (i) -= 1)
+template <typename T1, typename T2> inline void chmax(T1 &a, T2 b) { a = (a > b ? a : b); }
+template <typename T1, typename T2> inline void chmin(T1 &a, T2 b) { a = (a < b ? a : b); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os,const pair<T1,T2> &p) { os<<p.first<<' '<<p.second; return os;}
+template <typename T> ostream &operator<<(ostream &os,const vector<T> &v) { range(i,0,v.size()) {os<<(i?" ":"")<<v[i];} return os;}
+using ull = unsigned long long;
+using ll = long long;
+using Pll = pair<ll, ll>;
+using P = pair<int, int>;
+constexpr ll INF64 = INT64_MAX / 2;
+constexpr int INF32 = INT32_MAX / 2;
+constexpr char newl = '\n';
+
+int main() {
+  int N,T;
+  cin>>N>>T;
+  vector<int> t;
+  range(i,0,N){
+    int in;
+    cin>>in;
+    t.emplace_back(in);
+  }
+  t.emplace_back(t[N-1]+T);
+
+  ll ans=0;
+  range(i,0,N){
+    ans+=min(T,t[i+1]-t[i]);
+  }
+  cout<<ans<<endl;
+}
