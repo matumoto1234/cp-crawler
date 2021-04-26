67fb7f883d6837fb60d66b3c293542364e4abfae
crawler <>
2021-04-27 00:02:35 +0900

    "add ../atcoder/abc196/abc196_c/22098024.cpp"

---
+++ atcoder/abc196/abc196_c/22098024.cpp
@@ -0,0 +1,30 @@
+// detail: https://atcoder.jp/contests/abc196/submissions/22098024
+#include <bits/stdc++.h>
+using namespace std;
+#define range(i, l, r) for (int i = (int)(l); i < (int)(r); (i) += 1)
+#define rrange(i, l, r) for (int i = (int)(r)-1; i >= (int)(l); (i) -= 1)
+template <typename T1, typename T2> inline void chmax(T1 &a, T2 b) { a = (a > b ? a : b); }
+template <typename T1, typename T2> inline void chmin(T1 &a, T2 b) { a = (a < b ? a : b); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os,const pair<T1,T2> &p) { os<<p.first<<' '<<p.second; return os;}
+template <typename T> ostream &operator<<(ostream &os,const vector<T> &v) { range(i,0,v.size()) {os<<(i?" ":"")<<v[i];} return os;}
+using ull = unsigned long long;
+using ll = long long;
+using Pll = pair<ll, ll>;
+using P = pair<int, int>;
+constexpr ll INF64 = INT64_MAX / 2;
+constexpr int INF32 = INT32_MAX / 2;
+constexpr char newl = '\n';
+
+int main() {
+  ll n;
+  cin>>n;
+
+  ll ans=0;
+  for(ll i=1;i*i<=n;i++){
+    string s=to_string(i);
+    s+=s;
+    if(stoll(s)>n) break;
+    ans++;
+  }
+  cout<<ans<<endl;
+}
