496453fda12559e23cf982f7a1797f1ec10c2497
crawler <>
2021-05-01 07:22:15 +0900

    "add ../atcoder/tenka1-2013-quala/tenka1_2013_qualA_b/22181245.cpp"

---
+++ atcoder/tenka1-2013-quala/tenka1_2013_qualA_b/22181245.cpp
@@ -0,0 +1,32 @@
+// detail: https://atcoder.jp/contests/tenka1-2013-quala/submissions/22181245
+#include <bits/stdc++.h>
+using namespace std;
+#define range(i, l, r) for (int i = (int)(l); i < (int)(r); (i) += 1)
+#define rrange(i, l, r) for (int i = (int)(r)-1; i >= (int)(l); (i) -= 1)
+template <typename T1, typename T2> inline void chmax(T1 &a, T2 b) { a = (a > b ? a : b); }
+template <typename T1, typename T2> inline void chmin(T1 &a, T2 b) { a = (a < b ? a : b); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os,const pair<T1,T2> &p) { os<<p.first<<' '<<p.second; return os;}
+template <typename T> ostream &operator<<(ostream &os,const vector<T> &v) { range(i,0,v.size()) {os<<(i?" ":"")<<v[i];} return os;}
+using ull = unsigned long long;
+using ll = long long;
+using Pll = pair<ll, ll>;
+using P = pair<int, int>;
+constexpr ll INF64 = INT64_MAX / 2;
+constexpr int INF32 = INT32_MAX / 2;
+constexpr char newl = '\n';
+
+int main() {
+  int n;
+  cin>>n;
+  int ans=0;
+  range(i,0,n){
+    int sum=0;
+    range(j,0,5){
+      int v;
+      cin>>v;
+      sum+=v;
+    }
+    if(sum<20) ans++;
+  }
+  cout<<ans<<endl;
+}
