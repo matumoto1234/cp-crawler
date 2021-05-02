a8dfc0f715cd96cf9ed408e1ecb0db896e5db16a
matumoto1234 <matumotosizuoka@gmail.com>
2021-05-02 21:46:35 +0900

    "add ../atcoder/agc018/agc018_a/22263169.cpp"

---
+++ atcoder/agc018/agc018_a/22263169.cpp
@@ -0,0 +1,46 @@
+// detail: https://atcoder.jp/contests/agc018/submissions/22263169
+#include <bits/stdc++.h>
+using namespace std;
+#define range(i, l, r) for (int i = (int)(l); i < (int)(r); (i) += 1)
+#define rrange(i, l, r) for (int i = (int)(r)-1; i >= (int)(l); (i) -= 1)
+template <typename T1, typename T2> inline void chmax(T1 &a, T2 b) { a = (a > b ? a : b); }
+template <typename T1, typename T2> inline void chmin(T1 &a, T2 b) { a = (a < b ? a : b); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os,const pair<T1,T2> &p) { os<<p.first<<' '<<p.second; return os;}
+template <typename T> ostream &operator<<(ostream &os,const vector<T> &v) { range(i,0,v.size()) {os<<(i?" ":"")<<v[i];} return os;}
+using ull = unsigned long long;
+using ll = long long;
+using Pll = pair<ll, ll>;
+using P = pair<int, int>;
+constexpr ll INF64 = INT64_MAX / 2;
+constexpr int INF32 = INT32_MAX / 2;
+constexpr int dy[] = {0,-1,1,0,-1,1,-1,1};
+constexpr int dx[] = {-1,0,0,1,-1,-1,1,1};
+constexpr int mod998244353 = 998244353;
+constexpr int mod1000000007 = (int)1e9 + 7;
+constexpr char newl = '\n';
+
+int main() {
+  int n,k;
+  cin>>n>>k;
+  vector<int> a(n);
+  range(i,0,n){
+    cin>>a[i];
+  }
+
+  int v;
+  int ma = -1;
+  range(i,0,n){
+    if(i == 0){
+      v = a[i];
+      chmax(ma,a[i]);
+      continue;
+    }
+    v = gcd(v,a[i]);
+  }
+
+  if(v <= k && k <= ma && k % v == 0){
+    cout<<"POSSIBLE"<<endl;
+  }else{
+    cout<<"IMPOSSIBLE"<<endl;
+  }
+}
