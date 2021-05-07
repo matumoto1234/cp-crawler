08f76de1ce35241392b6ec56013d7ef7be13b92f
crawler <matumotosizuoka+crawler@gmail.com>
2021-05-07 18:23:01 +0000

    add ../AtCoder/arc034/arc034_2/22376392.cpp

---
+++ AtCoder/arc034/arc034_2/22376392.cpp
@@ -0,0 +1,42 @@
+// detail: https://atcoder.jp/contests/arc034/submissions/22376392
+#include <bits/stdc++.h>
+using namespace std;
+#define range(i, l, r) for (int i = (int)(l); i < (int)(r); (i) += 1)
+#define rrange(i, l, r) for (int i = (int)(r)-1; i >= (int)(l); (i) -= 1)
+template <typename T1, typename T2> inline void chmax(T1 &a, T2 b) { a = (a > b ? a : b); }
+template <typename T1, typename T2> inline void chmin(T1 &a, T2 b) { a = (a < b ? a : b); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os,const pair<T1,T2> &p) { os<<p.first<<' '<<p.second; return os;}
+template <typename T> ostream &operator<<(ostream &os,const vector<T> &v) { range(i,0,v.size()) {os<<(i?" ":"")<<v[i];} return os;}
+using ull = unsigned long long;
+using ll = long long;
+using Pll = pair<ll, ll>;
+using P = pair<int, int>;
+constexpr ll INF64 = INT64_MAX / 2;
+constexpr int INF32 = INT32_MAX / 2;
+constexpr int dy[] = {0,-1,1,0,-1,1,-1,1};
+constexpr int dx[] = {-1,0,0,1,-1,-1,1,1};
+constexpr int mod998244353 = 998244353;
+constexpr int mod1000000007 = (int)1e9 + 7;
+constexpr char newl = '\n';
+
+int main() {
+  ll n;
+  cin>>n;
+
+  vector<ll> ans;
+  string s = to_string(n);
+  for(ll i=n-s.size()*9;i<n;i++){
+    string t = to_string(i);
+    ll sum=0;
+    range(j,0,t.size()){
+      sum+=t[j]-'0';
+    }
+    sum+=i;
+    if(sum == n) ans.emplace_back(i);
+  }
+  
+  cout<<ans.size()<<endl;
+  range(i,0,ans.size()){
+    cout<<ans[i]<<newl;
+  }
+}
