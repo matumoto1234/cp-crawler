c104a42539b5be30919637a0836790249ddf37f0
crawler <matumotosizuoka+crawler@gmail.com>
2021-05-08 07:25:22 +0000

    add ../AtCoder/abc160/abc160_d/22385042.cpp

---
+++ AtCoder/abc160/abc160_d/22385042.cpp
@@ -0,0 +1,110 @@
+// detail: https://atcoder.jp/contests/abc160/submissions/22385042
+#include <bits/stdc++.h>
+using namespace std;
+#define range(i, l, r) for (int i = (int)(l); i < (int)(r); (i) += 1)
+#define rrange(i, l, r) for (int i = (int)(r)-1; i >= (int)(l); (i) -= 1)
+template <typename T1, typename T2> inline void chmax(T1 &a, T2 b) { a = (a > b ? a : b); }
+template <typename T1, typename T2> inline void chmin(T1 &a, T2 b) { a = (a < b ? a : b); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os,const pair<T1,T2> &p) { os<<p.first<<' '<<p.second; return os;}
+template <typename T> ostream &operator<<(ostream &os,const vector<T> &v) { range(i,0,v.size()) {os<<(i?" ":"")<<v[i];} return os;}
+using ull = unsigned long long;
+using ll = long long;
+using Pll = pair<ll, ll>;
+using P = pair<int, int>;
+constexpr ll INF64 = INT64_MAX / 2;
+constexpr int INF32 = INT32_MAX / 2;
+constexpr int dy[] = {0,-1,1,0,-1,1,-1,1};
+constexpr int dx[] = {-1,0,0,1,-1,-1,1,1};
+constexpr int mod998244353 = 998244353;
+constexpr int mod1000000007 = (int)1e9 + 7;
+constexpr char newl = '\n';
+
+template <typename T>
+struct Dijkstra {
+  struct edge {
+    int to;
+    T cost;
+    edge() {}
+    edge(int to, T cost) : to(to), cost(cost) {}
+    bool operator<(const edge &e) const { return cost > e.cost; }
+  };
+
+  T inf() { return numeric_limits<T>::max()/2; }
+
+  vector<vector<edge>> G;
+  vector<T> ds;
+  vector<int> bs;
+  Dijkstra(int n) : G(n) {}
+
+  void add_edge(int from, int to, T cost) { G[from].emplace_back(to, cost); }
+
+  void build(int start) {
+    int n = G.size();
+    ds.assign(n, inf());
+    bs.assign(n,-1);
+
+    priority_queue<edge> Q;
+    ds[start] = 0;
+    Q.emplace(start, ds[start]);
+
+    while (!Q.empty()) {
+      auto p = Q.top();
+      Q.pop();
+      int v = p.to;
+      if (ds[v] < p.cost) continue;
+      for (auto e : G[v]) {
+        if (ds[e.to] > ds[v] + e.cost) {
+          ds[e.to] = ds[v] + e.cost;
+          bs[e.to]=v;
+          Q.emplace(e.to, ds[e.to]);
+        }
+      }
+    }
+  }
+
+  T operator[](int k) { return ds[k]; }
+
+  vector<int> restore(int to){
+    vector<int> res;
+    if(bs[to]==-1){
+      res.emplace_back(to);
+      return res;
+    }
+    while(to!=-1){
+      res.emplace_back(to);
+      to=bs[to];
+    }
+    reverse(res.begin(),res.end());
+    return res;
+  }
+};
+
+int main() {
+  int n,x,y;
+  cin>>n>>x>>y;
+  x--, y--;
+  Dijkstra<int> G(n);
+  G.add_edge(x,y,1);
+  range(i,0,n-1){
+    G.add_edge(i,i+1,1);
+    G.add_edge(i+1,i,1);
+  }
+
+  vector<int> ans(n,0);
+  set<P> ps;
+  range(i,0,n){
+    G.build(i);
+    range(j,0,n){
+      if(i==j) continue;
+      if(ps.find(P(i,j)) != ps.end()) continue;
+
+      ans[G[j]]++;
+      ps.insert(P(i,j));
+      ps.insert(P(j,i));
+    }
+  }
+
+  range(i,1,n){
+    cout<<ans[i]<<newl;
+  }
+}
