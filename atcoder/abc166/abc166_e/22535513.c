1a457e98b149f1844de59481fce19ea033d76a9a
matumoto1234 <matumotosizuoka@gmail.com>
2021-05-12 16:49:16 +0900

    "add ../atcoder/abc166/abc166_e/22535513.c"

---
+++ atcoder/abc166/abc166_e/22535513.c
@@ -0,0 +1,54 @@
+// detail: https://atcoder.jp/contests/abc166/submissions/22535513
+#include <bits/stdc++.h>
+using namespace std;
+
+// clang-format off
+#define range(i, l, r) for ( int i = (int)(l); i < (int)(r); (i) += 1 )
+#define rrange(i, l, r) for ( int i = (int)(r)-1; i >= (int)(l); (i) -= 1 )
+#define debug(x) cerr << "(" << __LINE__ << ") " << #x << ": " << (x) << endl;
+template <typename T1, typename T2> inline bool chmax(T1 &a, T2 b) { return a < b && (a = b, true); }
+template <typename T1, typename T2> inline bool chmin(T1 &a, T2 b) { return a > b && (a = b, true); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const pair<T1, T2> &p) { os << p.first << ' ' << p.second; return os; }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const map<T1, T2> &v) { for ( pair<T1, T2> x : v ) { os << x << (x.first == v.rbegin()->first && x.second == v.rbegin()->second ? "" : "\n"); } return os; }
+template <typename T> ostream &operator<<(ostream &os, queue<T> v) { if(!v.empty()) { os << v.front(); v.pop(); } while (!v.empty()) { os << " " << v.front(); v.pop(); } return os; }
+template <typename T> ostream &operator<<(ostream &os, stack<T> v) { if(!v.empty()) { os << v.top(); v.pop(); } while (!v.empty()) { os << " " << v.top(); v.pop(); } return os; }
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, const deque<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, const set<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+using ull = unsigned long long;
+using ll = long long;
+using Pll = pair<ll, ll>;
+using P = pair<int, int>;
+constexpr ll INF64 = INT64_MAX / 2;
+constexpr int INF32 = INT32_MAX / 2;
+constexpr int dy[] = { 0, -1, 1, 0, -1, 1, -1, 1 };
+constexpr int dx[] = { -1, 0, 0, 1, -1, -1, 1, 1 };
+constexpr int mod998244353 = 998244353;
+constexpr int mod1000000007 = (int)1e9 + 7;
+constexpr char newl = '\n';
+// clang-format on
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for ( auto &A : a )
+    cin >> A;
+
+  map<int, int> mp;
+  range(i, 0, n)
+    mp[i + a[i]]++;
+
+  ll ans = 0;
+  range(i, 0, n) { ans += mp[i - a[i]]; }
+  cout << ans << endl;
+}
+
+/* memo
+
+|i-j| = a[i]+a[j]
+i>jとしたとき
+i-j = a[i]+a[j]
+i-a[i] = j+a[j]を満たすものの個数
+a[i]=0となるものはないので、i-a[i]=i+a[i]にはならない
+*/
