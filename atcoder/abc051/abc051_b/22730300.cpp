0bbf7d782b3de8baff15bd49fb774a1f45e94991
crawler <>
2021-05-19 19:35:27 +0900

    "add ../atcoder/abc051/abc051_b/22730300.cpp"

---
+++ atcoder/abc051/abc051_b/22730300.cpp
@@ -0,0 +1,44 @@
+// detail: https://atcoder.jp/contests/abc051/submissions/22730300
+#include <bits/stdc++.h>
+using namespace std;
+
+// clang-format off
+#define range(i, l, r) for ( int i = (int)(l); i < (int)(r); (i) += 1 )
+#define rrange(i, l, r) for ( int i = (int)(r)-1; i >= (int)(l); (i) -= 1 )
+#define debug(x) cerr << "(" << __LINE__ << ") " << #x << ": " << (x) << endl;
+template <typename T1, typename T2> inline bool chmax(T1 &a, T2 b) { return a < b && (a = b, true); }
+template <typename T1, typename T2> inline bool chmin(T1 &a, T2 b) { return a > b && (a = b, true); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const pair<T1, T2> &p) { os << p.first << ' ' << p.second; return os; }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const map<T1, T2> &v) { for ( pair<T1, T2> x : v ) { os << x << "\n"; } return os; }
+template <typename T> ostream &operator<<(ostream &os, queue<T> v) { if(!v.empty()) { os << v.front(); v.pop(); } while (!v.empty()) { os << " " << v.front(); v.pop(); } return os; }
+template <typename T> ostream &operator<<(ostream &os, stack<T> v) { if(!v.empty()) { os << v.top(); v.pop(); } while (!v.empty()) { os << " " << v.top(); v.pop(); } return os; }
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, const deque<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, const set<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+using ull = unsigned long long;
+using ll = long long;
+using Pll = pair<ll, ll>;
+using P = pair<int, int>;
+constexpr ll INF64 = INT64_MAX / 2;
+constexpr int INF32 = INT32_MAX / 2;
+constexpr int dy[] = { 0, -1, 1, 0, -1, 1, -1, 1 };
+constexpr int dx[] = { -1, 0, 0, 1, -1, -1, 1, 1 };
+constexpr int mod998244353 = 998244353;
+constexpr int mod1000000007 = (int)1e9 + 7;
+constexpr char newl = '\n';
+// clang-format on
+
+int main() {
+  int k,s;
+  cin>>k>>s;
+
+  int ans=0;
+  range(x,0,k+1){
+    range(y,0,k+1){
+      int z=s-x-y;
+      if(z<0 || z>k) continue;
+      ans++;
+    }
+  }
+  cout<<ans<<endl;
+}
