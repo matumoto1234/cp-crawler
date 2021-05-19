5d9c11ccb17e399341a502ce98403766fe357d6c
crawler <matumotosizuoka+crawler@gmail.com>
2021-05-19 22:37:23 +0000

    add ../AtCoder/abc113/abc113_c/22739892.cpp

---
+++ AtCoder/abc113/abc113_c/22739892.cpp
@@ -0,0 +1,60 @@
+// detail: https://atcoder.jp/contests/abc113/submissions/22739892
+#include <bits/stdc++.h>
+using namespace std;
+
+// clang-format off
+#define range(i, l, r) for ( int i = (int)(l); i < (int)(r); (i) += 1 )
+#define rrange(i, l, r) for ( int i = (int)(r)-1; i >= (int)(l); (i) -= 1 )
+#define debug(x) cerr << "(" << __LINE__ << ") " << #x << ": " << (x) << endl;
+template <typename T1, typename T2> inline bool chmax(T1 &a, T2 b) { return a < b && (a = b, true); }
+template <typename T1, typename T2> inline bool chmin(T1 &a, T2 b) { return a > b && (a = b, true); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const pair<T1, T2> &p) { os << p.first << ' ' << p.second; return os; }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const map<T1, T2> &v) { for ( pair<T1, T2> x : v ) { os << x << "\n"; } return os; }
+template <typename T> ostream &operator<<(ostream &os, queue<T> v) { if(!v.empty()) { os << v.front(); v.pop(); } while (!v.empty()) { os << " " << v.front(); v.pop(); } return os; }
+template <typename T> ostream &operator<<(ostream &os, stack<T> v) { if(!v.empty()) { os << v.top(); v.pop(); } while (!v.empty()) { os << " " << v.top(); v.pop(); } return os; }
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, const deque<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, const set<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+using ull = unsigned long long;
+using ll = long long;
+using Pll = pair<ll, ll>;
+using P = pair<int, int>;
+constexpr ll INF64 = INT64_MAX / 2;
+constexpr int INF32 = INT32_MAX / 2;
+constexpr int dy[] = { 0, -1, 1, 0, -1, 1, -1, 1 };
+constexpr int dx[] = { -1, 0, 0, 1, -1, -1, 1, 1 };
+constexpr int mod998244353 = 998244353;
+constexpr int mod1000000007 = (int)1e9 + 7;
+constexpr char newl = '\n';
+// clang-format on
+
+string reading_zero(int n) {
+  string res = "000000";
+  rrange(i, 0, 6) {
+    res[i] = n % 10 + '0';
+    n /= 10;
+  }
+  return res;
+}
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector<vector<P>> ps(n);
+  range(i, 0, m) {
+    int p, y;
+    cin >> p >> y;
+    p--;
+    ps[p].emplace_back(y, i);
+  }
+
+  range(i, 0, n) {
+    sort(ps[i].begin(), ps[i].end());
+  }
+
+  range(i, 0, n) {
+    for ( auto [year, idx] : ps[i] ) {
+      cout << reading_zero(i + 1) + reading_zero(idx) << newl;
+    }
+  }
+}
