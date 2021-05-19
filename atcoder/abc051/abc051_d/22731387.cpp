93486d0c73ec835d17bce6556721b98ea2988baf
crawler <>
2021-05-19 20:33:49 +0900

    "add ../atcoder/abc051/abc051_d/22731387.cpp"

---
+++ atcoder/abc051/abc051_d/22731387.cpp
@@ -0,0 +1,132 @@
+// detail: https://atcoder.jp/contests/abc051/submissions/22731387
+#include <bits/stdc++.h>
+using namespace std;
+
+// clang-format off
+#define range(i, l, r) for ( int i = (int)(l); i < (int)(r); (i) += 1 )
+#define rrange(i, l, r) for ( int i = (int)(r)-1; i >= (int)(l); (i) -= 1 )
+#define debug(x) cerr << "(" << __LINE__ << ") " << #x << ": " << (x) << endl;
+template <typename T1, typename T2> inline bool chmax(T1 &a, T2 b) { return a < b && (a = b, true); }
+template <typename T1, typename T2> inline bool chmin(T1 &a, T2 b) { return a > b && (a = b, true); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const pair<T1, T2> &p) { os << p.first << ' ' << p.second; return os; }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const map<T1, T2> &v) { for ( pair<T1, T2> x : v ) { os << x << "\n"; } return os; }
+template <typename T> ostream &operator<<(ostream &os, queue<T> v) { if(!v.empty()) { os << v.front(); v.pop(); } while (!v.empty()) { os << " " << v.front(); v.pop(); } return os; }
+template <typename T> ostream &operator<<(ostream &os, stack<T> v) { if(!v.empty()) { os << v.top(); v.pop(); } while (!v.empty()) { os << " " << v.top(); v.pop(); } return os; }
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, const deque<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, const set<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+using ull = unsigned long long;
+using ll = long long;
+using Pll = pair<ll, ll>;
+using P = pair<int, int>;
+constexpr ll INF64 = INT64_MAX / 2;
+constexpr int INF32 = INT32_MAX / 2;
+constexpr int dy[] = { 0, -1, 1, 0, -1, 1, -1, 1 };
+constexpr int dx[] = { -1, 0, 0, 1, -1, -1, 1, 1 };
+constexpr int mod998244353 = 998244353;
+constexpr int mod1000000007 = (int)1e9 + 7;
+constexpr char newl = '\n';
+// clang-format on
+
+template <typename T>
+struct Dijkstra {
+  struct edge {
+    int to;
+    T cost;
+    edge() {}
+    edge(int to, T cost) : to(to), cost(cost) {}
+    bool operator<(const edge &e) const { return cost > e.cost; }
+  };
+
+  T inf() { return numeric_limits<T>::max()/2; }
+
+  vector<vector<edge>> G;
+  vector<T> ds;
+  vector<int> bs;
+  Dijkstra(int n) : G(n) {}
+
+  void add_edge(int from, int to, T cost) { G[from].emplace_back(to, cost); }
+
+  void build(int start) {
+    int n = G.size();
+    ds.assign(n, inf());
+    bs.assign(n,-1);
+
+    priority_queue<edge> Q;
+    ds[start] = 0;
+    Q.emplace(start, ds[start]);
+
+    while (!Q.empty()) {
+      auto p = Q.top();
+      Q.pop();
+      int v = p.to;
+      if (ds[v] < p.cost) continue;
+      for (auto e : G[v]) {
+        if (ds[e.to] > ds[v] + e.cost) {
+          ds[e.to] = ds[v] + e.cost;
+          bs[e.to]=v;
+          Q.emplace(e.to, ds[e.to]);
+        }
+      }
+    }
+  }
+
+  T operator[](int k) { return ds[k]; }
+
+  vector<int> restore(int to){
+    vector<int> res;
+    if(bs[to]==-1){
+      res.emplace_back(to);
+      return res;
+    }
+    while(to!=-1){
+      res.emplace_back(to);
+      to=bs[to];
+    }
+    reverse(res.begin(),res.end());
+    return res;
+  }
+};
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  
+  Dijkstra<ll> G(n);
+
+  map<P,bool> edges;
+  vector<P> ab(m);
+
+  range(i,0,m){
+    int a,b,c;
+    cin>>a>>b>>c;
+    a--,b--;
+    G.add_edge(a,b,c);
+    G.add_edge(b,a,c);
+    edges[make_pair(a,b)]=true;
+    edges[make_pair(b,a)]=true;
+    ab[i].first = a;
+    ab[i].second = b;
+  }
+
+  range(i,0,n){
+    G.build(i);
+    range(j,0,n){
+      if(i==j) continue;
+      auto vs = G.restore(j);
+      
+      range(k,1,vs.size()){
+        int v = vs[k];
+        int u = vs[k-1];
+        edges[make_pair(v,u)]=false;
+        edges[make_pair(u,v)]=false;
+      }
+    }
+  }
+
+  int ans=0;
+  range(i,0,m){
+    if(edges[ab[i]]) ans++;
+  }
+  cout<<ans<<endl;
+}
