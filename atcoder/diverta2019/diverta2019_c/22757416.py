fe66e488bef8a0565a1e1e9fac794cf4fdfdf721
matumoto1234 <matumotosizuoka@gmail.com>
2021-05-21 08:05:09 +0900

    "add ../atcoder/diverta2019/diverta2019_c/22757416.py"

---
+++ atcoder/diverta2019/diverta2019_c/22757416.py
@@ -0,0 +1,24 @@
+# detail: https://atcoder.jp/contests/diverta2019/submissions/22757416
+n = int(input())
+s = [input() for _ in range(n)]
+
+acnt = 0
+bcnt = 0
+bacnt = 0
+cnt = 0
+for i in range(n):
+  if s[i][len(s[i]) - 1] == 'A':
+    acnt += 1
+  if s[i][0] == 'B':
+    bcnt += 1
+  if s[i][len(s[i]) - 1] == 'A' and s[i][0] == 'B':
+    bacnt += 1
+
+  cnt += s[i].count('AB')
+
+if bacnt == bcnt and bacnt == acnt:
+  cnt += bacnt - 1 
+else:
+  cnt += min(acnt, bcnt)
+
+print(cnt)
