49b3040ea4148a2b47ff4a6445909bb0d1480533
crawler <matumotosizuoka+crawler@gmail.com>
2021-05-22 13:13:41 +0000

    add ../AtCoder/abc202/abc202_d/22828602.cpp

---
+++ AtCoder/abc202/abc202_d/22828602.cpp
@@ -0,0 +1,84 @@
+// detail: https://atcoder.jp/contests/abc202/submissions/22828602
+#include <bits/stdc++.h>
+using namespace std;
+
+// clang-format off
+#define range(i, l, r) for ( int i = (int)(l); i < (int)(r); (i) += 1 )
+#define rrange(i, l, r) for ( int i = (int)(r)-1; i >= (int)(l); (i) -= 1 )
+#define debug(x) cerr << #x << ": " << (x) << endl;
+template <typename T1, typename T2> inline bool chmax(T1 &a, T2 b) { return a < b && (a = b, true); }
+template <typename T1, typename T2> inline bool chmin(T1 &a, T2 b) { return a > b && (a = b, true); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const pair<T1, T2> &p) { os << p.first << ' ' << p.second; return os; }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const map<T1, T2> &v) { for ( pair<T1, T2> x : v ) { os << x << "\n"; } return os; }
+template <typename T> ostream &operator<<(ostream &os, queue<T> v) { if(!v.empty()) { os << v.front(); v.pop(); } while (!v.empty()) { os << " " << v.front(); v.pop(); } return os; }
+template <typename T> ostream &operator<<(ostream &os, stack<T> v) { if(!v.empty()) { os << v.top(); v.pop(); } while (!v.empty()) { os << " " << v.top(); v.pop(); } return os; }
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, const deque<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, const set<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+using ull = unsigned long long;
+using ll = long long;
+using Pll = pair<ll, ll>;
+using P = pair<int, int>;
+constexpr ll INF64 = INT64_MAX / 2;
+constexpr int INF32 = INT32_MAX / 2;
+constexpr int dy[] = { 0, -1, 1, 0, -1, 1, -1, 1 };
+constexpr int dx[] = { -1, 0, 0, 1, -1, -1, 1, 1 };
+constexpr int mod998244353 = 998244353;
+constexpr int mod1000000007 = (int)1e9 + 7;
+constexpr char newl = '\n';
+// clang-format on
+
+template <typename T1,typename T2>
+T1 Binomial(T1 n,T2 r){
+  T1 res=1;
+  for(T1 i=0;i<r;i++){
+    res*=n-i;
+    res/=i+1;
+  }
+  return res;
+}
+
+ll a,b,k;
+bitset<64> bit;
+ll sum;
+void dfs(ll n){
+  if(n>b) return;
+
+  range(i,b-n+1,61){
+    ll v=Binomial((ll)i-1,b-n);
+    //cerr<<"n:"<<n<<" i:"<<i<<" v:"<<v<<endl;
+    if(sum+v>k){
+      bit[i-1]=1;
+      //debug("hello"s);
+      //debug(v);
+      //debug(sum);
+      //debug(bit);
+      dfs(n+1);
+      return;
+    }
+    else if(sum+v == k){
+      //debug(i);
+      if(n!=b)range(j,0,b) bit[i-j-1]=1;
+      else bit[i-1]=1;
+      sum+=v;
+      string ans;
+      range(i,0,a+b){
+        if(bit[i]) ans.push_back('b');
+        else ans.push_back('a');
+      }
+      reverse(ans.begin(),ans.end());
+      cout<<ans<<endl;
+      return;
+    }else{
+      sum+=v;
+    }
+  }
+}
+
+int main() {
+  cin>>a>>b>>k;
+
+  sum=0;
+  dfs(1);
+  // cout<<bit<<endl;
+}
