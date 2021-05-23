c39296e0097fa2280e3b81d48378e46ab72a0712
matumoto1234 <matumotosizuoka@gmail.com>
2021-05-24 08:04:40 +0900

    "add ../atcoder/abc063/abc063_b/22881648.py"

---
+++ atcoder/abc063/abc063_b/22881648.py
@@ -0,0 +1,15 @@
+# detail: https://atcoder.jp/contests/abc063/submissions/22881648
+s = int(input())
+
+dic = {}
+
+ans = True
+for i in range(len(s)):
+  if s[i] in dic == True:
+    ans = False
+  dic[s[i]] = 1
+
+if ans == True:
+  print("yes")
+else:
+  print("no")
