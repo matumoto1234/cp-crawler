8b3688b183beb723588b7b27e78dbd830983fc31
matumoto1234 <matumotosizuoka@gmail.com>
2021-05-26 05:34:10 +0900

    "add ../atcoder/sumitrust2019/sumitb2019_a/22920137.py"

---
+++ atcoder/sumitrust2019/sumitb2019_a/22920137.py
@@ -0,0 +1,6 @@
+# detail: https://atcoder.jp/contests/sumitrust2019/submissions/22920137
+vs = [ list(map(int,input().split())) for i in range(2)]
+if vs[0][0] == vs[1][0]:
+  print(1)
+else:
+  print(0)
