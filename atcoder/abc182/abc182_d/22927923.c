43bcd9a1e7fd8771c1a4ef782e65f81aa56b3ecf
matumoto1234 <matumotosizuoka@gmail.com>
2021-05-26 17:42:20 +0900

    "add ../atcoder/abc182/abc182_d/22927923.c"

---
+++ atcoder/abc182/abc182_d/22927923.c
@@ -0,0 +1,68 @@
+// detail: https://atcoder.jp/contests/abc182/submissions/22927923
+#include <bits/stdc++.h>
+#include <atcoder/segtree.hpp>
+using namespace atcoder;
+using namespace std;
+
+// clang-format off
+#define range(i, l, r) for ( int i = (int)(l); i < (int)(r); (i) += 1 )
+#define rrange(i, l, r) for ( int i = (int)(r)-1; i >= (int)(l); (i) -= 1 )
+#define debug(x) cerr << #x << ": " << (x) << endl;
+template <typename T1, typename T2> inline bool chmax(T1 &a, T2 b) { return a < b && (a = b, true); }
+template <typename T1, typename T2> inline bool chmin(T1 &a, T2 b) { return a > b && (a = b, true); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const pair<T1, T2> &p) { os << p.first << ' ' << p.second; return os; }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const map<T1, T2> &v) { for ( pair<T1, T2> x : v ) { os << x << "\n"; } return os; }
+template <typename T> ostream &operator<<(ostream &os, queue<T> v) { if(!v.empty()) { os << v.front(); v.pop(); } while (!v.empty()) { os << " " << v.front(); v.pop(); } return os; }
+template <typename T> ostream &operator<<(ostream &os, stack<T> v) { if(!v.empty()) { os << v.top(); v.pop(); } while (!v.empty()) { os << " " << v.top(); v.pop(); } return os; }
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, const deque<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, const set<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+using ull = unsigned long long;
+using ll = long long;
+using Pll = pair<ll, ll>;
+using P = pair<int, int>;
+constexpr ll INF64 = INT64_MAX / 2;
+constexpr int INF32 = INT32_MAX / 2;
+constexpr int dy[] = { 0, -1, 1, 0, -1, 1, -1, 1 };
+constexpr int dx[] = { -1, 0, 0, 1, -1, -1, 1, 1 };
+constexpr int mod998244353 = 998244353;
+constexpr int mod1000000007 = (int)1e9 + 7;
+constexpr char newl = '\n';
+// clang-format on
+
+template <typename T>
+struct CumulativeSum{
+  vector<T> sum;
+  CumulativeSum(const vector<T> &a){
+    int n=a.size();
+    sum.assign(n+1,0);
+    for(int i=0;i<n;i++) sum[i+1]=a[i]+sum[i];
+  }
+
+  T query(int l,int r){ return sum[r]-sum[l]; }
+};
+
+ll op(ll a,ll b){
+  return max(a,b);
+}
+
+ll e(){
+  return -INF64;
+}
+
+int main() {
+  int n;
+  cin>>n;
+  vector<ll> a(n);
+  for(auto &A:a) cin>>A;
+
+  CumulativeSum<ll> sum(a);
+  segtree<ll,op,e> seg(sum.sum);
+  ll ans=-INF64;
+  ll now=0;
+  range(i,0,n){
+    chmax(ans,now+seg.prod(0,i+1));
+    now+=sum.query(0,i+1);
+  }
+  cout<<ans<<endl;
+}
