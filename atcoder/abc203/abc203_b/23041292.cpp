1bea02e07732e998f964857a60d1429cd07b180f
matumoto1234 <matumotosizuoka@gmail.com>
2021-05-30 21:04:28 +0900

    "add ../atcoder/abc203/abc203_b/23041292.cpp"

---
+++ atcoder/abc203/abc203_b/23041292.cpp
@@ -0,0 +1,58 @@
+// detail: https://atcoder.jp/contests/abc203/submissions/23041292
+#include <bits/stdc++.h>
+using namespace std;
+
+// clang-format off
+#define rep(i, n) for (int i = 0; i < (int)(n); i += 1)
+#define rrep(i, n) for (int i = (int)(n); i >= 0; i -= 1)
+#define range(i, l, r) for ( int i = (int)(l); i < (int)(r); (i) += 1 )
+#define rrange(i, l, r) for ( int i = (int)(r)-1; i >= (int)(l); (i) -= 1 )
+#define debug(...) debug_func(0, #__VA_ARGS__, __VA_ARGS__)
+template <typename T> void debug_func(int i, T name) { cerr << endl; }
+template <typename T1, typename T2, typename... T3> void debug_func(int i, const T1 &name, const T2 &a, const T3 &...b) { for ( ; name[i] != ',' && name[i] != '\0'; i++ ) cerr << name[i]; cerr << ":" << a << " "; debug_func(i + 1, name, b...); }
+template <typename T> vector<T> make_vector(size_t a, T b) { return vector<T>(a, b); }
+template <typename... Ts> auto make_vector(size_t a, Ts... ts) { return vector<decltype(make_vector(ts...))>(a, make_vector(ts...)); }
+template <typename T1, typename T2> inline bool chmax(T1 &a, T2 b) { return a < b && (a = b, true); }
+template <typename T1, typename T2> inline bool chmin(T1 &a, T2 b) { return a > b && (a = b, true); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const pair<T1, T2> &p) { os << p.first << ' ' << p.second; return os; }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const map<T1, T2> &v) { for ( pair<T1, T2> x : v ) { os << x << "\n"; } return os; }
+template <typename T> ostream &operator<<(ostream &os, queue<T> v) { if(!v.empty()) { os << v.front(); v.pop(); } while (!v.empty()) { os << " " << v.front(); v.pop(); } return os; }
+template <typename T> ostream &operator<<(ostream &os, stack<T> v) { if(!v.empty()) { os << v.top(); v.pop(); } while (!v.empty()) { os << " " << v.top(); v.pop(); } return os; }
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, const deque<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, const set<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+template <typename T> void operator+=(vector<T> &vs, T x) { for ( T &v : vs ) v += x; }
+template <typename T> void operator-=(vector<T> &vs, T x) { for ( T &v : vs ) v -= x; }
+template <typename T> void operator*=(vector<T> &vs, T x) { for ( T &v : vs ) v *= x; }
+template <typename T> void operator/=(vector<T> &vs, T x) { for ( T &v : vs ) v /= x; }
+template <typename T> void operator%=(vector<T> &vs, T x) { for ( T &v : vs ) v %= x; }
+template <typename T> vector<T> operator+(const vector<T> &vs, T x) { vector<T> res = vs; for ( T &v : res ) v += x; return res; }
+template <typename T> vector<T> operator-(const vector<T> &vs, T x) { vector<T> res = vs; for ( T &v : res ) v -= x; return res; }
+template <typename T> vector<T> operator*(const vector<T> &vs, T x) { vector<T> res = vs; for ( T &v : res ) v *= x; return res; }
+template <typename T> vector<T> operator/(const vector<T> &vs, T x) { vector<T> res = vs; for ( T &v : res ) v /= x; return res; }
+template <typename T> vector<T> operator%(const vector<T> &vs, T x) { vector<T> res = vs; for ( T &v : res ) v %= x; return res; }
+using ull = unsigned long long;
+using ll = long long;
+using Pll = pair<ll, ll>;
+using P = pair<int, int>;
+constexpr ll INF64 = INT64_MAX / 2;
+constexpr int INF32 = INT32_MAX / 2;
+constexpr int dy[] = { 0, -1, 1, 0, -1, 1, -1, 1 };
+constexpr int dx[] = { -1, 0, 0, 1, -1, -1, 1, 1 };
+constexpr int mod998244353 = 998244353;
+constexpr int mod1000000007 = (int)1e9 + 7;
+constexpr char newl = '\n';
+// clang-format on
+
+int main() {
+  int n,k;
+  cin >> n >> k;
+  int ans=0;
+  range(i,1,n+1){
+    range(j,1,k+1){
+      int v=i*100 + j;
+      ans+=v;
+    }
+  }
+  cout<<ans<<endl;
+}
