b32d8d45b4bbcef31deed952000cd5fc894724ab
crawler <matumotosizuoka+crawler@gmail.com>
2021-06-03 22:04:22 +0000

    add ../AtCoder/arc004/arc004_4/23158634.py

---
+++ AtCoder/arc004/arc004_4/23158634.py
@@ -0,0 +1,38 @@
+# detail: https://atcoder.jp/contests/arc004/submissions/23158634
+from collections import Counter
+def mod_binomial(n,k,mod):
+  res=1
+  for i in range(1,k+1):
+    res*=n-(i-1)
+    res%=mod
+    res*=pow(i,mod-2,mod)
+    res%=mod
+  return res
+
+def primefactor(n):
+  ps=[]
+  i=2
+  x=n
+  while i*i<=n:
+    while(x%i==0):
+      ps.append(i)
+      x//=i
+    i+=1
+  if x>1:
+    ps.append(x)
+  return Counter(ps)
+  
+
+# main
+
+n,m=map(int,input().split())
+ps=primefactor(abs(n))
+mod=10**9+7
+sum1=1
+for cnt in ps.values():
+  sum1*=mod_binomial(m+cnt-1,m-1,mod)
+  sum1%=mod
+
+sum2=pow(2,m-1,mod)
+ans=(sum1*sum2)%mod
+print(ans)
