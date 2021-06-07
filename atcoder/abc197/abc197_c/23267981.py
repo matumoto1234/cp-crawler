ca2877e9e986d9bc39f43102135fa73153802cfd
matumoto1234 <matumotosizuoka@gmail.com>
2021-06-07 11:46:43 +0900

    "add ../atcoder/abc197/abc197_c/23267981.py"

---
+++ atcoder/abc197/abc197_c/23267981.py
@@ -0,0 +1,17 @@
+# detail: https://atcoder.jp/contests/abc197/submissions/23267981
+n=int(input())
+a=list(map(int,input().split()))
+
+ans=10**18
+for i in range(2**(n-1)):
+  temp=0
+  s=0
+  for j in range(n):
+    temp|=a[j]
+    if (i>>j)&1:
+      s^=temp
+      temp=0
+  s^=temp
+  ans=min(ans,s)
+
+print(ans)
