eaa4aa476930a47e3afce86f0dc54ac6ddf578bf
crawler <matumotosizuoka+crawler@gmail.com>
2021-06-07 08:47:04 +0000

    add ../AtCoder/code-festival-2016-quala/codefestival_2016_qualA_a/23274213.py

---
+++ AtCoder/code-festival-2016-quala/codefestival_2016_qualA_a/23274213.py
@@ -0,0 +1,8 @@
+# detail: https://atcoder.jp/contests/code-festival-2016-quala/submissions/23274213
+s=input()
+for i in range(len(s)):
+  print(s[i],end='')
+  if i==3:
+    print(end=' ')
+  
+print()
