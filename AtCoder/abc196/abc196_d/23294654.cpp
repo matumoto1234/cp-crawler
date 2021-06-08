6a51d2ddbf45994ca2d12513f2b248c3b0eae3ca
crawler <matumotosizuoka+crawler@gmail.com>
2021-06-08 10:55:05 +0000

    add ../AtCoder/abc196/abc196_d/23294654.cpp

---
+++ AtCoder/abc196/abc196_d/23294654.cpp
@@ -0,0 +1,94 @@
+// detail: https://atcoder.jp/contests/abc196/submissions/23294654
+#include <bits/stdc++.h>
+using namespace std;
+
+// clang-format off
+#define rep(i, n) for (int i = 0; i < (int)(n); i += 1)
+#define rrep(i, n) for (int i = (int)(n)-1; i >= 0; i -= 1)
+#define range(i, l, r) for ( int i = (int)(l); i < (int)(r); (i) += 1 )
+#define rrange(i, l, r) for ( int i = (int)(r)-1; i >= (int)(l); (i) -= 1 )
+#define debug(...) debug_func(#__VA_ARGS__, __VA_ARGS__)
+template <typename T, typename... T2> void debug_func(string_view name, const T &a, T2 &&...rest) { stack<char> bs; string_view lbs = "({[<"; string_view rbs = ")}]>"; int end = name.size(); for ( int i = 0; i < (int)name.size(); i++ ) { if ( lbs.find(name[i]) != string::npos ) { bs.push(name[i]); } if ( rbs.find(name[i]) != string::npos ) { if ( !bs.empty() ) { bs.pop(); } } if ( name[i] == ',' && bs.empty() ) { end = i; break; } } cerr << name.substr(0, end) << ":" << a; if constexpr ( sizeof...(rest) == 0 ) { cerr << endl; } else { cerr << ' '; debug_func(name.substr(name.find_first_not_of(' ', end + 1)), forward<T2>(rest)...); } }
+template <typename T> vector<T> make_vector(size_t a, T b) { return vector<T>(a, b); }
+template <typename... Ts> auto make_vector(size_t a, Ts... ts) { return vector<decltype(make_vector(ts...))>(a, make_vector(ts...)); }
+template <typename T1, typename T2> inline bool chmax(T1 &a, T2 b) { return a < b && (a = b, true); }
+template <typename T1, typename T2> inline bool chmin(T1 &a, T2 b) { return a > b && (a = b, true); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const pair<T1, T2> &p) { os << p.first << ' ' << p.second; return os; }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const map<T1, T2> &v) { for ( pair<T1, T2> x : v ) { os << x << "\n"; } return os; }
+template <typename T> ostream &operator<<(ostream &os, queue<T> v) { if(!v.empty()) { os << v.front(); v.pop(); } while (!v.empty()) { os << " " << v.front(); v.pop(); } return os; }
+template <typename T> ostream &operator<<(ostream &os, stack<T> v) { if(!v.empty()) { os << v.top(); v.pop(); } while (!v.empty()) { os << " " << v.top(); v.pop(); } return os; }
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, const deque<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, const set<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+template <typename T> void operator+=(vector<T> &vs, T x) { for ( T &v : vs ) v += x; }
+template <typename T> void operator-=(vector<T> &vs, T x) { for ( T &v : vs ) v -= x; }
+template <typename T> void operator*=(vector<T> &vs, T x) { for ( T &v : vs ) v *= x; }
+template <typename T> void operator/=(vector<T> &vs, T x) { for ( T &v : vs ) v /= x; }
+template <typename T> void operator%=(vector<T> &vs, T x) { for ( T &v : vs ) v %= x; }
+template <typename T> vector<T> operator+(const vector<T> &vs, T x) { vector<T> res = vs; for ( T &v : res ) v += x; return res; }
+template <typename T> vector<T> operator-(const vector<T> &vs, T x) { vector<T> res = vs; for ( T &v : res ) v -= x; return res; }
+template <typename T> vector<T> operator*(const vector<T> &vs, T x) { vector<T> res = vs; for ( T &v : res ) v *= x; return res; }
+template <typename T> vector<T> operator/(const vector<T> &vs, T x) { vector<T> res = vs; for ( T &v : res ) v /= x; return res; }
+template <typename T> vector<T> operator%(const vector<T> &vs, T x) { vector<T> res = vs; for ( T &v : res ) v %= x; return res; }
+using ull = unsigned long long;
+using ll = long long;
+using Pll = pair<ll, ll>;
+using P = pair<int, int>;
+constexpr ll INF64 = INT64_MAX / 2;
+constexpr int INF32 = INT32_MAX / 2;
+constexpr int dy[] = { 0, -1, 1, 0, -1, 1, -1, 1 };
+constexpr int dx[] = { -1, 0, 0, 1, -1, -1, 1, 1 };
+constexpr int mod998244353 = 998244353;
+constexpr int mod1000000007 = (int)1e9 + 7;
+constexpr char newl = '\n';
+// clang-format on
+ 
+int main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  int h,w,a,b;
+  cin>>h>>w>>a>>b;
+  // dp[i][j][k][S]:=(i,j)まで埋めたとき,1*1のマスをk個置いたビット列Sの状態
+  auto dp=make_vector(h+1,w+1,b+10,1<<w,0);
+  dp[0][0][0][0]=1;
+  rep(i,h) rep(j,w) rep(k,b+1) rep(S,1<<w) {
+    if(S>>j&1){
+      int nextS=S&~(1<<j);
+      if(j+1<w){
+        dp[i][j+1][k][nextS]+=dp[i][j][k][S];
+      }else{
+        dp[i+1][0][k][nextS]+=dp[i][j][k][S];
+      }
+      continue;
+    }
+
+    // 横置き
+    if(j+1<w && !(S&(1<<(j+1)))){
+      int nextS=S|(1<<(j+1));
+      dp[i][j+1][k][nextS]+=dp[i][j][k][S];
+    }
+    // 縦置き
+    if(i+1<h){
+      int nextS=S|(1<<j);
+      if(j+1<w){
+        dp[i][j+1][k][nextS]+=dp[i][j][k][S];
+      }else{
+        dp[i+1][0][k][nextS]+=dp[i][j][k][S];
+      }
+    }
+    // 1*1
+    int nextS=S;
+    if(j+1<w){
+      dp[i][j+1][k+1][nextS]+=dp[i][j][k][S];
+    }else{
+      dp[i+1][0][k+1][nextS]+=dp[i][j][k][S];
+    }
+  }
+  int ans=0;
+  rep(S,1<<w){
+    ans+=dp[h-1][w-1][b][S];
+  }
+  cout<<ans<<endl;
+  // cout<<dp[h][0][b][0]<<endl;
+}
