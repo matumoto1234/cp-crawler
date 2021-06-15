25864addf7be402938638e7033015b0f099ba4cd
crawler <matumotosizuoka+crawler@gmail.com>
2021-06-15 09:31:18 +0000

    add ../AtCoder/abc205/abc205_b/23492189.cpp

---
+++ AtCoder/abc205/abc205_b/23492189.cpp
@@ -0,0 +1,33 @@
+// detail: https://atcoder.jp/contests/abc205/submissions/23492189
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+  int n;
+  vector<int> vec(n);
+  cin>>n;
+  for(int i=0;i<n;i++){
+    cin>>vec[i];
+  }
+  
+  sort(vec.begin(),vec.end());
+  bool answer = true;
+  
+  for(int i = 0; i < n; i++){
+  if(!(vec[i] == i+1)){
+    answer = false;  
+    break;
+  }
+  }
+  
+  if(answer)
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
+ 
+  return 0;
+}
+
