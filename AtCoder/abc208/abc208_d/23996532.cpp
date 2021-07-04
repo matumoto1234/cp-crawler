c28b8270ac5d03febe1b5bc0505cf70f1b60e0a6
crawler <matumotosizuoka+crawler@gmail.com>
2021-07-04 17:18:26 +0000

    add ../AtCoder/abc208/abc208_d/23996532.cpp

---
+++ AtCoder/abc208/abc208_d/23996532.cpp
@@ -0,0 +1,137 @@
+// detail: https://atcoder.jp/contests/abc208/submissions/23996532
+#include <bits/stdc++.h>
+using namespace std;
+
+// {{{
+
+// clang-format off
+#define rep(i, n) for (int i = 0; i < (int)(n); i += 1)
+#define rrep(i, n) for (int i = (int)(n)-1; i >= 0; i -= 1)
+#define range(i, l, r) for ( int i = (int)(l); i < (int)(r); (i) += 1 )
+#define rrange(i, l, r) for ( int i = (int)(r)-1; i >= (int)(l); (i) -= 1 )
+#define debug(...) debug_func(#__VA_ARGS__, __VA_ARGS__)
+template <typename T, typename... T2> void debug_func(string_view name, const T &a, T2 &&...rest) { stack<char> bs; string_view lbs = "({[<"; string_view rbs = ")}]>"; int end = name.size(); for ( int i = 0; i < (int)name.size(); i++ ) { if ( lbs.find(name[i]) != string::npos ) { bs.push(name[i]); } if ( rbs.find(name[i]) != string::npos ) { if ( !bs.empty() ) { bs.pop(); } } if ( name[i] == ',' && bs.empty() ) { end = i; break; } } cerr << name.substr(0, end) << ":" << a; if constexpr ( sizeof...(rest) == 0 ) { cerr << endl; } else { cerr << ' '; debug_func(name.substr(name.find_first_not_of(' ', end + 1)), forward<T2>(rest)...); } }
+template <typename T> vector<T> make_vector(size_t a, T b) { return vector<T>(a, b); }
+template <typename... Ts> auto make_vector(size_t a, Ts... ts) { return vector<decltype(make_vector(ts...))>(a, make_vector(ts...)); }
+template <typename T1, typename T2> inline bool chmax(T1 &a, T2 b) { return a < b && (a = b, true); }
+template <typename T1, typename T2> inline bool chmin(T1 &a, T2 b) { return a > b && (a = b, true); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const pair<T1, T2> &p) { os << p.first << ' ' << p.second; return os; }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const map<T1, T2> &v) { for ( pair<T1, T2> x : v ) { os << x << "\n"; } return os; }
+template <typename T> ostream &operator<<(ostream &os, queue<T> v) { if(!v.empty()) { os << v.front(); v.pop(); } while (!v.empty()) { os << " " << v.front(); v.pop(); } return os; }
+template <typename T> ostream &operator<<(ostream &os, stack<T> v) { if(!v.empty()) { os << v.top(); v.pop(); } while (!v.empty()) { os << " " << v.top(); v.pop(); } return os; }
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, const deque<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, const set<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+template <typename T> void operator+=(vector<T> &vs, T x) { for ( T &v : vs ) v += x; }
+template <typename T> void operator-=(vector<T> &vs, T x) { for ( T &v : vs ) v -= x; }
+template <typename T> void operator*=(vector<T> &vs, T x) { for ( T &v : vs ) v *= x; }
+template <typename T> void operator/=(vector<T> &vs, T x) { for ( T &v : vs ) v /= x; }
+template <typename T> void operator%=(vector<T> &vs, T x) { for ( T &v : vs ) v %= x; }
+template <typename T> vector<T> operator+(const vector<T> &vs, T x) { vector<T> res = vs; for ( T &v : res ) v += x; return res; }
+template <typename T> vector<T> operator-(const vector<T> &vs, T x) { vector<T> res = vs; for ( T &v : res ) v -= x; return res; }
+template <typename T> vector<T> operator*(const vector<T> &vs, T x) { vector<T> res = vs; for ( T &v : res ) v *= x; return res; }
+template <typename T> vector<T> operator/(const vector<T> &vs, T x) { vector<T> res = vs; for ( T &v : res ) v /= x; return res; }
+template <typename T> vector<T> operator%(const vector<T> &vs, T x) { vector<T> res = vs; for ( T &v : res ) v %= x; return res; }
+using ull = unsigned long long;
+using ll = long long;
+using Pll = pair<ll, ll>;
+using P = pair<int, int>;
+constexpr ll INF64 = INT64_MAX / 2;
+constexpr int INF32 = INT32_MAX / 2;
+constexpr int dy[] = { 0, -1, 1, 0, -1, 1, -1, 1 };
+constexpr int dx[] = { -1, 0, 0, 1, -1, -1, 1, 1 };
+constexpr int mod998244353 = 998244353;
+constexpr int mod1000000007 = (int)1e9 + 7;
+constexpr char newl = '\n';
+// clang-format on
+
+// }}}
+
+
+template <typename T>
+struct warshall_floyd {
+  vector<vector<T>> ds;
+  vector<vector<int>> ns;
+
+  T inf() { return numeric_limits<T>::max() / 2; }
+
+  warshall_floyd(int V) : ds(V, vector<T>(V, inf())) {
+    for ( int i = 0; i < V; i++ ) ds[i][i] = 0;
+  }
+
+  void add_edge(int from, int to, T cost) {
+    ds[from][to] = cost;
+  }
+
+  ll build() {
+    ll res=0;
+    int V = ds.size();
+
+    ns.resize(V, vector<int>(V));
+    for ( int i = 0; i < V; i++ ) {
+      for ( int j = 0; j < V; j++ ) {
+        ns[i][j] = j;
+      }
+    }
+
+    for ( int k = 0; k < V; k++ ) {
+      for ( int i = 0; i < V; i++ ) {
+        for ( int j = 0; j < V; j++ ) {
+          if ( ds[i][k] == inf() || ds[k][j] == inf() ) continue;
+          if ( ds[i][j] > ds[i][k] + ds[k][j] ) {
+            ds[i][j] = ds[i][k] + ds[k][j];
+            ns[i][j] = ns[i][k];
+          }
+        }
+      }
+      
+      range(i,0,V){
+        range(j,0,V){
+          if(ds[i][j]==inf()) continue;
+          res+=ds[i][j];
+        }
+      }
+    }
+    return res;
+  }
+
+  vector<T> &operator[](int k) { return ds[k]; }
+
+  bool neg_cycle() {
+    int V = ds.size();
+    for ( int i = 0; i < V; i++ ) {
+      if ( ds[i][i] < 0 ) return true;
+    }
+    return true;
+  }
+
+  vector<int> restore(int s, int g) {
+    vector<int> res;
+    for ( int v = s; v != g; v = ns[v][g] ) {
+      res.emplace_back(v);
+    }
+    res.emplace_back(g);
+    return res;
+  }
+};
+
+
+int main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  int n,m;
+  cin>>n>>m;
+  
+  warshall_floyd<ll> g(n);
+  range(i,0,m){
+    int a,b,c;
+    cin>>a>>b>>c;
+    a--,b--;
+    g.add_edge(a,b,c);
+  }
+
+  ll ans=g.build();
+  if(ans>=g.inf()) ans=0;
+  cout<<ans<<endl;
+}
