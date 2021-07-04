36e195fdfa4c0da300173413c527a079c6f726c6
matumoto1234 <matumotosizuoka@gmail.com>
2021-07-04 21:02:35 +0900

    "add ../atcoder/abc208/abc208_a/23952483.cpp"

---
+++ atcoder/abc208/abc208_a/23952483.cpp
@@ -0,0 +1,12 @@
+// detail: https://atcoder.jp/contests/abc208/submissions/23952483
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+  int a,b;
+  cin>>a>>b;
+  if(a<=b && b<=a*6){
+    cout<<"Yes"<<endl;
+  }else{
+    cout<<"No"<<endl;
+  }
+}
