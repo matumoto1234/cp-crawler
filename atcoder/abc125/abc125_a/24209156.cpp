8204638e275121f1bc70fe0dd2470f8f1037d78f
crawler <>
2021-07-14 00:53:37 +0900

    "add ../atcoder/abc125/abc125_a/24209156.cpp"

---
+++ atcoder/abc125/abc125_a/24209156.cpp
@@ -0,0 +1,23 @@
+// detail: https://atcoder.jp/contests/abc125/submissions/24209156
+#include <bits/stdc++.h>
+using namespace std;
+/* ループ処理 0からnまで*/
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+int main()
+{
+  int A, B, T;
+  cin >> A >> B >> T;
+  int sum = 0;
+  if (A > T)
+  {
+    cout << 0 << endl;
+    return 0;
+  }
+  for (int i = A; i <= (double)T + 0.5; i += A)
+  {
+    sum += B;
+  }
+  cout << sum << endl;
+  return 0;
+}
