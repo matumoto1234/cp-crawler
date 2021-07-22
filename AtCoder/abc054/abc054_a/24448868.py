c7597e8b27b11be5f852711637ac2c4a1e80a93b
crawler <matumotosizuoka+crawler@gmail.com>
2021-07-22 16:02:31 +0000

    add ../AtCoder/abc054/abc054_a/24448868.py

---
+++ AtCoder/abc054/abc054_a/24448868.py
@@ -0,0 +1,13 @@
+# detail: https://atcoder.jp/contests/abc054/submissions/24448868
+a,b=map(int,input().split())
+if a==1:
+  a+=13
+if b==1:
+  b+=13
+  
+if a>b:
+  print("Alice")
+elif a<b:
+  print("Bob")
+else:
+  print("Draw")
