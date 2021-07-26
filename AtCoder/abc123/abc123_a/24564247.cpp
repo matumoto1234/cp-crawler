c9e1610810e7182ae1fa0eeadd7ca819c0603ff9
crawler <matumotosizuoka+crawler@gmail.com>
2021-07-26 12:15:03 +0000

    add ../AtCoder/abc123/abc123_a/24564247.cpp

---
+++ AtCoder/abc123/abc123_a/24564247.cpp
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/abc123/submissions/24564247
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int a,b,c,d,e,k;
+  cin>>a>>b>>c>>d>>e>>k;
+  if(e-a<=k){
+    cout<<"Yay!"<<endl;
+  }else{
+    cout<<":("<<endl;
+  }
+}
