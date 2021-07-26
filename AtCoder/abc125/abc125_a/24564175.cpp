006d597ac374da59405d6c25a24a7c126c1a3a28
crawler <matumotosizuoka+crawler@gmail.com>
2021-07-26 12:10:23 +0000

    add ../AtCoder/abc125/abc125_a/24564175.cpp

---
+++ AtCoder/abc125/abc125_a/24564175.cpp
@@ -0,0 +1,9 @@
+// detail: https://atcoder.jp/contests/abc125/submissions/24564175
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int a,b,t;
+  cin>>a>>b>>t;
+  cout<<(t/a)*b<<endl;
+}
