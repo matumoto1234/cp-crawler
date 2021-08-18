f723d43cac1210b80d2e4305a587442fe144c628
crawler <matumotosizuoka+crawler@gmail.com>
2021-08-18 06:57:09 +0000

    add ../AtCoder/typical90/typical90_p/25134385.cpp

---
+++ AtCoder/typical90/typical90_p/25134385.cpp
@@ -0,0 +1,34 @@
+// detail: https://atcoder.jp/contests/typical90/submissions/25134385
+#include<stdio.h>
+
+int main(void){
+  int charge;
+  int coin[3];
+  scanf("%d%d%d%d",&charge,&coin[0],&coin[1],&coin[2]);
+  sort(a,a+3);
+  int N,i,j,k;
+  for(i = 0 ; ; i++ ){
+    for(j = 0 ; ; j++ ){
+      for(k = 0 ; ; k++){
+	N = coin[0] * i + coin[1] * j + coin[2] * k;
+	if( charge == N )break;
+	
+	if( charge < N ){
+	  N = N - coin[2] * k;
+	  break;
+	}
+      }
+      if(charge == N )break;
+      
+      if( charge < N ){
+	N = N - coin[1] * j;
+	break;
+      }
+    }
+    if( charge == N ){
+      printf("%d\n",i+j+k);
+      break;
+    }  
+  }
+  return 0;
+}
