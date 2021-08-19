6bb09517216ac028c6a66bc8f7f08dc02fb84a98
crawler <matumotosizuoka+crawler@gmail.com>
2021-08-19 13:29:18 +0000

    add ../AtCoder/arc086/arc086_b/25157316.cpp

---
+++ AtCoder/arc086/arc086_b/25157316.cpp
@@ -0,0 +1,176 @@
+// detail: https://atcoder.jp/contests/arc086/submissions/25157316
+#include <bits/stdc++.h>
+using namespace std;
+
+// {{{
+
+// clang-format off
+#define len(x) ((int)(x).size())
+#define _over_load(_1,_2,_3,_4,NAME,...) NAME
+#define range(...) _over_load(__VA_ARGS__, range4, range3, range2)(__VA_ARGS__)
+#define range2(i, r) for ( int i = 0; i < (int)(r); (i) += 1)
+#define range3(i, l, r) for ( int i = (int)(l); i < (int)(r); (i) += 1)
+#define range4(i, l, r, inc) for ( int i = (int)(l); i < (int)(r); (i) += (inc))
+#define rrange(...) _over_load(__VA_ARGS__, rrange4, rrange3, rrange2)(__VA_ARGS__)
+#define rrange2(i, r) for ( int i = (int)(r) - 1; i >= 0; (i) -= 1)
+#define rrange3(i, l, r) for ( int i = (int)(r) - 1; i >= (int)(l); (i) -= 1)
+#define rrange4(i, l, r, inc) for ( int i = (int)(r) - 1; i >= (int)(l); (i) -= inc)
+#define whole(f, x, ...) ([&](decltype((x)) container) { return (f)( begin(container), end(container), ## __VA_ARGS__); })(x)
+#define rwhole(f, x, ...) ([&](decltype((x)) container) { return (f)( rbegin(container), rend(container), ## __VA_ARGS__); })(x)
+#define debug(...) debug_function(#__VA_ARGS__, __VA_ARGS__)
+template <typename T, typename... T2> void debug_function(string_view name, const T &a, T2 &&...rest) {
+  stack<char> bs;
+  string_view lbs = "({[<", rbs = ")}]>";
+  int end = name.size();
+  for ( int i = 0; i < (int)name.size(); i++ ) {
+    if ( lbs.find(name[i]) != string::npos ) bs.push(name[i]);
+    if ( rbs.find(name[i]) != string::npos && !bs.empty() ) bs.pop();
+    if ( name[i] == ',' && bs.empty() ) {
+      end = i;
+      break;
+    }
+  }
+  cerr << name.substr(0, end) << ":" << a;
+  if constexpr ( sizeof...(rest) == 0 ) {
+    cerr << '\n';
+  } else {
+    cerr << ' ';
+    debug_function(name.substr(name.find_first_not_of(' ', end + 1)), forward<T2>(rest)...);
+  }
+}
+template <typename T> vector<T> make_vector(size_t a, T b) { return vector<T>(a, b); }
+template <typename... Ts> auto make_vector(size_t a, Ts... ts) { return vector<decltype(make_vector(ts...))>(a, make_vector(ts...)); }
+template <typename T1, typename T2> inline bool chmax(T1 &a, T2 b) { return a < b && (a = b, true); }
+template <typename T1, typename T2> inline bool chmin(T1 &a, T2 b) { return a > b && (a = b, true); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const pair<T1, T2> &p) { os << p.first << ' ' << p.second; return os; }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const map<T1, T2> &v) { for ( pair<T1, T2> x : v ) { os << "(" << x.first <<", " << x.second << ")" << (v.rbegin()->first == x.first ? "" : ", "); } return os; }
+template <typename T> ostream &operator<<(ostream &os, queue<T> v) { if(!v.empty()) { os << v.front(); v.pop(); } while (!v.empty()) { os << " " << v.front(); v.pop(); } return os; }
+template <typename T> ostream &operator<<(ostream &os, stack<T> v) { if(!v.empty()) { os << v.top(); v.pop(); } while (!v.empty()) { os << " " << v.top(); v.pop(); } return os; }
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, const deque<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, const set<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+template <typename T1, typename T2> istream &operator>>(istream &is, pair<T1, T2> &p) { is >> p.first >> p.second; return is; }
+template <typename T> istream &operator>>(istream &is, vector<T> &v) { for (T &in : v) is >> in; return is; }
+struct IoSetup { IoSetup(int x = 15) { cin.tie(nullptr); ios::sync_with_stdio(false); cout << fixed << setprecision(x); cerr << fixed << setprecision(x); } } iosetup;
+using ull = unsigned long long;
+using ll = long long;
+using Pll = pair<ll, ll>;
+using P = pair<int, int>;
+constexpr ll INF64 = INT64_MAX / 2;
+constexpr int INF32 = INT32_MAX / 2;
+constexpr int dy[] = { 0, -1, 1, 0, -1, 1, -1, 1 };
+constexpr int dx[] = { -1, 0, 0, 1, -1, -1, 1, 1 };
+constexpr int mod998244353 = 998244353;
+constexpr int mod1000000007 = (int)1e9 + 7;
+constexpr char newl = '\n';
+// clang-format on
+
+// }}}
+
+
+
+bool is_valid(vector<int> as){
+  range(i,len(as)-1){
+    if(as[i]>as[i+1]) return false;
+  }
+  return true;
+}
+
+
+bool solve1(int n,vector<int> as){
+  vector<P> ans;
+  rrange(i, 1, n) {
+    int &a = as[i], &na = as[i - 1];
+
+    if ( abs(a) > abs(na) ) {
+      // na:3 a:5 ok
+      // na:3 a:-5 ng
+      // na:-3 a:5 ok
+      // na:-3 a:-5 ng
+      if ( na > a and a < 0 ) {
+        // na:3 a:-5
+        // na:-3 a:-5
+        na += a;
+        na += a;
+        ans.emplace_back(i, i - 1);
+        ans.emplace_back(i, i - 1);
+      }
+    } else {
+      // na:5 a:3 ng
+      // na:5 a:-3 ng
+      // na:-5 a:3 ok
+      // na:-5 a:-3 ok
+      if ( na > a and na > 0 ) {
+        // na:5 a:3
+        // na:5 a:-3
+        a += na;
+        a += na;
+        ans.emplace_back(i - 1, i);
+        ans.emplace_back(i - 1, i);
+      }
+    }
+  }
+
+  // debug(as);
+  if(!is_valid(as)) return false;
+
+  for ( auto &[a, b] : ans ) {
+    a++, b++;
+  }
+
+  assert( len(ans) <= 2 * n );
+
+  cout << len(ans) << endl;
+  range(i, len(ans)) {
+    cout << ans[i] << newl; 
+  }
+  return true;
+}
+
+bool solve2(int n,vector<int> as){
+  vector<P> ans;
+  range(i, n - 1) {
+    int &l = as[i], &r = as[i + 1];
+
+    if ( abs(r) > abs(l) ) {
+      if ( l > r and r < 0 ) {
+        l += r;
+        l += r;
+        ans.emplace_back(i + 1, i);
+        ans.emplace_back(i + 1, i);
+      }
+    } else {
+      if ( l > r and l > 0 ) {
+        r += l;
+        r += l;
+        ans.emplace_back(i, i + 1);
+        ans.emplace_back(i, i + 1);
+      }
+    }
+  }
+
+  // debug(as);
+  if(!is_valid(as)) return false;
+
+  for ( auto &[a, b] : ans ) {
+    a++, b++;
+  }
+
+  assert( len(ans) <= 2 * n );
+
+  cout << len(ans) << endl;
+  range(i, len(ans)) {
+    cout << ans[i] << newl; 
+  }
+  return true;
+}
+
+int main() {
+  int n;
+  cin >> n;
+  vector<int> as(n);
+  cin >> as;
+
+  if(solve1(n,as)) return 0;
+  solve2(n,as);
+}
