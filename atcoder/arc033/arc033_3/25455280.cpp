ce03a8ac3b7a43ce239ce0e0db8f1d5035a95640
crawler <>
2021-08-30 04:52:32 +0900

    "add ../atcoder/arc033/arc033_3/25455280.cpp"

---
+++ atcoder/arc033/arc033_3/25455280.cpp
@@ -0,0 +1,121 @@
+// detail: https://atcoder.jp/contests/arc033/submissions/25455280
+#include <bits/stdc++.h>
+using namespace std;
+
+template <typename T>
+class fenwick_tree {
+private:
+  int n;
+  vector<T> dat;
+
+  // [1,r]
+  T sum(int r) {
+    T res = 0;
+    for ( int k = r; k > 0; k -= (k & -k) ) {
+      res += dat[k];
+    }
+    return res;
+  }
+
+public:
+  fenwick_tree(int n_) : n(n_ + 1), dat(n_ + 2, 0) {}
+
+  void add(int i, T x) {
+    for ( int k = ++i; k < n; k += (k & -k) ) {
+      dat[k] += x;
+    }
+  }
+
+  T get(int k) { return dat[k]; }
+
+  // [l,r)
+  T sum(int l, int r) { return sum(r) - sum(l); }
+
+  // min({x | sum(x) >= w})
+  int lower_bound(T w) {
+    if ( w <= 0 ) return 0;
+    int x = 0, twopow = 1;
+    while ( twopow < n ) {
+      twopow <<= 1;
+    }
+    for ( int sz = twopow; sz > 0; sz >>= 1 ) {
+      if ( x + sz <= n && dat[x + sz] < w ) {
+        w -= dat[x + sz];
+        x += sz;
+      }
+    }
+    x++;
+    return x;
+  }
+
+  // min({x | sum(x) > w})
+  int upper_bound(T w) { return lower_bound(w + 1); }
+};
+
+
+
+template<typename T>
+class BIT{
+private:
+  // \sum_{j < i}  v[j]
+  T sum(int i){
+    T s(0);
+    for(int x=i;x>0;x-=(x&-x))
+      s+=bit[x];
+    return s;
+  }
+public:
+  int n;
+  vector<T> bit;
+  BIT(int n_):n(n_+1),bit(n_+2,0){}
+
+  // v[i] += a
+  void add(int i,T a){
+    for(int x=++i;x<=n;x+=(x&-x))
+      bit[x]+=a;
+  }
+
+  // \sum_{l <= i < r} v[i]
+  T query(int l,int r){return sum(r)-sum(l);}
+
+  // min({x | sum(x) >= w})
+  int lower_bound(const T w){
+    if(w<=0) return 0;
+    T r=w;
+    int x=0,p=1;
+    while(p<n) p<<=1;
+    for(int k=p;k>0;k>>=1){
+      if(x+k<=n and bit[x+k]<r){
+        r-=bit[x+k];
+        x+=k;
+      }
+    }
+    x++;
+    assert(sum(x-1)<w and sum(x)>=w);
+    return x;
+  }
+
+  // min({x | sum(x) > w})
+  int upper_bound(T w){return lower_bound(w+1);}
+};
+
+
+// ARC033_C
+int main() {
+  int q;
+  cin >> q;
+
+  // fenwick_tree<int> xset(200000);
+  BIT<int> xset(300000);
+  for ( int i = 0; i < q; i++ ) {
+    int t, x;
+    cin >> t >> x;
+    if ( t == 1 ) {
+      xset.add(x, 1);
+    } else {
+      int v = xset.lower_bound(x);
+      cout << v - 1 << endl;
+      xset.add(v, -1);
+    }
+  }
+}
