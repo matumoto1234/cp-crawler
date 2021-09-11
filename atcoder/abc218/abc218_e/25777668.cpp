182d72318782483420f01aa898ff1d139a758d72
matumoto1234 <matumotosizuoka@gmail.com>
2021-09-11 22:16:07 +0900

    "add ../atcoder/abc218/abc218_e/25777668.cpp"

---
+++ atcoder/abc218/abc218_e/25777668.cpp
@@ -0,0 +1,152 @@
+// detail: https://atcoder.jp/contests/abc218/submissions/25777668
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// {{{
+
+// clang-format off
+#define len(x) ((int)(x).size())
+#define _over_load(_1,_2,_3,_4,NAME,...) NAME
+#define range(...) _over_load(__VA_ARGS__, range4, range3, range2)(__VA_ARGS__)
+#define range2(i, r) for ( int i = 0; i < (int)(r); (i) += 1)
+#define range3(i, l, r) for ( int i = (int)(l); i < (int)(r); (i) += 1)
+#define range4(i, l, r, inc) for ( int i = (int)(l); i < (int)(r); (i) += (inc))
+#define rrange(...) _over_load(__VA_ARGS__, rrange4, rrange3, rrange2)(__VA_ARGS__)
+#define rrange2(i, r) for ( int i = (int)(r) - 1; i >= 0; (i) -= 1)
+#define rrange3(i, l, r) for ( int i = (int)(r) - 1; i >= (int)(l); (i) -= 1)
+#define rrange4(i, l, r, inc) for ( int i = (int)(r) - 1; i >= (int)(l); (i) -= inc)
+#define whole(f, x, ...) ([&](decltype((x)) container) { return (f)( begin(container), end(container), ## __VA_ARGS__); })(x)
+#define rwhole(f, x, ...) ([&](decltype((x)) container) { return (f)( rbegin(container), rend(container), ## __VA_ARGS__); })(x)
+#define debug(...) debug_function(#__VA_ARGS__, __VA_ARGS__)
+template <typename T, typename... T2> void debug_function(string_view name, const T &a, T2 &&...rest) {
+  stack<char> bs;
+  string_view lbs = "({[<", rbs = ")}]>";
+  int end = name.size();
+  for ( int i = 0; i < (int)name.size(); i++ ) {
+    if ( lbs.find(name[i]) != string::npos ) bs.push(name[i]);
+    if ( rbs.find(name[i]) != string::npos && !bs.empty() ) bs.pop();
+    if ( name[i] == ',' && bs.empty() ) {
+      end = i;
+      break;
+    }
+  }
+  cerr << name.substr(0, end) << ":" << a;
+  if constexpr ( sizeof...(rest) == 0 ) {
+    cerr << '\n';
+  } else {
+    cerr << ' ';
+    debug_function(name.substr(name.find_first_not_of(' ', end + 1)), forward<T2>(rest)...);
+  }
+}
+template <typename T> vector<T> make_vector(size_t a, T b) { return vector<T>(a, b); }
+template <typename... Ts> auto make_vector(size_t a, Ts... ts) { return vector<decltype(make_vector(ts...))>(a, make_vector(ts...)); }
+template <typename T1, typename T2> inline bool chmax(T1 &a, T2 b) { return a < b && (a = b, true); }
+template <typename T1, typename T2> inline bool chmin(T1 &a, T2 b) { return a > b && (a = b, true); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const pair<T1, T2> &p) { os << p.first << ' ' << p.second; return os; }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const map<T1, T2> &v) { for ( pair<T1, T2> x : v ) { os << "(" << x.first <<", " << x.second << ")" << (v.rbegin()->first == x.first ? "" : ", "); } return os; }
+template <typename T> ostream &operator<<(ostream &os, queue<T> v) { if(!v.empty()) { os << v.front(); v.pop(); } while (!v.empty()) { os << " " << v.front(); v.pop(); } return os; }
+template <typename T> ostream &operator<<(ostream &os, stack<T> v) { if(!v.empty()) { os << v.top(); v.pop(); } while (!v.empty()) { os << " " << v.top(); v.pop(); } return os; }
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, const deque<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, const set<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+template <typename T1, typename T2> istream &operator>>(istream &is, pair<T1, T2> &p) { is >> p.first >> p.second; return is; }
+template <typename T> istream &operator>>(istream &is, vector<T> &v) { for (T &in : v) is >> in; return is; }
+struct IoSetup { IoSetup(int x = 15) { cin.tie(nullptr); ios::sync_with_stdio(false); cout << fixed << setprecision(x); cerr << fixed << setprecision(x); } } iosetup;
+using ull = unsigned long long;
+using ll = long long;
+using Pll = pair<ll, ll>;
+using P = pair<int, int>;
+constexpr ll INF64 = INT64_MAX / 2;
+constexpr int INF32 = INT32_MAX / 2;
+constexpr int dy[] = { 0, -1, 1, 0, -1, 1, -1, 1 };
+constexpr int dx[] = { -1, 0, 0, 1, -1, -1, 1, 1 };
+constexpr int mod998244353 = 998244353;
+constexpr int mod1000000007 = (int)1e9 + 7;
+constexpr char newl = '\n';
+// clang-format on
+
+// }}}
+
+
+class union_find {
+private:
+  int grp_cnt, merge_cnt;
+  vector<int> siz, par;
+
+public:
+  union_find(int N) : grp_cnt(N), merge_cnt(0), siz(N, 1), par(N) {
+    iota(par.begin(), par.end(), 0);
+  }
+
+  int root(int x) {
+    if ( x == par[x] ) return x;
+    return par[x] = root(par[x]);
+  }
+
+  int group_count() { return grp_cnt; }
+
+  int merge_count() { return merge_cnt; }
+
+  int size(int x) { return siz[root(x)]; }
+
+  bool same(int x, int y) { return root(x) == root(y); }
+
+  bool merge(int x, int y) {
+    x = root(x);
+    y = root(y);
+    if ( x == y ) return false;
+    if ( siz[x] < siz[y] ) swap(x, y);
+    siz[x] += siz[y];
+    par[y] = x;
+    grp_cnt--;
+    merge_cnt++;
+    return true;
+  }
+
+  // Θ(NlogN)
+  // 2つのunion_findでi番目の頂点と同じ連結成分であるものの個数(i番目の頂点を含む)
+  vector<int> connect_count(union_find tree) {
+    map<pair<int, int>, int> mp;
+
+    int N = par.size();
+    for ( int i = 0; i < N; i++ ) {
+      pair<int, int> p = make_pair(root(i), tree.root(i));
+      mp[p]++;
+    }
+
+    vector<int> res(N);
+    for ( int i = 0; i < N; i++ ) {
+      pair<int, int> p = make_pair(root(i), tree.root(i));
+      res[i] = mp[p];
+    }
+    return res;
+  }
+};
+
+int main() {
+  int n,m;
+  cin>>n>>m;
+
+  vector<pair<int,P>> es;
+  range(i,m){
+    int a,b,c;
+    cin>>a>>b>>c;
+    a--,b--;
+    es.emplace_back(c,make_pair(a,b));
+  }
+
+  whole(sort,es);
+
+  union_find uf(n);
+  ll ans=0;
+  range(i,m){
+    auto [cost,p]=es[i];
+    auto [a,b]=p;
+
+    if(!uf.merge(a,b) and cost>=0){
+      ans+=cost;
+    }
+  }
+  cout<<ans<<endl;
+}
