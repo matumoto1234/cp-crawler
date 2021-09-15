5a40a599eccad219e46f24a1c4f2dbab07222c8f
crawler <matumotosizuoka+crawler@gmail.com>
2021-09-15 14:19:15 +0000

    add ../AtCoder/abc218/abc218_a/25865427.cpp

---
+++ AtCoder/abc218/abc218_a/25865427.cpp
@@ -0,0 +1,8 @@
+// detail: https://atcoder.jp/contests/abc218/submissions/25865427
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  static_assert(false,"test");
+  cout<<"hello"<<endl;
+}
