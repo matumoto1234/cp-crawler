d00012ab0b3c3c1a8430a6bf9dfe260613158d4a
matumoto1234 <matumotosizuoka@gmail.com>
2021-10-01 00:08:26 +0900

    "add ../atcoder/tenka1-2013-quala/tenka1_2013_qualA_a/26243826.ts"

---
+++ atcoder/tenka1-2013-quala/tenka1_2013_qualA_a/26243826.ts
@@ -0,0 +1,11 @@
+// detail: https://atcoder.jp/contests/tenka1-2013-quala/submissions/26243826
+'use strict';
+
+(() => {
+  let x: number = 42;
+  const limit: number = 130000000;
+  while (x < limit) {
+    x *= 2;
+  }
+  console.log(x);
+})();
