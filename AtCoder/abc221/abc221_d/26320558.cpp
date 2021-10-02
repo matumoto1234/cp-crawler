1c10a9ca76fc556a6fa851504564116a52d48eb1
crawler <matumotosizuoka+crawler@gmail.com>
2021-10-02 14:44:25 +0000

    add ../AtCoder/abc221/abc221_d/26320558.cpp

---
+++ AtCoder/abc221/abc221_d/26320558.cpp
@@ -0,0 +1,142 @@
+// detail: https://atcoder.jp/contests/abc221/submissions/26320558
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// {{{
+
+// clang-format off
+#define len(x) (static_cast<int>((x).size()))
+#define _over_load(_1,_2,_3,_4,NAME,...) NAME
+#define range(...) _over_load(__VA_ARGS__, range4, range3, range2)(__VA_ARGS__)
+#define range2(i, r) for ( int i = 0; i < static_cast<int>(r); (i) += 1)
+#define range3(i, l, r) for ( int i = static_cast<int>(l); i < static_cast<int>(r); (i) += 1)
+#define range4(i, l, r, step) for ( int i = static_cast<int>(l); i < static_cast<int>(r); (i) += (step))
+#define rrange(...) _over_load(__VA_ARGS__, rrange4, rrange3, rrange2)(__VA_ARGS__)
+#define rrange2(i, r) for ( int i = static_cast<int>(r) - 1; i >= 0; (i) -= 1)
+#define rrange3(i, l, r) for ( int i = static_cast<int>(r) - 1; i >= static_cast<int>(l); (i) -= 1)
+#define rrange4(i, l, r, step) for ( int i = static_cast<int>(r) - 1; i >= static_cast<int>(l); (i) -= (step))
+#define whole(f, x, ...) ([&](decltype((x)) container) { return (f)( begin(container), end(container), ## __VA_ARGS__); })(x)
+#define rwhole(f, x, ...) ([&](decltype((x)) container) { return (f)( rbegin(container), rend(container), ## __VA_ARGS__); })(x)
+#define debug(...) debug_function(#__VA_ARGS__, __VA_ARGS__)
+template <typename T, typename... T2> void debug_function(string_view name, const T &a, T2 &&...rest) {
+  stack<char> bs;
+  string_view lbs = "({[<", rbs = ")}]>";
+  int end = name.size();
+  for ( int i = 0; i < len(name); i++ ) {
+    if ( lbs.find(name[i]) != string::npos ) bs.push(name[i]);
+    if ( rbs.find(name[i]) != string::npos and !bs.empty() ) bs.pop();
+    if ( name[i] == ',' and bs.empty() ) {
+      end = i;
+      break;
+    }
+  }
+  cerr << name.substr(0, end) << ":" << a;
+  if constexpr ( sizeof...(rest) == 0 ) {
+    cerr << '\n';
+  } else {
+    cerr << ' ';
+    debug_function(name.substr(name.find_first_not_of(' ', end + 1)), forward<T2>(rest)...);
+  }
+}
+template <typename T> vector<T> make_vector(size_t a, T b) { return vector<T>(a, b); }
+template <typename... Ts> auto make_vector(size_t a, Ts... ts) { return vector<decltype(make_vector(ts...))>(a, make_vector(ts...)); }
+template <typename T1, typename T2> inline bool chmax(T1 &a, T2 b) { return a < b and (a = b, true); }
+template <typename T1, typename T2> inline bool chmin(T1 &a, T2 b) { return a > b and (a = b, true); }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const pair<T1, T2> &p) { os << p.first << ' ' << p.second; return os; }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const map<T1, T2> &v) { for ( pair<T1, T2> x : v ) { os << "(" << x.first <<", " << x.second << ")" << (v.rbegin()->first == x.first ? "" : ", "); } return os; }
+template <typename T> ostream &operator<<(ostream &os, queue<T> v) { if(!v.empty()) { os << v.front(); v.pop(); } while (!v.empty()) { os << " " << v.front(); v.pop(); } return os; }
+template <typename T> ostream &operator<<(ostream &os, stack<T> v) { if(!v.empty()) { os << v.top(); v.pop(); } while (!v.empty()) { os << " " << v.top(); v.pop(); } return os; }
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &v) { range(i, len(v)) { cout << v[i] << (i == len(v) - 1 ? "" : " "); } return os; }
+template <typename T> ostream &operator<<(ostream &os, const deque<T> &v) { range(i, len(v)) { cout << v[i] << (i == len(v) - 1 ? "" : " "); } return os; }
+template <typename T> ostream &operator<<(ostream &os, const set<T> &v) { bool is_f = true; for ( T x : v ) { os << (is_f ? "" : " ") << x; is_f = false; } return os; }
+template <typename T1, typename T2> istream &operator>>(istream &is, pair<T1, T2> &p) { is >> p.first >> p.second; return is; }
+template <typename T> istream &operator>>(istream &is, vector<T> &v) { for (T &in : v) is >> in; return is; }
+struct IoSetup { IoSetup(int x = 15) { cin.tie(nullptr); ios::sync_with_stdio(false); cout << fixed << setprecision(x); cerr << fixed << setprecision(x); } } iosetup;
+using ull = unsigned long long;
+using ll = long long;
+using Pll = pair<ll, ll>;
+using P = pair<int, int>;
+constexpr ll INF64 = INT64_MAX / 2;
+constexpr int INF32 = INT32_MAX / 2;
+constexpr int dy[] = { 0, -1, 1, 0, -1, 1, -1, 1 };
+constexpr int dx[] = { -1, 0, 0, 1, -1, -1, 1, 1 };
+constexpr int mod998244353 = 998244353;
+constexpr int mod1000000007 = static_cast<int>(1e9) + 7;
+constexpr char newl = '\n';
+// clang-format on
+
+// }}}
+
+
+template <typename T>
+struct compress {
+  vector<T> xs;
+  compress() {}
+  compress(int N) : xs(N, 0) {}
+  compress(const vector<T> &vs) : xs(vs) {}
+
+  void add(T x) { xs.emplace_back(x); }
+  void add(const vector<T> &vs) {
+    for ( const T &x : vs ) {
+      xs.emplace_back(x);
+    }
+  }
+
+  void build() {
+    sort(xs.begin(), xs.end());
+    xs.erase(unique(xs.begin(), xs.end()), xs.end());
+  }
+
+  vector<T> get(const vector<T> &vs) {
+    vector<T> res = vs;
+    for ( T &x : res ) {
+      x = lower_bound(xs.begin(), xs.end(), x) - xs.begin();
+    }
+    return res;
+  }
+
+  int get(T k) const {
+    return lower_bound(xs.begin(), xs.end(), k) - xs.begin();
+  }
+
+  const T &operator[](int k) const { return xs[k]; }
+};
+
+
+int main() {
+  int n;
+  cin>>n;
+
+  vector<P> ab(n);
+  compress<int> cmp;
+  for(auto &[a,b]:ab){
+    cin>>a>>b;
+    cmp.add(a);
+    cmp.add(a+b);
+  }
+
+  cmp.build();
+
+  vector<int> imos(len(cmp.xs),0);
+  for(auto [a,b]:ab){
+    imos[cmp.get(a)]++;
+    imos[cmp.get(a+b)]--;
+  }
+
+  range(i,1,len(imos)){
+    imos[i]+=imos[i-1];
+  }
+
+  // debug(imos);
+
+  vector<int> ans(1e6,0);
+  int pre=1;
+  range(i,len(imos)){
+    ans[imos[i]]=cmp[i] - pre + 1;
+    pre=cmp[i];
+  }
+
+  range(i,n-1) cout<<ans[i]<<' ';
+  cout<<ans[n]<<endl;
+}
