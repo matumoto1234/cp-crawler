7c7decd693fc69516a15b348264522688a921fc1
crawler <matumotosizuoka+crawler@gmail.com>
2021-11-27 13:39:54 +0000

    add ../AtCoder/abc229/abc229_d/27548308.cpp

---
+++ AtCoder/abc229/abc229_d/27548308.cpp
@@ -0,0 +1,176 @@
+// detail: https://atcoder.jp/contests/abc229/submissions/27548308
+#include <bits/stdc++.h>
+using namespace std;
+
+// {{{ Templates
+
+// clang-format off
+
+// Macros
+#define over_load_(_1,_2,_3,_4,NAME,...) NAME
+#define rep(...) over_load_(__VA_ARGS__, rep4, rep3, rep2)(__VA_ARGS__)
+#define rep2(i, r) for ( int i = 0; i < static_cast<int>(r); (i) += 1)
+#define rep3(i, l, r) for ( int i = static_cast<int>(l); i < static_cast<int>(r); (i) += 1)
+#define rep4(i, l, r, stride) for ( int i = static_cast<int>(l); i < static_cast<int>(r); (i) += (stride))
+#define rrep(...) over_load_(__VA_ARGS__, rrep4, rrep3, rrep2)(__VA_ARGS__)
+#define rrep2(i, r) for ( int i = static_cast<int>(r) - 1; i >= 0; (i) -= 1)
+#define rrep3(i, l, r) for ( int i = static_cast<int>(r) - 1; i >= static_cast<int>(l); (i) -= 1)
+#define rrep4(i, l, r, stride) for ( int i = static_cast<int>(r) - 1; i >= static_cast<int>(l); (i) -= (stride))
+#define len(x) (static_cast<int>((x).size()))
+#define whole(f, x, ...) ([&](decltype((x)) container) { return (f)( begin(container), end(container), ## __VA_ARGS__); })(x)
+#define rwhole(f, x, ...) ([&](decltype((x)) container) { return (f)( rbegin(container), rend(container), ## __VA_ARGS__); })(x)
+#define debug(...) debug_function(#__VA_ARGS__, __VA_ARGS__)
+
+// Operators
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const pair<T1, T2> &p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const map<T1, T2> &v) { bool is_first = true; for (auto x: v) { os << (is_first ? "" : " ") << x; is_first = false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, queue<T> v) { bool is_first = true; while (!v.empty()) { os << (is_first?"":" ")<<v.front(); v.pop(); is_first = false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, stack<T> v) { bool is_first = true; while (!v.empty()) { os << (is_first?"":" ") << v.top(); v.pop(); is_first=false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &v) { rep (i, len(v)) cout << v[i] << (i == len(v) - 1 ? "" : " "); return os; }
+template <typename T> ostream &operator<<(ostream &os, const vector<vector<T>> &v) { for (const auto &vec: v) { cout << vec << '\n'; } return os; }
+template <typename T> ostream &operator<<(ostream &os, const deque<T> &v) { rep (i, len(v)) cout << v[i] << (i == len(v) - 1 ? "" : " "); return os; }
+template <typename T> ostream &operator<<(ostream &os, const set<T> &v) { bool is_first = true; for (T x: v) { os << (is_first ? "" : " ") << x; is_first = false; } return os; }
+template <typename T> istream &operator>>(istream &is, vector<T> &v) { for (T &in: v) { is >> in; } return is; }
+
+// For debug macro
+int find_comma_not_bracketed(string_view s){ stack<char> bs; string lbs = "({[", rbs = ")}]"; for (size_t i = 0; i < s.size(); i++) { if (lbs.find(s[i]) != string::npos) bs.push(s[i]); if (rbs.find(s[i]) != string::npos and !bs.empty()) bs.pop(); if (s[i] == ',' and bs.empty()) return i; } return s.size(); }
+template <typename T, typename... Ts> void debug_function(string_view name, const T &a, Ts &&...rest) { int end = find_comma_not_bracketed(name); cerr << name.substr(0, end) << ":" << a; if constexpr (sizeof...(rest) == 0) { cerr << '\n'; } else { cerr << ' '; debug_function(name.substr(name.find_first_not_of(' ', end + 1)), forward<Ts>(rest)...); } }
+
+// Functions
+template <typename T> vector<T> make_vector(size_t a, T b) { return vector<T>(a, b); }
+template <typename... Ts> auto make_vector(size_t a, Ts... ts) { return vector<decltype(make_vector(ts...))>(a, make_vector(ts...)); }
+template <typename T1, typename T2> inline bool chmax(T1 &a, T2 b) { return a < b and (a = b, true); }
+template <typename T1, typename T2> inline bool chmin(T1 &a, T2 b) { return a > b and (a = b, true); }
+
+// Structs
+struct IoSetup { IoSetup(int x = 15) { cin.tie(nullptr); ios::sync_with_stdio(false); cout << fixed << setprecision(x); cerr << fixed << setprecision(x); } } iosetup;
+
+// Type aliases
+using ull = unsigned long long;
+using ll = long long;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+
+// Literals
+constexpr ll INF64 = INT64_MAX / 2;
+constexpr int INF32 = INT32_MAX / 2;
+constexpr int dy[] = { 0, -1, 1, 0, -1, 1, -1, 1 };
+constexpr int dx[] = { -1, 0, 0, 1, -1, -1, 1, 1 };
+constexpr int mod998244353 = 998244353;
+constexpr int mod1000000007 = static_cast<int>(1e9) + 7;
+constexpr char newl = '\n';
+
+// clang-format on
+
+// }}} Templates
+
+
+namespace tools {
+  using namespace std;
+}
+
+#include <vector>
+
+namespace tools {
+  template <typename Iterator>
+  auto runlength(Iterator first, Iterator last) {
+    vector<pair<decltype(*first), int>> res;
+    for (auto it = first; it != last; it++) {
+      if (res.empty() or res.back().first != *it) res.emplace_back(*it, 0);
+      res.back().second++;
+    }
+    return res;
+  }
+} // namespace tools
+using namespace tools;
+
+namespace data_structure {
+  using namespace std;
+}
+
+#include <vector>
+
+namespace data_structure {
+  template <typename T>
+  struct CumulativeSum {
+    vector<T> data;
+    CumulativeSum(int n): data(n + 1, pair(0, 0)) {}
+    CumulativeSum(int n, T x): data(n + 1, x) {}
+
+    void set(int k, T x) { data[k + 1] = x; }
+
+    void add(int k, T x) { data[k + 1] = x; }
+
+    T get(int k) { return data[k + 1]; }
+
+    void build() {
+      for (int i = 0; i < static_cast<int>(data.size()) - 1; i++) {
+        data[i + 1].first += data[i].first;
+        data[i + 1].second += data[i].second;
+      }
+    }
+
+    // [l,r)
+    T query(int l, int r) { return pair(data[r].first - data[l].first, data[r].second - data[l].second); }
+  };
+} // namespace data_structure
+using namespace data_structure;
+
+int main() {
+  string s;
+  int k;
+
+  cin >> s >> k;
+
+  auto vs = whole(runlength, s);
+  char huge = 'X';
+  char &hoge = huge;
+  char fuga = '.';
+  char &hoge2 = fuga;
+  if (vs.front().first == 'X') { vs.insert(vs.begin(), pair<char&, int>(hoge2, 0)); }
+  if (vs.back().first == '.') { vs.emplace_back(hoge, 0); }
+
+  vector<pll> vs2;
+  rep(i, 0, len(vs), 2) {
+    auto [dot, cnt_dot] = vs[i];
+    auto [x, cnt_x] = vs[i + 1];
+    vs2.emplace_back(cnt_dot, cnt_x);
+  }
+
+  int n = len(vs2);
+  CumulativeSum<pll> sum(n);
+
+  rep(i, n) {
+    auto [cost, val] = vs2[i];
+
+    sum.set(i, pair(cost, val));
+  }
+
+  sum.build();
+
+  ll ans = 0;
+
+  rep(i, n - 1) {
+    ll valid = i + 1;
+    ll invalid = n;
+    auto is_valid = [&](ll m) {
+      auto [cost, val] = sum.query(i + 1, m);
+      return cost <= k;
+    };
+    while (abs(valid - invalid) > 1) {
+      ll mid = (valid + invalid) / 2;
+      if (is_valid(mid))
+        valid = mid;
+      else
+        invalid = mid;
+    }
+
+    auto [cost, val] = sum.query(i + 1, valid);
+    val += sum.query(i, i + 1).second;
+    // debug(cost);
+
+    chmax(ans, val + cost);
+  }
+
+  cout << ans << endl;
+}
