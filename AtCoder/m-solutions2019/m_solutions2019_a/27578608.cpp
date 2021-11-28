c51b286b35a66cf0f3a00111164f4bfb6cc25ea0
crawler <matumotosizuoka+crawler@gmail.com>
2021-11-28 14:01:07 +0000

    add ../AtCoder/m-solutions2019/m_solutions2019_a/27578608.cpp

---
+++ AtCoder/m-solutions2019/m_solutions2019_a/27578608.cpp
@@ -0,0 +1,9 @@
+// detail: https://atcoder.jp/contests/m-solutions2019/submissions/27578608
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int n;
+  cin>>n;
+  cout<<(n-2)*180<<endl;
+}
