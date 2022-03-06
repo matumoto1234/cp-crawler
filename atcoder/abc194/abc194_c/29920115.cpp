947611f63ab0f2c1779effe31e9468a20cc40efe
matumoto1234 <matumotosizuoka@gmail.com>
2022-03-06 18:36:38 +0900

    "add ../atcoder/abc194/abc194_c/29920115.cpp"

---
+++ atcoder/abc194/abc194_c/29920115.cpp
@@ -0,0 +1,17 @@
+// detail: https://atcoder.jp/contests/abc194/submissions/29920115
+int main(){
+    int n;
+    cin >> n;
+    vector<ll> A(n),cnt(405);//Aiが何回出るかカウント
+    rep(i,0,n) {
+        cin >> A[i];
+        cnt[A[i]+200]++;
+    }
+    ll ans=0;
+    for(ll i=0;i<=400;i++){
+        for(ll j=i;j<=400;j++){
+            ans+=cnt[i]*cnt[j]*(j-i)*(j-i);
+        }
+    }
+    cout << ans ;
+}
