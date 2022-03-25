c52ccd9a50dacce256d13d5ebe256448d95571f2
matumoto1234 <matumotosizuoka@gmail.com>
2022-03-25 11:16:54 +0900

    "add ../atcoder/cf17-final/cf17_final_a/30393745.cpp"

---
+++ atcoder/cf17-final/cf17_final_a/30393745.cpp
@@ -0,0 +1,139 @@
+// detail: https://atcoder.jp/contests/cf17-final/submissions/30393745
+#include <bits/stdc++.h>
+using namespace std;
+
+// {{{ Templates
+
+// clang-format off
+
+// Macros
+#define over_load_(_1,_2,_3,_4,NAME,...) NAME
+#define rep(...) over_load_(__VA_ARGS__, rep4, rep3, rep2)(__VA_ARGS__)
+#define rep2(i, r) for ( int i = 0; i < static_cast<int>(r); (i) += 1)
+#define rep3(i, l, r) for ( int i = static_cast<int>(l); i < static_cast<int>(r); (i) += 1)
+#define rep4(i, l, r, stride) for ( int i = static_cast<int>(l); i < static_cast<int>(r); (i) += (stride))
+#define rrep(...) over_load_(__VA_ARGS__, rrep4, rrep3, rrep2)(__VA_ARGS__)
+#define rrep2(i, r) for ( int i = static_cast<int>(r) - 1; i >= 0; (i) -= 1)
+#define rrep3(i, l, r) for ( int i = static_cast<int>(r) - 1; i >= static_cast<int>(l); (i) -= 1)
+#define rrep4(i, l, r, stride) for ( int i = static_cast<int>(r) - 1; i >= static_cast<int>(l); (i) -= (stride))
+#define len(x) (static_cast<int>((x).size()))
+#define whole(f, x, ...) ([&](decltype((x)) container) { return (f)( begin(container), end(container), ## __VA_ARGS__); })(x)
+#define rwhole(f, x, ...) ([&](decltype((x)) container) { return (f)( rbegin(container), rend(container), ## __VA_ARGS__); })(x)
+#define debug(...) debug_function(#__VA_ARGS__, __VA_ARGS__)
+
+// Operators
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const pair<T1, T2> &p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const map<T1, T2> &v) { bool is_first = true; for (auto x: v) { os << (is_first ? "" : " ") << x; is_first = false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, queue<T> v) { bool is_first = true; while (!v.empty()) { os << (is_first?"":" ")<<v.front(); v.pop(); is_first = false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, stack<T> v) { bool is_first = true; while (!v.empty()) { os << (is_first?"":" ") << v.top(); v.pop(); is_first=false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &v) { rep (i, len(v)) os << v[i] << (i == len(v) - 1 ? "" : " "); return os; }
+template <typename T> ostream &operator<<(ostream &os, const vector<vector<T>> &v) { for (const auto &vec: v) { os << vec << '\n'; } return os; }
+template <typename T> ostream &operator<<(ostream &os, const deque<T> &v) { rep (i, len(v)) os << v[i] << (i == len(v) - 1 ? "" : " "); return os; }
+template <typename T> ostream &operator<<(ostream &os, const set<T> &v) { bool is_first = true; for (T x: v) { os << (is_first ? "" : " ") << x; is_first = false; } return os; }
+template <typename T> istream &operator>>(istream &is, vector<T> &v) { for (T &in: v) { is >> in; } return is; }
+
+// For debug macro
+int find_comma_not_bracketed(string_view s){ stack<char> bs; string lbs = "({[", rbs = ")}]"; for (size_t i = 0; i < s.size(); i++) { if (lbs.find(s[i]) != string::npos) bs.push(s[i]); if (rbs.find(s[i]) != string::npos and !bs.empty()) bs.pop(); if (s[i] == ',' and bs.empty()) return i; } return s.size(); }
+template <typename T, typename... Ts> void debug_function(string_view name, const T &a, Ts &&...rest) { int end = find_comma_not_bracketed(name); cerr << name.substr(0, end) << ":" << a; if constexpr (sizeof...(rest) == 0) { cerr << '\n'; } else { cerr << ' '; debug_function(name.substr(name.find_first_not_of(' ', end + 1)), forward<Ts>(rest)...); } }
+
+// Functions
+template <typename T> vector<T> make_vector(size_t a, T b) { return vector<T>(a, b); }
+template <typename... Ts> auto make_vector(size_t a, Ts... ts) { return vector<decltype(make_vector(ts...))>(a, make_vector(ts...)); }
+template <typename T1, typename T2> inline bool chmax(T1 &a, T2 b) { return a < b and (a = b, true); }
+template <typename T1, typename T2> inline bool chmin(T1 &a, T2 b) { return a > b and (a = b, true); }
+
+// Structs
+struct IoSetup { IoSetup(int x = 15) { cin.tie(nullptr); ios::sync_with_stdio(false); cout << fixed << setprecision(x); cerr << fixed << setprecision(x); } } iosetup;
+
+// Type aliases
+using ull = unsigned long long;
+using ll = long long;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+
+// Literals
+constexpr ll INF64 = INT64_MAX / 2;
+constexpr int INF32 = INT32_MAX / 2;
+constexpr int dy[] = { 0, 1, -1, 0, -1, 1, -1, 1 };
+constexpr int dx[] = { 1, 0, 0, -1, -1, -1, 1, 1 };
+constexpr int mod998244353 = 998244353;
+constexpr int mod1000000007 = static_cast<int>(1e9) + 7;
+constexpr char newl = '\n';
+
+// clang-format on
+
+// }}} Templates
+
+string remove_A(string s) {
+  string res = "";
+  rep(i, len(s)) {
+    if (s[i] == 'A')
+      continue;
+
+    res.push_back(s[i]);
+  }
+
+  return res;
+}
+
+bool is_valid_kih(string s) {
+  int idx = s.find_first_of('K');
+  return s[idx + 1] == 'I' and s[idx + 2] == 'H';
+}
+
+bool is_valid_between_kih_and_b(string s) {
+  int idx_h = s.find_first_of('H');
+  int idx_b = s.find_first_of('B');
+
+  idx_h++;
+  int cnt_A = idx_b - idx_h;
+  return cnt_A <= 1;
+}
+
+bool is_valid_between_b_and_r(string s) {
+  int idx_b = s.find_first_of('B');
+  int idx_r = s.find_first_of('R');
+
+  idx_b++;
+  int cnt_A = idx_r - idx_b;
+  return cnt_A <= 1;
+}
+
+bool is_valid_front_of_k(string s) {
+  int idx_k = s.find_first_of('K');
+  int cnt_A = idx_k;
+  return cnt_A <= 1;
+}
+
+bool is_valid_back_of_r(string s) {
+  int idx_r = s.find_first_of('R');
+  idx_r++;
+  int cnt_A = len(s) - idx_r;
+  return cnt_A <= 1;
+}
+
+int main() {
+  string s;
+  cin >> s;
+
+  if (remove_A(s) != "KIHBR") {
+    cout << "NO" << endl;
+    return 0;
+  }
+
+  // KIH が連続である
+  // KIH と B の間は A が一つ以下である
+  // B と R の間は A が一つ以下である
+  // K の前は A が一つ以下である
+  // R の後は A が一つ以下である
+
+  if (is_valid_kih(s)
+  and is_valid_between_kih_and_b(s)
+  and is_valid_between_b_and_r(s)
+  and is_valid_front_of_k(s)
+  and is_valid_back_of_r(s)) {
+    cout << "YES" << endl;
+  } else {
+    cout << "NO" << endl;
+  }
+}
