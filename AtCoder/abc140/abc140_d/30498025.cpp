5d6e96344cd6a61f0535382cea18ba9bd952b42e
crawler <>
2022-03-27 12:45:19 +0900

    add ../AtCoder/abc140/abc140_d/30498025.cpp

---
+++ AtCoder/abc140/abc140_d/30498025.cpp
@@ -0,0 +1,161 @@
+// detail: https://atcoder.jp/contests/abc140/submissions/30498025
+#include <bits/stdc++.h>
+using namespace std;
+
+// {{{ Templates
+
+// clang-format off
+
+// Macros
+#define over_load_(_1,_2,_3,_4,NAME,...) NAME
+#define rep(...) over_load_(__VA_ARGS__, rep4, rep3, rep2)(__VA_ARGS__)
+#define rep2(i, r) for ( int i = 0; i < static_cast<int>(r); (i) += 1)
+#define rep3(i, l, r) for ( int i = static_cast<int>(l); i < static_cast<int>(r); (i) += 1)
+#define rep4(i, l, r, stride) for ( int i = static_cast<int>(l); i < static_cast<int>(r); (i) += (stride))
+#define rrep(...) over_load_(__VA_ARGS__, rrep4, rrep3, rrep2)(__VA_ARGS__)
+#define rrep2(i, r) for ( int i = static_cast<int>(r) - 1; i >= 0; (i) -= 1)
+#define rrep3(i, l, r) for ( int i = static_cast<int>(r) - 1; i >= static_cast<int>(l); (i) -= 1)
+#define rrep4(i, l, r, stride) for ( int i = static_cast<int>(r) - 1; i >= static_cast<int>(l); (i) -= (stride))
+#define len(x) (static_cast<int>((x).size()))
+#define whole(f, x, ...) ([&](decltype((x)) container) { return (f)( begin(container), end(container), ## __VA_ARGS__); })(x)
+#define rwhole(f, x, ...) ([&](decltype((x)) container) { return (f)( rbegin(container), rend(container), ## __VA_ARGS__); })(x)
+#define debug(...) debug_function(#__VA_ARGS__, __VA_ARGS__)
+
+// Operators
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const pair<T1, T2> &p) { os << "(" << p.first << "," << p.second << ")"; return os; }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const map<T1, T2> &v) { bool is_first = true; for (auto x: v) { os << (is_first ? "" : " ") << x; is_first = false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, queue<T> v) { bool is_first = true; while (!v.empty()) { os << (is_first?"":" ")<<v.front(); v.pop(); is_first = false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, stack<T> v) { bool is_first = true; while (!v.empty()) { os << (is_first?"":" ") << v.top(); v.pop(); is_first=false; } return os; }
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &v) { rep (i, len(v)) os << v[i] << (i == len(v) - 1 ? "" : " "); return os; }
+template <typename T> ostream &operator<<(ostream &os, const vector<vector<T>> &v) { for (const auto &vec: v) { os << vec << '\n'; } return os; }
+template <typename T> ostream &operator<<(ostream &os, const deque<T> &v) { rep (i, len(v)) os << v[i] << (i == len(v) - 1 ? "" : " "); return os; }
+template <typename T> ostream &operator<<(ostream &os, const set<T> &v) { bool is_first = true; for (T x: v) { os << (is_first ? "" : " ") << x; is_first = false; } return os; }
+template <typename T> istream &operator>>(istream &is, vector<T> &v) { for (T &in: v) { is >> in; } return is; }
+
+// For debug macro
+int find_comma_not_bracketed(string_view s){ stack<char> bs; string lbs = "({[", rbs = ")}]"; for (size_t i = 0; i < s.size(); i++) { if (lbs.find(s[i]) != string::npos) bs.push(s[i]); if (rbs.find(s[i]) != string::npos and !bs.empty()) bs.pop(); if (s[i] == ',' and bs.empty()) return i; } return s.size(); }
+template <typename T, typename... Ts> void debug_function(string_view name, const T &a, Ts &&...rest) { int end = find_comma_not_bracketed(name); cerr << name.substr(0, end) << ":" << a; if constexpr (sizeof...(rest) == 0) { cerr << '\n'; } else { cerr << ' '; debug_function(name.substr(name.find_first_not_of(' ', end + 1)), forward<Ts>(rest)...); } }
+
+// Functions
+template <typename T> vector<T> make_vector(size_t a, T b) { return vector<T>(a, b); }
+template <typename... Ts> auto make_vector(size_t a, Ts... ts) { return vector<decltype(make_vector(ts...))>(a, make_vector(ts...)); }
+template <typename T1, typename T2> inline bool chmax(T1 &a, T2 b) { return a < b and (a = b, true); }
+template <typename T1, typename T2> inline bool chmin(T1 &a, T2 b) { return a > b and (a = b, true); }
+
+// Structs
+struct IoSetup { IoSetup(int x = 15) { cin.tie(nullptr); ios::sync_with_stdio(false); cout << fixed << setprecision(x); cerr << fixed << setprecision(x); } } iosetup;
+
+// Type aliases
+using ull = unsigned long long;
+using ll = long long;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+
+// Literals
+constexpr ll INF64 = INT64_MAX / 2;
+constexpr int INF32 = INT32_MAX / 2;
+constexpr int dy[] = { 0, 1, -1, 0, -1, 1, -1, 1 };
+constexpr int dx[] = { 1, 0, 0, -1, -1, -1, 1, 1 };
+constexpr int mod998244353 = 998244353;
+constexpr int mod1000000007 = static_cast<int>(1e9) + 7;
+constexpr char newl = '\n';
+
+// clang-format on
+
+// }}} Templates
+
+
+namespace tools {
+  using namespace std;
+}
+
+#include <type_traits>
+#include <vector>
+
+namespace tools {
+  template <typename Iterator>
+  auto runlength(Iterator first, Iterator last) {
+    vector<pair<remove_reference_t<decltype(*first)>, int>> res;
+    for (auto it = first; it != last; it++) {
+      if (res.empty() or res.back().first != *it)
+        res.emplace_back(*it, 0);
+      res.back().second++;
+    }
+    return res;
+  }
+} // namespace tools
+using namespace tools;
+
+
+int calc_happiness(string s) {
+  int res = 0;
+  rep(i, len(s)) {
+    if (s[i] == 'L' and i - 1 >= 0 and s[i - 1] == 'L') {
+      res++;
+    }
+
+    if (s[i] == 'R' and i + 1 < len(s) and s[i + 1] == 'R') {
+      res++;
+    }
+  }
+  return res;
+}
+
+
+int main() {
+  int n, k;
+  cin >> n >> k;
+
+  string s;
+  cin >> s;
+
+  auto zipped = whole(runlength, s);
+
+  int max_len = -INF32;
+  char max_len_char = 'A';
+  int max_len_idx = -1;
+
+  rep(i, len(zipped)) {
+    auto [ch, length] = zipped[i];
+    if (chmax(max_len, length)) {
+      max_len_char = ch;
+      max_len_idx = i;
+    }
+  }
+
+  if (k % 2) {
+    if (max_len_char == 'L')
+      max_len_char = 'R';
+    else
+      max_len_char = 'L';
+  }
+
+  int begin = k % 2;
+  begin = (begin + 1) % 2;
+
+  rep(i, begin, 2 * k + begin, 2) {
+    int idx1 = max_len_idx + i;
+    int idx2 = max_len_idx - i;
+
+    chmin(idx1, len(zipped) - 1);
+    chmax(idx2, 0);
+
+    // debug(idx1, idx2, i);
+
+    auto &[ch1, length1] = zipped[idx1];
+    auto &[ch2, length2] = zipped[idx2];
+    ch1 = ch2 = max_len_char;
+  }
+
+  string restore = "";
+  rep(i, len(zipped)) {
+    const auto [ch, length] = zipped[i];
+    restore += string(length, ch);
+  }
+
+  // debug(restore, zipped);
+
+
+  int ans = calc_happiness(restore);
+  cout << ans << endl;
+}
