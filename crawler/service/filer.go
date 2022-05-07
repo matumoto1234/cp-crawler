6aa4d761d254c51ddc4231b8d5d2c5a8948a73c4
crawler <>
2022-05-07 21:34:08 +0900

    #7:fix extension list

--- crawler/service/filer.go
+++ crawler/service/filer.go
@@ -16,20 +16,42 @@ type Filer interface {
 	toExtension(lang string) (string, error)
 }
 
-type affixes [2]string
+type comment struct {
+	lang string
+	affixes [2]string
+}
+
+func newComment(lang string, affixes [2]string) comment {
+	return comment{
+		lang: lang,
+		affixes: affixes,
+	}
+}
+
+type extension struct {
+	lang string
+	ext string
+}
+
+func newExtension(lang string, ext string) extension {
+	return extension{
+		lang: lang,
+		ext: ext,
+	}
+}
 
 type filerImpl struct {
-	comments   map[string]affixes
-	extensions map[string]string
+	comments   []comment
+	extensions []extension
 }
 
 func (c *filerImpl) commentOutLine(lang, line string) (string, error) {
-	for key, affix := range c.comments {
-		contains := strings.Contains(lang, key)
+	for _, v := range c.comments {
+		contains := strings.Contains(lang, v.lang)
 		if contains {
 
-			prefix := affix[0]
-			suffix := affix[1]
+			prefix := v.affixes[0]
+			suffix := v.affixes[1]
 			return prefix + line + suffix, nil
 		}
 	}
@@ -37,10 +59,10 @@ func (c *filerImpl) commentOutLine(lang, line string) (string, error) {
 }
 
 func (c *filerImpl) toExtension(lang string) (string, error) {
-	for key, ext := range c.extensions {
-		contains := strings.Contains(lang, key)
+	for _, v := range c.extensions {
+		contains := strings.Contains(lang, v.lang)
 		if contains {
-			return ext, nil
+			return v.ext, nil
 		}
 	}
 	return "", errors.New(fmt.Sprintf("GetExtension: %v is not supported.", lang))
@@ -78,88 +100,88 @@ func (c *filerImpl) MakeContent(s *model.Submission) ([]byte, error) {
 }
 
 func Newfiler() Filer {
-	comments := map[string]affixes{
-		"Awk":        {"#", ""},
-		"C#":         {"//", ""},
-		"C++":        {"//", ""},
-		"Bash":       {"#", ""},
-		"Brainfuck":  {"#", ""},
-		"COBOL":      {"*", ""},
-		"Clojure":    {";", ""},
-		"Lisp":       {";", ""},
-		"Crystal":    {"#", ""},
-		"Fortran":    {"*", ""},
-		"Go":         {"//", ""},
-		"Haskell":    {"--", ""},
-		"JavaScript": {"//", ""},
-		"Java":       {"//", ""},
-		"Julia":      {"#", ""},
-		"Kotlin":     {"//", ""},
-		"MoonScript": {"--", ""},
-		"Nim":        {"#", ""},
-		"OCaml":      {"*", "*"},
-		"Objective":  {"//", ""},
-		"Octave":     {"%{", "%}"},
-		"PHP":        {"<!--", "--?"},
-		"Pascal":     {"//", ""},
-		"Perl":       {"#", ""},
-		"Py":         {"#", ""},
-		"Ruby":       {"#", ""},
-		"Rust":       {"//", ""},
-		"Scala":      {"//", ""},
-		"Scheme":     {";", ""},
-		"Sed":        {"//", ""}, // inaccurate
-		"Swift":      {"//", ""},
-		"Text":       {"//", ""}, // inaccurate
-		"TypeScript": {"//", ""},
-		"Unlambda":   {"#", ""},
-		"Basic":      {"'", ""},
-		"ML":         {"*", "*"},
-		"F#":         {"//", ""},
-		"C":          {"//", ""},
-		"D":          {"//", ""},
+	comments := []comment{
+		newComment("Awk", [2]string{"#", ""}),
+		newComment("C#", [2]string{"//", ""}),
+		newComment("C++", [2]string{"//", ""}),
+		newComment("Bash", [2]string{"#", ""}),
+		newComment("Brainfuck", [2]string{"#", ""}),
+		newComment("COBOL", [2]string{"*", ""}),
+		newComment("Clojure", [2]string{";", ""}),
+		newComment("Lisp", [2]string{";", ""}),
+		newComment("Crystal", [2]string{"#", ""}),
+		newComment("Fortran", [2]string{"*", ""}),
+		newComment("Go", [2]string{"//", ""}),
+		newComment("Haskell", [2]string{"--", ""}),
+		newComment("JavaScript", [2]string{"//", ""}),
+		newComment("Java", [2]string{"//", ""}),
+		newComment("Julia", [2]string{"#", ""}),
+		newComment("Kotlin", [2]string{"//", ""}),
+		newComment("MoonScript", [2]string{"--", ""}),
+		newComment("Nim", [2]string{"#", ""}),
+		newComment("OCaml", [2]string{"*", "*"}),
+		newComment("Objective", [2]string{"//", ""}),
+		newComment("Octave", [2]string{"%{", "%}"}),
+		newComment("PHP", [2]string{"<!--", "--?"}),
+		newComment("Pascal", [2]string{"//", ""}),
+		newComment("Perl", [2]string{"#", ""}),
+		newComment("Py", [2]string{"#", ""}),
+		newComment("Ruby", [2]string{"#", ""}),
+		newComment("Rust", [2]string{"//", ""}),
+		newComment("Scala", [2]string{"//", ""}),
+		newComment("Scheme", [2]string{";", ""}),
+		newComment("Sed", [2]string{"//", ""}), // inaccurate
+		newComment("Swift", [2]string{"//", ""}),
+		newComment("Text", [2]string{"//", ""}), // inaccurate
+		newComment("TypeScript", [2]string{"//", ""}),
+		newComment("Unlambda", [2]string{"#", ""}),
+		newComment("Basic", [2]string{"'", ""}),
+		newComment("ML", [2]string{"*", "*"}),
+		newComment("F#", [2]string{"//", ""}),
+		newComment("C", [2]string{"//", ""}),
+		newComment("D", [2]string{"//", ""}),
 	}
 
-	extensions := map[string]string{
-		"Awk":        "awk",
-		"C#":         "cs",
-		"C++":        "cpp",
-		"Bash":       "sh",
-		"Brainfuck":  "bf",
-		"COBOL":      "cobol",
-		"Clojure":    "clj",
-		"Lisp":       "lisp",
-		"Crystal":    "cr",
-		"Fortran":    "f",
-		"Go":         "go",
-		"Haskell":    "hs",
-		"JavaScript": "js",
-		"Java":       "java",
-		"Julia":      "jl",
-		"Kotlin":     "kt",
-		"MoonScript": "moon",
-		"Nim":        "nim",
-		"OCaml":      "ml",
-		"Objective":  "m",
-		"Octave":     "m",
-		"PHP":        "php",
-		"Pascal":     "pas",
-		"Perl":       "pl",
-		"Py":         "py",
-		"Ruby":       "rb",
-		"Rust":       "rs",
-		"Scala":      "scala",
-		"Scheme":     "ss",
-		"Sed":        "sed",
-		"Swift":      "swift",
-		"Text":       "txt",
-		"TypeScript": "ts",
-		"Unlambda":   "unl",
-		"Basic":      "vb",
-		"ML":         "sml",
-		"F#":         "fs",
-		"C":          "c",
-		"D":          "d",
+	extensions := []extension{
+		newExtension("Awk", "awk"),
+		newExtension("C#", "cs"),
+		newExtension("C++", "cpp"),
+		newExtension("Bash", "sh"),
+		newExtension("Brainfuck", "bf"),
+		newExtension("COBOL", "cobol"),
+		newExtension("Clojure", "clj"),
+		newExtension("Lisp", "lisp"),
+		newExtension("Crystal", "cr"),
+		newExtension("Fortran", "f"),
+		newExtension("Go", "go"),
+		newExtension("Haskell", "hs"),
+		newExtension("JavaScript", "js"),
+		newExtension("Java", "java"),
+		newExtension("Julia", "jl"),
+		newExtension("Kotlin", "kt"),
+		newExtension("MoonScript", "moon"),
+		newExtension("Nim", "nim"),
+		newExtension("OCaml", "ml"),
+		newExtension("Objective", "m"),
+		newExtension("Octave", "m"),
+		newExtension("PHP", "php"),
+		newExtension("Pascal", "pas"),
+		newExtension("Perl", "pl"),
+		newExtension("Py", "py"),
+		newExtension("Ruby", "rb"),
+		newExtension("Rust", "rs"),
+		newExtension("Scala", "scala"),
+		newExtension("Scheme", "ss"),
+		newExtension("Sed", "sed"),
+		newExtension("Swift", "swift"),
+		newExtension("Text", "txt"),
+		newExtension("TypeScript", "ts"),
+		newExtension("Unlambda", "unl"),
+		newExtension("Basic", "vb"),
+		newExtension("ML", "sml"),
+		newExtension("F#", "fs"),
+		newExtension("C", "c"),
+		newExtension("D", "d"),
 	}
 
 	return &filerImpl{
