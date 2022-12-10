8e621da979c57ebf1f0a2cc2b0c9bb101ac08381
crawler <matumotosizuoka+crawler@gmail.com>
2022-12-10 05:22:43 +0000

    "add ../atcoder/APG4b/APG4b_cr/37125147.rs"

---
+++ atcoder/APG4b/APG4b_cr/37125147.rs
@@ -0,0 +1,10 @@
+// detail: https://atcoder.jp/contests/APG4b/submissions/37125147
+use proconio::input;
+
+fn main() {
+    input!{
+        a: i64,
+        b: i64,
+    };
+    println!("{}", a+b);
+}
