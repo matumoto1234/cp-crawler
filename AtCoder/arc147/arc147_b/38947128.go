db52bcb4eff250f63a8ac34994bf3bc38d587967
crawler <>
2023-02-17 22:33:29 +0900

    add ../AtCoder/arc147/arc147_b/38947128.go

---
+++ AtCoder/arc147/arc147_b/38947128.go
@@ -0,0 +1,91 @@
+// detail: https://atcoder.jp/contests/arc147/submissions/38947128
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func swap(x, y *int) {
+	*x, *y = *y, *x
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func main() {
+	stdin := bufio.NewReader(os.Stdin)
+	stdout := bufio.NewWriter(os.Stdout)
+	defer stdout.Flush()
+
+	var n int
+	fmt.Fscan(stdin, &n)
+
+	p := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(stdin, &p[i])
+	}
+
+	ans := make([]string, 0, 100000)
+
+	for i := 0; i < n; {
+		now := i
+		dist := abs(p[now] - (now + 1))
+		if dist%2 == 0 {
+			i++
+			continue
+		}
+
+		for {
+			next := now + 1
+			distNext := abs(p[next] - (next + 1))
+
+			if distNext%2 == 1 {
+				swap(&p[now], &p[next])
+				ans = append(ans, fmt.Sprintf("A %d", now+1))
+				now++
+				break
+			}
+
+			swap(&p[now], &p[now+2])
+			ans = append(ans, fmt.Sprintf("B %d", now+1))
+			now += 2
+		}
+	}
+
+	// t := target
+	for t := 1; t <= n; t++ {
+		// target index
+		ti := -1
+		for i := 0; i < n; i++ {
+			if t == p[i] {
+				ti = i
+				break
+			}
+		}
+
+		// destination index
+		dest := t - 1
+		for ti != dest {
+			if ti < dest {
+				swap(&p[ti], &p[ti+2])
+				ans = append(ans, fmt.Sprintf("B %d", ti+1))
+				ti += 2
+			} else {
+				swap(&p[ti], &p[ti-2])
+				ans = append(ans, fmt.Sprintf("B %d", ti-2+1))
+				ti -= 2
+			}
+		}
+	}
+
+	fmt.Fprintln(stdout, len(ans))
+	for _, s := range ans {
+		fmt.Fprintln(stdout, s)
+	}
+}
