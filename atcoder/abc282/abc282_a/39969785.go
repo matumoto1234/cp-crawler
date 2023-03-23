4239c4687f40bca736782347f46a9e96de30dfa9
crawler <matumotosizuoka+crawler@gmail.com>
2023-03-23 12:01:23 +0000

    "add ../atcoder/abc282/abc282_a/39969785.go"

---
+++ atcoder/abc282/abc282_a/39969785.go
@@ -0,0 +1,13 @@
+// detail: https://atcoder.jp/contests/abc282/submissions/39969785
+package main
+
+import "fmt"
+
+func main() {
+	var k int
+	fmt.Scan(&k)
+
+	alphabet := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+	fmt.Println(alphabet[:k])
+}
