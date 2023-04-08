47572c1614715d5da978fd3c12020b48f9928e32
crawler <>
2023-04-08 21:58:19 +0900

    add ../AtCoder/arc159/arc159_b/40419506.cpp

---
+++ AtCoder/arc159/arc159_b/40419506.cpp
@@ -0,0 +1,32 @@
+// detail: https://atcoder.jp/contests/arc159/submissions/40419506
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+
+ll naive_solve(ll a, ll b) {
+  int cnt = 0;
+  while (a >= 1 and b >= 1) {
+    int g = gcd(a, b);
+    a -= g;
+    b -= g;
+    cnt++;
+  }
+
+  return cnt;
+}
+
+int main() {
+  ll a, b;
+  cin >> a >> b;
+
+  cout << naive_solve(a, b) << endl;
+
+  // for (int a = 0; a <= 10; a++) {
+  //   for (int b = 0; b <= 10; b++) {
+  //     int ans = naive_solve(a, b);
+
+  //     cout << "a:" << a << " b:" << b << " ans:" << ans << endl;
+  //   }
+  // }
+}
