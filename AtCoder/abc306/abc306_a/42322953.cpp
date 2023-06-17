845fc5db1c640adee4e40fe34615dfe8d7a62e12
crawler <matumotosizuoka+crawler@gmail.com>
2023-06-17 12:03:42 +0000

    add ../AtCoder/abc306/abc306_a/42322953.cpp

---
+++ AtCoder/abc306/abc306_a/42322953.cpp
@@ -0,0 +1,16 @@
+// detail: https://atcoder.jp/contests/abc306/submissions/42322953
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+
+  string s;
+  cin >> s;
+
+  for (auto c: s) {
+    cout << c << c;
+  }
+  cout << endl;
+}
