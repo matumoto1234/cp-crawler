71869f4206bda95b6480deae6b5fbef19764ac44
crawler <matumotosizuoka+crawler@gmail.com>
2023-06-27 05:08:41 +0000

    add ../AtCoder/typical90/typical90_b/42995101.cpp

---
+++ AtCoder/typical90/typical90_b/42995101.cpp
@@ -0,0 +1,49 @@
+// detail: https://atcoder.jp/contests/typical90/submissions/42995101
+#include <bits/stdc++.h>
+using namespace std;
+
+bool is_valid(string s) {
+  int open = 0;
+  for (int i = 0; i < (int)s.size(); i++) {
+    if (s[i] == '(') {
+      open++;
+      continue;
+    }
+
+    if (open == 0) {
+      return false;
+    }
+
+    open--;
+  }
+
+  return open == 0;
+}
+
+int main() {
+  int n;
+  cin >> n;
+
+  vector<string> ans;
+
+  for (int bit = 0; bit < (1 << n); bit++) {
+    string s = "";
+    for (int i = 0; i < n; i++) {
+      if (bit >> i & 1) {
+        s += "(";
+      } else {
+        s += ")";
+      }
+    }
+
+    if (is_valid(s)) {
+      ans.push_back(s);
+    }
+  }
+
+  sort(ans.begin(), ans.end());
+
+  for (const auto &s: ans) {
+    cout << s << endl;
+  }
+}
