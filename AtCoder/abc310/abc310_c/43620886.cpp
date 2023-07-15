91687a17b0a47f1ee593a2113ce67e01d4219f17
crawler <matumotosizuoka+crawler@gmail.com>
2023-07-15 12:59:21 +0000

    add ../AtCoder/abc310/abc310_c/43620886.cpp

---
+++ AtCoder/abc310/abc310_c/43620886.cpp
@@ -0,0 +1,30 @@
+// detail: https://atcoder.jp/contests/abc310/submissions/43620886
+#include <bits/stdc++.h>
+using namespace std;
+
+string reversed(string s) {
+  reverse(s.begin(), s.end());
+  return s;
+}
+
+int main() {
+  int n;
+  cin >> n;
+
+  vector<string> s(n);
+  for (auto &v: s) {
+    cin >> v;
+  }
+
+  sort(s.begin(), s.end());
+
+  int cnt = 1;
+  for (int i = 0; i < n - 1; i++) {
+    if (s[i] == s[i + 1] or reversed(s[i]) == s[i + 1] or s[i] == reversed(s[i + 1])) {
+      continue;
+    }
+    cnt++;
+  }
+
+  cout << cnt << endl;
+}
