2ad2224412dc69aea0e050ace44046df232c20cc
crawler <matumotosizuoka+crawler@gmail.com>
2023-10-19 08:48:21 +0000

    add ../AtCoder/abc036/abc036_a/46735009.go

---
+++ AtCoder/abc036/abc036_a/46735009.go
@@ -0,0 +1,11 @@
+// detail: https://atcoder.jp/contests/abc036/submissions/46735009
+package main
+
+import "fmt"
+
+func main() {
+	var a, b int
+	fmt.Scan(&a, &b)
+
+	fmt.Println((b + a - 1) / a)
+}
