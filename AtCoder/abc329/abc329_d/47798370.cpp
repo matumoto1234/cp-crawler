cdb4030e2922c73c87b6dd8da75011a5b66a83a3
crawler <matumotosizuoka+crawler@gmail.com>
2023-11-21 06:46:23 +0000

    add ../AtCoder/abc329/abc329_d/47798370.cpp

---
+++ AtCoder/abc329/abc329_d/47798370.cpp
@@ -0,0 +1,42 @@
+// detail: https://atcoder.jp/contests/abc329/submissions/47798370
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+
+  vector<int> a(m);
+  for (auto &v : a) {
+    cin >> v;
+    v--;
+  }
+
+  map<int, set<int>> votes_to_candidates;
+  map<int, int> candidate_to_votes;
+
+  for (int i = 0; i < n; i++) {
+    votes_to_candidates[0].insert(i);
+    candidate_to_votes[i] = 0;
+  }
+
+  auto move_next_votes = [&](int candidate) -> void {
+    const auto &votes = candidate_to_votes[candidate];
+
+    votes_to_candidates[votes].erase(candidate);
+
+    if (votes_to_candidates[votes].empty()) {
+      votes_to_candidates.erase(votes);
+    }
+
+    votes_to_candidates[votes + 1].insert(candidate);
+    candidate_to_votes[candidate]++;
+  };
+
+  for (auto candi : a) {
+    move_next_votes(candi);
+
+    auto [_, candidates] = *votes_to_candidates.rbegin();
+    cout << *candidates.begin() + 1 << "\n";
+  }
+}
